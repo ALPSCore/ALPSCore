5c0bccb2bdd2ec86dcc06251199a982838053983
troyer <troyer@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2003-05-24 17:59:49 +0000

    Added explicit instantiation of some regex templates

--- parser/xmlstream.C
+++ parser/xmlstream.C
@@ -220,3 +220,8 @@ std::string convert(const std::string& str)
 }
 
 } // end namespace alps
+
+
+template boost::re_detail::perl_matcher<__gnu_cxx::__normal_iterator<char const*, std::basic_string<char, std::char_traits<char>, std::allocator<char> > >, std::allocator<boost::sub_match<__gnu_cxx::__normal_iterator<char const*, std::basic_string<char, std::char_traits<char>, std::allocator<char> > > > >, boost::regex_traits<char>, std::allocator<char> >;
+template boost::re_detail::perl_matcher<__gnu_cxx::__normal_iterator<char const*, std::basic_string<char, std::char_traits<char>, std::allocator<char> > >, std::allocator<boost::sub_match<__gnu_cxx::__normal_iterator<char const*, std::basic_string<char, std::char_traits<char>, std::allocator<char> > > > >, boost::regex_traits<char>, std::allocator<char> >;
+
