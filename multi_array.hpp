cdecc8af245816ddaac52d8f75f152123c6ce7ee
troyer <troyer@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2004-10-01 12:45:18 +0000

    *** empty log message ***

--- multi_array.hpp
+++ multi_array.hpp
@@ -134,7 +134,7 @@ public:
   multi_array operator-() const {
     multi_array res(*this);
     std::transform(res.begin(),res.end(),res.begin(),std::negate<T>());
-    return *this;
+    return res;
   }
  
 const multi_array& operator+=(const multi_array& x)
