5d63bdda5ef9238fcec57f40805eeab402ef430e
wistaria <wistaria@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-01-22 02:23:58 +0000

    Fixed three test failures (exmc_optimize, wl_weight, and exp_number) on Windows

--- parapack/exp_number.C
+++ parapack/exp_number.C
@@ -26,6 +26,7 @@
 *****************************************************************************/
 
 #include <alps/parapack/exp_number.h>
+#include <alps/parser/xmlstream.h>
 #include <iostream>
 
 int main() {
@@ -40,64 +41,65 @@ try {
   alps::exp_double w = alps::exp_value(9999);
   alps::exp_double p = -2.5;
 
-  std::cout << "x = " << x << " = " << static_cast<double>(x) << std::endl;
-  std::cout << "y = " << y << " = " << static_cast<double>(y) <<  std::endl;
-  std::cout << "x + y = " << x + y << " = " << static_cast<double>(x + y) <<  std::endl;
-  // std::cout << "x - y = " << x - y << " = " << static_cast<double>(x - y) <<  std::endl;
-  std::cout << "x * y = " << x * y << " = " << static_cast<double>(x * y) <<  std::endl;
-  std::cout << "x / y = " << x / y << " = " << static_cast<double>(x / y) <<  std::endl;
-  std::cout << "x + 1.2 = " << x + 1.2 << " = " << static_cast<double>(x + 1.2) <<  std::endl;
-  std::cout << "x - 1.2 = " << x - 1.2 << " = " << static_cast<double>(x - 1.2) <<  std::endl;
-  std::cout << "x - 3.5 = " << x - 3.5 << " = " << static_cast<double>(x - 3.5) <<  std::endl;
-  std::cout << "x * 1.2 = " << x * 1.2 << " = " << static_cast<double>(x * 1.2) <<  std::endl;
-  std::cout << "x / 1.2 = " << x / 1.2 << " = " << static_cast<double>(x / 1.2) <<  std::endl;
-  std::cout << "3.5 + x = " << 3.5 + x << " = " << static_cast<double>(3.5 + x) <<  std::endl;
-  std::cout << "3.5 - x = " << 3.5 - x << " = " << static_cast<double>(3.5 - x) <<  std::endl;
-  std::cout << "1.2 - x = " << 1.2 - x << " = " << static_cast<double>(1.2 - x) <<  std::endl;
-  std::cout << "3.5 * x = " << 3.5 * x << " = " << static_cast<double>(3.5 * x) <<  std::endl;
-  std::cout << "3.5 / x = " << 3.5 / x << " = " << static_cast<double>(3.5 / x) <<  std::endl;
+  std::cout << "x = " << x << " = " << alps::precision(static_cast<double>(x), 6) << std::endl;
+  std::cout << "y = " << y << " = " << alps::precision(static_cast<double>(y), 6) << std::endl;
+  std::cout << "x + y = " << x + y << " = " << alps::precision(static_cast<double>(x + y), 6) << std::endl;
+  // std::cout << "x - y = " << x - y << " = " << static_cast<double>(x - y) << std::endl;
+  std::cout << "x * y = " << x * y << " = " << alps::precision(static_cast<double>(x * y), 6) << std::endl;
+  std::cout << "x / y = " << x / y << " = " << alps::precision(static_cast<double>(x / y), 6) << std::endl;
+  std::cout << "x + 1.2 = " << x + 1.2 << " = " << alps::precision(static_cast<double>(x + 1.2), 6) << std::endl;
+  std::cout << "x - 1.2 = " << x - 1.2 << " = " << alps::precision(static_cast<double>(x - 1.2), 6) << std::endl;
+  std::cout << "x - 3.5 = " << x - 3.5 << " = " << alps::precision(static_cast<double>(x - 3.5), 6) << std::endl;
+  std::cout << "x * 1.2 = " << x * 1.2 << " = " << alps::precision(static_cast<double>(x * 1.2), 6) << std::endl;
+  std::cout << "x / 1.2 = " << x / 1.2 << " = " << alps::precision(static_cast<double>(x / 1.2), 6) << std::endl;
+  std::cout << "3.5 + x = " << 3.5 + x << " = " << alps::precision(static_cast<double>(3.5 + x), 6) << std::endl;
+  std::cout << "3.5 - x = " << 3.5 - x << " = " << alps::precision(static_cast<double>(3.5 - x), 6) << std::endl;
+  std::cout << "1.2 - x = " << 1.2 - x << " = " << alps::precision(static_cast<double>(1.2 - x), 6) << std::endl;
+  std::cout << "3.5 * x = " << 3.5 * x << " = " << alps::precision(static_cast<double>(3.5 * x), 6) << std::endl;
+  std::cout << "3.5 / x = " << 3.5 / x << " = " << alps::precision(static_cast<double>(3.5 / x), 6) << std::endl;
 
-  std::cout << "v = " << v << " = " << static_cast<double>(v) <<  std::endl;
-  std::cout << "w = " << w << " = " << static_cast<double>(w) <<  std::endl;
-  std::cout << "pow(v,3) = " << pow(v,3) << " = " << static_cast<double>(pow(v,3)) <<  std::endl;
-  std::cout << "v + w = " << v + w << " = " << static_cast<double>(v + w) <<  std::endl;
-  std::cout << "v - w = " << v - w << " = " << static_cast<double>(v - w) <<  std::endl;
-  std::cout << "v * w = " << v * w << " = " << static_cast<double>(v * w) <<  std::endl;
-  std::cout << "v / w = " << v / w << " = " << static_cast<double>(v / w) <<  std::endl;
-  std::cout << "v > w = " << (v > w) <<  std::endl;
-  std::cout << "v >= w = " << (v >= w) <<  std::endl;
-  std::cout << "v == w = " << (v == w) <<  std::endl;
-  std::cout << "v <= w = " << (v <= w) <<  std::endl;
-  std::cout << "v < w = " << (v < w) <<  std::endl;
-  std::cout << "v > v = " << (v > v) <<  std::endl;
-  std::cout << "v >= v = " << (v >= v) <<  std::endl;
-  std::cout << "v == v = " << (v == v) <<  std::endl;
-  std::cout << "v <= v = " << (v <= v) <<  std::endl;
-  std::cout << "v < v = " << (v < v) <<  std::endl;
+  std::cout << "v = " << v << " = " << alps::precision(static_cast<double>(v), 6) << std::endl;
+  std::cout << "w = " << w << " = " << alps::precision(static_cast<double>(w), 6) << std::endl;
+  std::cout << "pow(v,3) = " << pow(v,3) << " = " << alps::precision(static_cast<double>(pow(v,3)), 6)
+            << std::endl;
+  std::cout << "v + w = " << v + w << " = " << alps::precision(static_cast<double>(v + w), 6) << std::endl;
+  std::cout << "v - w = " << v - w << " = " << alps::precision(static_cast<double>(v - w), 6) << std::endl;
+  std::cout << "v * w = " << v * w << " = " << alps::precision(static_cast<double>(v * w), 6) << std::endl;
+  std::cout << "v / w = " << v / w << " = " << alps::precision(static_cast<double>(v / w), 6) << std::endl;
+  std::cout << "v > w = " << (v > w) << std::endl;
+  std::cout << "v >= w = " << (v >= w) << std::endl;
+  std::cout << "v == w = " << (v == w) << std::endl;
+  std::cout << "v <= w = " << (v <= w) << std::endl;
+  std::cout << "v < w = " << (v < w) << std::endl;
+  std::cout << "v > v = " << (v > v) << std::endl;
+  std::cout << "v >= v = " << (v >= v) << std::endl;
+  std::cout << "v == v = " << (v == v) << std::endl;
+  std::cout << "v <= v = " << (v <= v) << std::endl;
+  std::cout << "v < v = " << (v < v) << std::endl;
 
-  std::cout << "p = " << p << " = " << static_cast<double>(p) <<  std::endl;
-  std::cout << "-p = " << -p << " = " << static_cast<double>(-p) <<  std::endl;
-  std::cout << "p - p = " << p - p << " = " << static_cast<double>(p - p) <<  std::endl;
-  std::cout << "x + p = " << x + p << " = " << static_cast<double>(x + p) <<  std::endl;
-  std::cout << "x - p = " << x - p << " = " << static_cast<double>(x - p) <<  std::endl;
-  std::cout << "x * p = " << x * p << " = " << static_cast<double>(x * p) <<  std::endl;
-  std::cout << "x / p = " << x / p << " = " << static_cast<double>(x / p) <<  std::endl;
-  std::cout << "y + p = " << y + p << " = " << static_cast<double>(y + p) <<  std::endl;
-  std::cout << "y - p = " << y - p << " = " << static_cast<double>(y - p) <<  std::endl;
-  std::cout << "y * p = " << y * p << " = " << static_cast<double>(y * p) <<  std::endl;
-  std::cout << "y / p = " << y / p << " = " << static_cast<double>(y / p) <<  std::endl;
-  std::cout << "x > p = " << (x > p) <<  std::endl;
-  std::cout << "x >= p = " << (x >= p) <<  std::endl;
-  std::cout << "x == p = " << (x == p) <<  std::endl;
-  std::cout << "x <= p = " << (x <= p) <<  std::endl;
-  std::cout << "x < p = " << (x < p) <<  std::endl;
-  std::cout << "y > p = " << (y > p) <<  std::endl;
-  std::cout << "y >= p = " << (y >= p) <<  std::endl;
-  std::cout << "y == p = " << (y == p) <<  std::endl;
-  std::cout << "y <= p = " << (y <= p) <<  std::endl;
-  std::cout << "y < p = " << (y < p) <<  std::endl;
-  std::cout << "p == p = " << (p == p) <<  std::endl;
-  std::cout << "p == -p = " << (p == -p) <<  std::endl;
+  std::cout << "p = " << p << " = " << alps::precision(static_cast<double>(p), 6) << std::endl;
+  std::cout << "-p = " << -p << " = " << alps::precision(static_cast<double>(-p), 6) << std::endl;
+  std::cout << "p - p = " << p - p << " = " << alps::precision(static_cast<double>(p - p), 6) << std::endl;
+  std::cout << "x + p = " << x + p << " = " << alps::precision(static_cast<double>(x + p), 6) << std::endl;
+  std::cout << "x - p = " << x - p << " = " << alps::precision(static_cast<double>(x - p), 6) << std::endl;
+  std::cout << "x * p = " << x * p << " = " << alps::precision(static_cast<double>(x * p), 6) << std::endl;
+  std::cout << "x / p = " << x / p << " = " << alps::precision(static_cast<double>(x / p), 6) << std::endl;
+  std::cout << "y + p = " << y + p << " = " << alps::precision(static_cast<double>(y + p), 6) << std::endl;
+  std::cout << "y - p = " << y - p << " = " << alps::precision(static_cast<double>(y - p), 6) << std::endl;
+  std::cout << "y * p = " << y * p << " = " << alps::precision(static_cast<double>(y * p), 6) << std::endl;
+  std::cout << "y / p = " << y / p << " = " << alps::precision(static_cast<double>(y / p), 6) << std::endl;
+  std::cout << "x > p = " << (x > p) << std::endl;
+  std::cout << "x >= p = " << (x >= p) << std::endl;
+  std::cout << "x == p = " << (x == p) << std::endl;
+  std::cout << "x <= p = " << (x <= p) << std::endl;
+  std::cout << "x < p = " << (x < p) << std::endl;
+  std::cout << "y > p = " << (y > p) << std::endl;
+  std::cout << "y >= p = " << (y >= p) << std::endl;
+  std::cout << "y == p = " << (y == p) << std::endl;
+  std::cout << "y <= p = " << (y <= p) << std::endl;
+  std::cout << "y < p = " << (y < p) << std::endl;
+  std::cout << "p == p = " << (p == p) << std::endl;
+  std::cout << "p == -p = " << (p == -p) << std::endl;
 
 #ifndef BOOST_NO_EXCEPTIONS
 }
