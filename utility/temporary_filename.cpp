3ddf11bf526edfcc43054395227d5f1b8129db33
troyer <troyer@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-04-29 19:15:26 +0000

    Fixed Windows compatibility issues

--- utility/temporary_filename.cpp
+++ utility/temporary_filename.cpp
@@ -11,9 +11,10 @@
 
 
 #ifdef BOOST_MSVC
-  #include <fcntl.h>
+#include <fcntl.h>
+#include <io.h>
 #else
-  #include <unistd.h>
+#include <unistd.h>
 #endif
 
 
@@ -23,7 +24,7 @@ namespace alps {
     name +="XXXXXX";
 #ifdef BOOST_MSVC
     mktemp(const_cast<char*>(name.c_str())); 
-    ret=open(tmpl,O_RDWR|O_BINARY|O_CREAT|O_EXCL|_O_SHORT_LIVED, 128|256);
+    int res=open(name.c_str(),O_RDWR|O_BINARY|O_CREAT|O_EXCL|_O_SHORT_LIVED, 128|256);
 #else
     int res = mkstemp(const_cast<char*>(name.c_str()));
 #endif
