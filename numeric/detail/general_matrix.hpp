f038b713bde26bd92d5fc9ad2f85d30882507b01
gullc <gullc@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-05-04 22:11:17 +0000

    adding ALPS license blocks

--- numeric/detail/general_matrix.hpp
+++ numeric/detail/general_matrix.hpp
@@ -8,8 +8,24 @@
  *                              Brigitte Surer <surerb@phys.ethz.ch>
  *
  *
- * THIS SOFTWARE NEEDS AN APPROPRIATE LICENSE BLOCK HERE
- *****************************************************************************/
+* This software is part of the ALPS Applications, published under the ALPS
+* Application License; you can use, redistribute it and/or modify it under
+* the terms of the license, either version 1 or (at your option) any later
+* version.
+* 
+* You should have received a copy of the ALPS Application License along with
+* the ALPS Applications; see the file LICENSE.txt. If not, the license is also
+* available from http://alps.comp-phys.org/.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+* FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT 
+* SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE 
+* FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE, 
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+* DEALINGS IN THE SOFTWARE.
+*
+*****************************************************************************/
 
 #ifndef GENBLAS_MATRIX
 #define GENBLAS_MATRIX
