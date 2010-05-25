da270dd42c9e70c9d37872ed62a9cfa15e1a0573
troyer <troyer@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-05-25 14:57:30 +0000

    Fixed subworkflows for VT 1.5 and fixed license, copyright and tab issues

--- hdf5/hdf5.cpp
+++ hdf5/hdf5.cpp
@@ -1,3 +1,30 @@
+/*****************************************************************************
+*
+* ALPS Project: Algorithms and Libraries for Physics Simulations
+*
+* ALPS Libraries
+*
+* Copyright (C) 2010 by Lukas Gamper <gamperl -at- gmail.com>
+*
+* This software is part of the ALPS libraries, published under the ALPS
+* Library License; you can use, redistribute it and/or modify it under
+* the terms of the license, either version 1 or (at your option) any later
+* version.
+* 
+* You should have received a copy of the ALPS Library License along with
+* the ALPS Libraries; see the file LICENSE.txt. If not, the license is also
+* available from http://alps.comp-phys.org/.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+* FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT 
+* SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE 
+* FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE, 
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+* DEALINGS IN THE SOFTWARE.
+*
+*****************************************************************************/
+
 #include <alps/hdf5.hpp>
 
 #include <string>
@@ -53,7 +80,7 @@ template<typename T> void test(std::string const & name) {
         std::cout << name << " SUCCESS" << std::endl;
 }
 #define HDF5_FOREACH_TYPE(type)                                                                                                                            \
-	test<type>(#type);
+    test<type>(#type);
 #define HDF5_FOREACH_COMPLEX(type)                                                                                                                         \
     HDF5_FOREACH_TYPE(std::complex< type >)
 #define HDF5_FOREACH_SCALAR(callback)                                                                                                                      \
