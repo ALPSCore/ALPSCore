b24164dfebcb6f7d75a8b74816d88d1ee4e24388
troyer <troyer@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-07-02 16:51:04 +0000

    Attempted fix for #349

--- utility/temporary_filename.cpp
+++ utility/temporary_filename.cpp
@@ -30,7 +30,9 @@ namespace alps {
     int res=0; 
 	//int res=open(name.c_str(),O_RDWR|O_BINARY|O_CREAT|O_EXCL|_O_SHORT_LIVED, 128|256);
 #else
-    int res = mkstemp(const_cast<char*>(name.c_str()));
+	name = mktemp(const_cast<char*>(name.c_str()));
+    int res=0;
+    //int res = mkstemp(const_cast<char*>(name.c_str()));
 #endif
     if (res<0)
       boost::throw_exception(std::runtime_error("Could not open temporary file"));
