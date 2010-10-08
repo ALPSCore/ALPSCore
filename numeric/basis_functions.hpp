aa9603c9973ea3c356280c9075e1ccaa3c93af50
tamama <tamama@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-10-08 14:23:56 +0000

    fourier_real struct added in <basis_functions.hpp>

--- numeric/basis_functions.hpp
+++ numeric/basis_functions.hpp
@@ -31,8 +31,10 @@
 #ifndef ALPS_NUMERIC_BASIS_FUNCTIONS_HPP
 #define ALPS_NUMERIC_BASIS_FUNCTIONS_HPP
 
+#include <alps/numeric/vector_functions.hpp>
 
 #include <boost/iterator/counting_iterator.hpp>
+#include <boost/bind.hpp>
 #include <boost/lambda/lambda.hpp>
 
 
@@ -82,6 +84,93 @@ struct polynomial
 
 			return std::inner_product(C_prime.begin(), C_prime.end(), X.begin(), 0.);
 		}	
+};
+
+
+struct fourier_real
+{
+	template <class T>
+	static T
+		cos
+		(	T & x
+		)
+	{
+		return std::cos(x);
+	}
+
+	template <class T>
+	static T
+		sin
+		(	T & x
+		)
+	{
+		return std::sin(x);
+	}
+
+
+	template <class S, class T>
+	static T
+		evaluate
+			(	std::vector<T>	const &	A		//	[a0, a1, a2,...]
+			,	std::vector<T>	const &	B		//	[b0, b1, b2,...]
+			,	T								const &	x		//	x
+			,	S								const & N		//	N
+			)
+		{
+			T	_2PIx_over_N	=	((M_2PI * x) / N);
+
+			std::vector<T>	_2PInx_over_N;
+			_2PInx_over_N.reserve(A.size());
+			std::transform
+				(	boost::counting_iterator<T>(0)
+				,	boost::counting_iterator<T>(A.size())
+				,	std::back_inserter(_2PInx_over_N)
+				, boost::lambda::_1 * _2PIx_over_N
+				);	
+
+			std::vector<T>	C = alps::numeric::cos(_2PInx_over_N);
+			std::vector<T>	S = alps::numeric::sin(_2PInx_over_N);
+
+			return ( std::accumulate(C.begin(), C.end(), 0.) +  std::accumulate(S.begin()+1, S.end(), 0.) );
+
+/*
+			C.reserve(A.size());
+			S.reserve(B.size());
+
+			std::transform
+				(	A.begin()
+				, A.end()
+				, _2PInx_over_N.begin()
+				, std::back_inserter(C)
+				, boost::bind
+					(	std::multiplies<T>()
+					, boost::lambda::_1
+					,	boost::bind
+						(	&alps::numeric::fourier_real::cos<T>
+					  ,	boost::lambda::_2
+						)
+					)
+				);
+
+			std::transform
+				(	B.begin()
+				, B.end()
+				, _2PInx_over_N.begin()
+				, std::back_inserter(S)
+				, boost::bind
+					(	std::multiplies<T>()
+					, boost::lambda::_1
+					,	boost::bind
+						(	&alps::numeric::fourier_real::sin<T>
+					  ,	boost::lambda::_2
+						)
+					)
+				);
+
+			return ( std::accumulate(C.begin(), C.end(), 0.) +	std::accumulate(S.begin()+1, S.end(), 0.)	);
+*/
+
+		} // objective:	F(x) = a0 + a1 * cos(2\pix/N) + a2 * cos(4\pix/N) + ... + b1 * sin(2\pix/N) + b2 * sin(4\pix/N) + ...
 };
 
 
