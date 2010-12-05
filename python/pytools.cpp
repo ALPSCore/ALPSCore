138fd2a5a8cab5c420602db1c955ebe282d3fb13
troyer <troyer@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2010-12-05 00:50:16 +0000

    Added pytools.cpp docstrings

--- python/pytools.cpp
+++ python/pytools.cpp
@@ -57,17 +57,65 @@ public:
     
 };
 
+const char convert2xml_docstring[] = 
+  "converts a file to XML\n"
+  "\n"
+  "This function takes the path to an ALPS file as input and converts it to XML.\n"
+  "It returns a string with the path to the resulting XML file";
+
+const char hdf5_name_encode_docstring[] = 
+"encodes a string for use in HDF5 paths\n"
+"\n"
+"This function takes a string and escapes all needed characters for it to be "
+"used in HDF5 path names.";
+
+const char hdf5_name_decode_docstring[] = 
+"decodes a string fromHDF5 paths\n"
+"\n"
+"This function takes a string used in an HDF5 path name and replaces all "
+"escaped characters.";
+
+const char search_xml_library_path_docstring[] = 
+"returns the full path for an ALPS XML file\n"
+"\n"
+"This function takes the name for an ALPS library XML or XSL file and returns "
+"the full path.";
+
+const char rng_docstring[] = 
+"a uniform random number generator class\n\n"
+"This class uses the Mersenne Twister rgenerator mt19937 to generate uniform "
+"random numbers in the range [0,1).\n"
+"The constructor takes an optional integer random seed argument.\n"
+"Random numbers are created using the function call operator.\n";
+
+
+namespace  {
+  void wrap_with_signature()
+  {
+    using namespace boost::python;
+    def("convert2xml", alps::convert2xml,convert2xml_docstring);
+    def("hdf5_name_encode", alps::hdf5_name_encode,hdf5_name_encode_docstring);
+    def("hdf5_name_decode", alps::hdf5_name_decode,hdf5_name_decode_docstring);
+    def("search_xml_library_path", alps::search_xml_library_path,search_xml_library_path_docstring);
+  }
+  
+  void wrap_without_signature()
+  {
+    using namespace boost::python;
+    docstring_options doc_options(true);
+    doc_options.disable_cpp_signatures();
+    class_<WrappedRNG>("rng", rng_docstring,init<optional<int> >("the constructor takes an optional integer argument as random number seed"))
+    .def("__deepcopy__",  &alps::python::make_copy<WrappedRNG>, "the deepcopy function creates a new copy of the generator")
+    .def("__call__", static_cast<WrappedRNG::result_type(WrappedRNG::*)()>(&WrappedRNG::operator()), "returns a uniform random number in [0,1)")
+    ;
+  }
+  
+}
+
 BOOST_PYTHON_MODULE(pytools_c)
 {
   using namespace boost::python;
-  def("convert2xml", alps::convert2xml);
-  def("hdf5_name_encode", alps::hdf5_name_encode);
-  def("hdf5_name_decode", alps::hdf5_name_decode);
-  def("search_xml_library_path", alps::search_xml_library_path);
-    
-    class_<WrappedRNG>("rng", init<optional<int> >() )
-    .def("__deepcopy__",  &alps::python::make_copy<WrappedRNG>)
-    .def("__call__", static_cast<WrappedRNG::result_type(WrappedRNG::*)()>(&WrappedRNG::operator()))
-    ;
+  wrap_with_signature();
+  wrap_without_signature();
 }
 
