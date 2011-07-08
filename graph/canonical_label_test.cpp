55f6a554d4fe1eababbd4b2d3c0b30d11d4dc344
gamperl <gamperl@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2011-07-08 13:46:51 +0000

    fixed bug in generation of canonical label

---
+++ graph/canonical_label_test.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+#include <alps/graph/canonical_properties.hpp>
+#include <boost/graph/adjacency_list.hpp>
+#include <iostream>
+
+int main() {
+    using alps::graph::canonical_properties;
+    typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> graph_type;
+    graph_type g;
+
+    add_edge(0, 1,g);
+    add_edge(0, 2,g);
+    add_edge(0, 3,g);
+    add_edge(2, 1,g);
+
+    alps::graph::graph_label<graph_type>::type label(get<1>(canonical_properties(g)));
+    
+    std::cout << label << std::endl;
+    return 0;
+}
