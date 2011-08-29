d37f04b238d534be894877343ed57c2328a6628e
gamperl <gamperl@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2011-08-29 11:52:32 +0000

    skip unconnected subgraphs

--- graph/subgraphs.hpp
+++ graph/subgraphs.hpp
@@ -32,12 +32,32 @@
 #include <boost/graph/graph_traits.hpp>
 
 #include <set>
+#include <deque>
 
 namespace alps {
 	namespace graph {
 	
 		namespace detail {
 		
+			template<typename Graph> bool is_connected(
+				  typename boost::graph_traits<Graph>::vertex_descriptor const & s
+				, typename boost::graph_traits<Graph>::vertex_descriptor const & t
+				, Graph const & G
+			) {
+				std::set<typename boost::graph_traits<Graph>::vertex_descriptor> V;
+				std::deque<typename boost::graph_traits<Graph>::vertex_descriptor> S(1, s);
+				typename boost::graph_traits<Graph>::adjacency_iterator ai, ae;
+				while (S.size()) {
+					for (tie(ai, ae) = adjacent_vertices(S.front(), G); ai != ae; ++ai)
+						if (*ai == t)
+							return true;
+						else if (V.insert(*ai).second)
+							S.push_back(*ai);
+					S.pop_front();
+				}
+				return false;
+			}
+
 			template<typename Graph> void subgraphs_helper(
 				  std::set<boost::dynamic_bitset<> > & L
 				, typename boost::graph_traits<Graph>::vertex_descriptor const & s
@@ -46,14 +66,16 @@ namespace alps {
 			) {
 				remove_edge(s, t, G);
 				if (num_edges(G)) {
-					boost::dynamic_bitset<> l(num_vertices(G) * (num_vertices(G) + 1) / 2);
-					typename boost::graph_traits<Graph>::edge_iterator it, end;
-					for	(boost::tie(it, end) = edges(G); it != end; ++it)
-						l[source(*it, G) * num_vertices(G) - (source(*it, G) - 1) * source(*it, G) / 2 + target(*it, G) - source(*it, G)] = true;
-					if (L.insert(l).second) {
-						typename boost::graph_traits<Graph>::edge_iterator it, end;
-						for	(boost::tie(it, end) = edges(G); it != end; ++it)
-							detail::subgraphs_helper(L, source(*it, G), target(*it, G), G);
+					if (!out_degree(s, G) || !out_degree(t, G) || is_connected(s, t, G)) {
+						boost::dynamic_bitset<> l(num_vertices(G) * (num_vertices(G) + 1) / 2);
+						typename boost::graph_traits<Graph>::edge_iterator ei, ee;
+						for	(boost::tie(ei, ee) = edges(G); ei != ee; ++ei)
+							l[source(*ei, G) * num_vertices(G) - (source(*ei, G) - 1) * source(*ei, G) / 2 + target(*ei, G) - source(*ei, G)] = true;
+						if (L.insert(l).second) {
+							typename boost::graph_traits<Graph>::edge_iterator ei, ee;
+							for	(boost::tie(ei, ee) = edges(G); ei != ee; ++ei)
+								detail::subgraphs_helper(L, source(*ei, G), target(*ei, G), G);
+						}
 					}
 				}
 			}
