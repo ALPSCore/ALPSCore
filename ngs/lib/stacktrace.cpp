9571e368519db7506d40c05f7d0b996bd0e79c56
wistaria <wistaria@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2011-09-06 03:34:35 +0000

    Reverted [5724]

--- ngs/lib/stacktrace.cpp
+++ ngs/lib/stacktrace.cpp
@@ -27,7 +27,7 @@
 
 #include <alps/ngs/stacktrace.hpp>
 
-#if defined(__GNUC__) && !defined ALPS_NGS_NO_STACKTRACE
+#if defined(__GNUC__) && not defined ALPS_NGS_NO_STACKTRACE
 
 
     #include <execinfo.h>
