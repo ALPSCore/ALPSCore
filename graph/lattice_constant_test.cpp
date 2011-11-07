cc6de5ee6037cf78bbbe92eb6c03b7bad5855cd4
gamperl <gamperl@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2011-11-07 17:58:54 +0000

    improved lattice_constant

--- graph/lattice_constant_test.cpp
+++ graph/lattice_constant_test.cpp
@@ -1,40 +1,28 @@
-// TODO: remove
-#include <iostream>
-
-
 #include <alps/graph/lattice_constant.hpp>
+
 #include <boost/graph/adjacency_list.hpp>
 #include <iostream>
-
-template <typename Graph>
-Graph build_square_lattice(unsigned int side_length)
-{
-    Graph g;
-    for(unsigned int j=1; j < side_length; ++j)
-        add_edge(j,j-1,g);
-    
-    for(unsigned int i=1; i < side_length; ++i)
-    {
-        add_edge(i*side_length,(i-1)*side_length,g);
-        for(unsigned int j=1; j < side_length; ++j)
-        {
-            add_edge(i*side_length+j,(i-1)*side_length+j,g);
-            add_edge(i*side_length+j,i*side_length+j-1,g);
-        }
-    }
-    return g;
-}
 
 int main() {
     using boost::get;
     using alps::graph::canonical_properties;
 
-    typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> graph_type;
     typedef unsigned int lc_type;
+    typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS> graph_type;
+    typedef alps::graph_helper<>::vertex_iterator vertex_iterator;
+    typedef alps::graph_helper<>::edge_iterator edge_iterator;
 
-    unsigned int side_length = 3;
-//    unsigned int side_length = 10;
-    graph_type lattice = build_square_lattice<graph_type>(side_length);
+    alps::Parameters parm;
+	unsigned int side_length = 3;
+	
+    parm["LATTICE"] = "square lattice";
+    parm["W"] = parm["L"] = 5;
+    alps::graph_helper<> lattice(parm);
+	
+	graph_type lattice_graph(num_vertices(lattice.graph()));
+	boost::graph_traits<alps::graph_helper<>::graph_type>::edge_iterator it, et;
+	for(boost::tie(it, et) = edges(lattice.graph()); it != et; ++it)
+		add_edge(source(*it, lattice.graph()), target(*it, lattice.graph()), lattice_graph);
 
     std::vector<std::pair<graph_type,lc_type> > g;
 
@@ -43,11 +31,11 @@ int main() {
     //  |   |
     //  2---3
     //
-    g.push_back(std::make_pair(graph_type(),4));
-    add_edge(0, 1,g.back().first);
-    add_edge(1, 3,g.back().first);
-    add_edge(3, 2,g.back().first);
-    add_edge(2, 0,g.back().first);
+    g.push_back(std::make_pair(graph_type(), 1));
+    add_edge(0, 1, g.back().first);
+    add_edge(1, 3, g.back().first);
+    add_edge(3, 2, g.back().first);
+    add_edge(2, 0, g.back().first);
 /*
     //
     //  1---0---2
@@ -84,9 +72,15 @@ int main() {
     add_edge(1, 5,g.back().first);
 */
     int success = 0;
-    for(std::vector<std::pair<graph_type,lc_type> >::iterator it=g.begin(); it != g.end(); ++it)
+    for(std::vector<std::pair<graph_type,lc_type> >::iterator it= g.begin(); it != g.end(); ++it)
     {
-        lc_type lc = alps::graph::lattice_constant(it->first, lattice, std::vector<boost::graph_traits<graph_type>::vertex_descriptor>(1,side_length*side_length/2+side_length/2 - 1));
+        lc_type lc = alps::graph::lattice_constant(
+			  it->first
+			, lattice_graph
+			, lattice.lattice()
+			, lattice.graph()
+			, std::vector<boost::graph_traits<graph_type>::vertex_descriptor>(1,side_length*side_length/2+side_length/2 - 1)
+		);
         if ( lc != it->second)
         {
             std::cout<<"ERROR: lattice constant does not match!"<<std::endl;
