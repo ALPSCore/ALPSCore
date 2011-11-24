3b3b8b340b3b0ab4c389e04dd548a10d47fd1e48
gamperl <gamperl@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2011-11-24 13:38:00 +0000

    fixed performance issue in hdf5 on ar << make_pvp(path, T const *, size);

---
+++ hdf5/hdf5_pair.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <alps/hdf5.hpp>
+#include <alps/hdf5/vector.hpp>
+
+int main() {
+	std::vector<double> a(1e6);
+	alps::hdf5::archive ar("creal.h5",alps::hdf5::archive::WRITE);
+	ar << alps::make_pvp("a",
+		std::make_pair(
+			static_cast< double const *>(&a.front())
+			, std::vector<std::size_t>(1,a.size())
+		)
+	);
+}
