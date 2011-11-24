81d76fb015db8ecc71b8ffc1708bbc3213237c5e
gamperl <gamperl@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2011-11-24 15:37:04 +0000

    some hdf5 improvements

--- hdf5/hdf5_pair.cpp
+++ hdf5/hdf5_pair.cpp
@@ -1,14 +1,27 @@
 #include <vector>
+#include <complex>
+
 #include <alps/hdf5.hpp>
 #include <alps/hdf5/vector.hpp>
 
 int main() {
-	std::vector<double> a(1e6);
 	alps::hdf5::archive ar("creal.h5",alps::hdf5::archive::WRITE);
-	ar << alps::make_pvp("a",
-		std::make_pair(
-			static_cast< double const *>(&a.front())
-			, std::vector<std::size_t>(1,a.size())
-		)
-	);
+	{
+		std::vector<double> a(1e6);
+		ar << alps::make_pvp("a",
+			std::make_pair(
+				static_cast< double const *>(&a.front())
+				, std::vector<std::size_t>(1,a.size())
+			)
+		);
+	}
+	{
+		std::vector<std::complex<double> > a(1e6);
+		ar << alps::make_pvp("a",
+			std::make_pair(
+				static_cast<std::complex<double> const *>(&a.front())
+				, std::vector<std::size_t>(1,a.size())
+			)
+		);
+	}
 }
