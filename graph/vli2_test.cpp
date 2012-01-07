addaf7c50adf94313445b2c09f2ff1bb380332e9
gamperl <gamperl@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2012-01-07 13:15:55 +0000

    improved vli, introduced two complement, removed carry bits

---
+++ graph/vli2_test.cpp
@@ -0,0 +1,42 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                 *
+ * ALPS Project: Algorithms and Libraries for Physics Simulations                  *
+ *                                                                                 *
+ * ALPS Libraries                                                                  *
+ *                                                                                 *
+ * Copyright (C) 2010 - 2012 by Lukas Gamper <gamperl@gmail.com>                   *
+ *                                                                                 *
+ * This software is part of the ALPS libraries, published under the ALPS           *
+ * Library License; you can use, redistribute it and/or modify it under            *
+ * the terms of the license, either version 1 or (at your option) any later        *
+ * version.                                                                        *
+ *                                                                                 *
+ * You should have received a copy of the ALPS Library License along with          *
+ * the ALPS Libraries; see the file LICENSE.txt. If not, the license is also       *
+ * available from http://alps.comp-phys.org/.                                      *
+ *                                                                                 *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,        *
+ * FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT       *
+ * SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE       *
+ * FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,     *
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER     *
+ * DEALINGS IN THE SOFTWARE.                                                       *
+ *                                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+#include <alps/graph/vli.hpp>
+
+#include <iostream>
+
+int main() {
+	{
+		alps::graph::vli<6> a(1), b(8264), c(-1), d(-862), e(100005), f(72677163250000LL);
+		std::cout << a << " " << b << " " << c << " " << d << " " << e << " " << (f += 18947077082LL) << std::endl;
+	}
+	{
+		alps::graph::vli<4> a(0);
+		a -= 854116085LL;
+		std::cout << a << std::endl;
+	}
+};
