3253a6dade73ef289b2ca70b0cd1d66009d414b9
gamperl <gamperl@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2012-01-22 12:33:07 +0000

    minor hdf5 fixes

--- ngs/lib/hdf5.cpp
+++ ngs/lib/hdf5.cpp
@@ -694,8 +694,6 @@ namespace alps {
                     ALPS_NGS_THROW_RUNTIME_ERROR("Not Implemented, path: " + path)
         #define ALPS_NGS_HDF5_READ_VECTOR(T)                                                                                                                           \
             void archive::read(std::string path, T * value, std::vector<std::size_t> chunk, std::vector<std::size_t> offset) const {                                   \
-			    if (!is_data(path) && !is_attribute(path))																																\
-					ALPS_NGS_THROW_RUNTIME_ERROR("the path does not exists: " + path)																				\
                 std::vector<std::size_t> data_size = extent(path);                                                                                                 \
                 if (offset.size() == 0)                                                                                                                            \
                     offset = std::vector<std::size_t>(dimensions(path), 0);                                                                                        \
@@ -711,7 +709,9 @@ namespace alps {
                         if (chunk[i] == 0)                                                                                                                         \
                             ALPS_NGS_THROW_RUNTIME_ERROR("size is zero in one dimension in path: " + path)                                                         \
                     if ((path = complete_path(path)).find_last_of('@') == std::string::npos) {                                                                     \
-                        if (!is_data(path) || is_scalar(path))                                                                                                     \
+						if (!is_data(path))																															\
+							ALPS_NGS_THROW_RUNTIME_ERROR("the path does not exists: " + path)																		\
+                        if (is_scalar(path))																														\
                             ALPS_NGS_THROW_RUNTIME_ERROR("scalar - vector conflict in path: " + path)                                                              \
                         detail::data_type data_id(H5Dopen2(context_->file_id_, path.c_str(), H5P_DEFAULT));                                                        \
                         detail::type_type type_id(H5Dget_type(data_id));                                                                                           \
@@ -740,7 +740,9 @@ namespace alps {
                         ALPS_NGS_HDF5_FOREACH_NATIVE_TYPE_INTEGRAL(ALPS_NGS_HDF5_READ_VECTOR_DATA_HELPER, T)                                                       \
                         } else ALPS_NGS_THROW_RUNTIME_ERROR("invalid type")                                                                                        \
                     } else {                                                                                                                                       \
-                        if (!is_attribute(path) || is_scalar(path))                                                                                                \
+						if (!is_attribute(path))																													\
+							ALPS_NGS_THROW_RUNTIME_ERROR("the path does not exists: " + path)																		\
+                        if (is_scalar(path))																														\
                             ALPS_NGS_THROW_RUNTIME_ERROR("scalar - vector conflict in path: " + path)                                                              \
                         hid_t parent_id;                                                                                                                           \
                         if (is_group(path.substr(0, path.find_last_of('@') - 1)))                                                                                  \
