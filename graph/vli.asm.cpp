c6ae6c254c8b6bc86dbe0bcada60c443932e3306
gamperl <gamperl@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2012-02-06 13:24:52 +0000

    fixed non existing macro

--- graph/vli.asm.cpp
+++ graph/vli.asm.cpp
@@ -26,7 +26,7 @@
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 // atm vli only works with GCC
-#if defined(__GNUG__) && !defined(__ICC) && !defined(__FCC_VERSION) && defined(_GLIBCXX_USE_INT128)
+#if defined(__GNUG__) && !defined(__ICC) && !defined(__FCC_VERSION)
 
 #include <boost/cstdint.hpp>
 
