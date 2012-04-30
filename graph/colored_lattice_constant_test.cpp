1ac01caeb7d4708a5b312d8e4513c34912379a81
hehn <hehn@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2012-04-30 13:52:19 +0000

    cleanup of the graph codes and tests

--- graph/colored_lattice_constant_test.cpp
+++ graph/colored_lattice_constant_test.cpp
@@ -1,3 +1,31 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *                                                                                 *
+ * ALPS Project: Algorithms and Libraries for Physics Simulations                  *
+ *                                                                                 *
+ * ALPS Libraries                                                                  *
+ *                                                                                 *
+ * Copyright (C) 2011 - 2012 by Andreas Hehn <hehn@phys.ethz.ch>                   *
+ *                              Robin Jaeger <jaegerr@phys.ethz.ch>                *
+ *                                                                                 *
+ * This software is part of the ALPS libraries, published under the ALPS           *
+ * Library License; you can use, redistribute it and/or modify it under            *
+ * the terms of the license, either version 1 or (at your option) any later        *
+ * version.                                                                        *
+ *                                                                                 *
+ * You should have received a copy of the ALPS Library License along with          *
+ * the ALPS Libraries; see the file LICENSE.txt. If not, the license is also       *
+ * available from http://alps.comp-phys.org/.                                      *
+ *                                                                                 *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,        *
+ * FITNESS FOR A PARTICULAR PURPOSE, TITLE AND NON-INFRINGEMENT. IN NO EVENT       *
+ * SHALL THE COPYRIGHT HOLDERS OR ANYONE DISTRIBUTING THE SOFTWARE BE LIABLE       *
+ * FOR ANY DAMAGES OR OTHER LIABILITY, WHETHER IN CONTRACT, TORT OR OTHERWISE,     *
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER     *
+ * DEALINGS IN THE SOFTWARE.                                                       *
+ *                                                                                 *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #include <alps/graph/lattice_constant.hpp>
 #include <alps/lattice/lattice.h>
 #include <boost/graph/adjacency_list.hpp>
@@ -30,8 +58,6 @@ int main() {
 	boost::graph_traits<alps::graph_helper<>::graph_type>::edge_iterator it, et;
 	for(boost::tie(it, et) = edges(lattice.graph()); it != et; ++it)
     {
-//        std::cout<<source(*it, lattice.graph())<<" - "<<target(*it, lattice.graph())<<" t:";
-//        std::cout<<get(alps::edge_type_t(),lattice.graph(),*it)<<std::endl;
         boost::graph_traits<graph_type>::edge_descriptor  e = add_edge(source(*it, lattice.graph()), target(*it, lattice.graph()), lattice_graph).first;
 		if(get(alps::edge_type_t(),lattice.graph(),*it) == 1)
 			put(alps::edge_type_t(), lattice_graph, e, 1);
@@ -250,148 +276,6 @@ int main() {
     put(alps::edge_type_t(), g.back().first, e, 1);
     e = add_edge(2, 4,g.back().first).first;
     put(alps::edge_type_t(), g.back().first, e, 0);
-
-    /*
-    parm["LATTICE"] = "anisotropic square lattice";
-    parm["L"] = side_length;
-    alps::graph_helper<> lattice(parm);
-	
-	graph_type lattice_graph(num_vertices(lattice.graph()));
-	boost::graph_traits<alps::graph_helper<>::graph_type>::edge_iterator it, et;
-	for(boost::tie(it, et) = edges(lattice.graph()); it != et; ++it)
-    {
-//        std::cout<<source(*it, lattice.graph())<<" - "<<target(*it, lattice.graph())<<" t:";
-//        std::cout<<get(alps::edge_type_t(),lattice.graph(),*it)<<std::endl;
-        boost::graph_traits<graph_type>::edge_descriptor  e = add_edge(source(*it, lattice.graph()), target(*it, lattice.graph()), lattice_graph).first;
-        put(alps::edge_type_t(), lattice_graph, e, get(alps::edge_type_t(),lattice.graph(),*it) );
-    }
-
-    std::vector<std::pair<graph_type,lc_type> > g;
-    //
-    //  0...1
-    //  |   |
-    //  2...3
-    //
-    g.push_back(std::make_pair(graph_type(), 1));
-    boost::graph_traits<graph_type>::edge_descriptor e = add_edge(0, 1, g.back().first).first;
-    put(alps::edge_type_t(), g.back().first, e, 0);
-    e = add_edge(1, 3, g.back().first).first;
-    put(alps::edge_type_t(), g.back().first, e, 1);
-    e = add_edge(3, 2, g.back().first).first;
-    put(alps::edge_type_t(), g.back().first, e, 0);
-    e = add_edge(2, 0, g.back().first).first;
-    put(alps::edge_type_t(), g.back().first, e, 1);
-    
-    //
-    //  0...1
-    //  .   |
-    //  2___3
-    //
-    g.push_back(std::make_pair(graph_type(), 0));
-    e = add_edge(0, 1, g.back().first).first;
-    put(alps::edge_type_t(), g.back().first, e, 0);
-    e = add_edge(1, 3, g.back().first).first;
-    put(alps::edge_type_t(), g.back().first, e, 1);
-    e = add_edge(3, 2, g.back().first).first;
-    put(alps::edge_type_t(), g.back().first, e, 1);
-    e = add_edge(2, 0, g.back().first).first;
-    put(alps::edge_type_t(), g.back().first, e, 0);
-
-
-
-    //
-    //  1___0___2
-    //
-    g.push_back(std::make_pair(graph_type(),1));
-    e = add_edge(0, 1,g.back().first).first;
-    put(alps::edge_type_t(), g.back().first, e, 0);
-    e = add_edge(0, 2,g.back().first).first;
-    put(alps::edge_type_t(), g.back().first, e, 0);
-    
-    //
-    //  1...0___2
-    //
-    g.push_back(std::make_pair(graph_type(),4));
-    e = add_edge(0, 1,g.back().first).first;
-    put(alps::edge_type_t(), g.back().first, e, 0);
-    e = add_edge(0, 2,g.back().first).first;
-    put(alps::edge_type_t(), g.back().first, e, 1);
-    */
-
-    
-//    //
-//    //  3---1---0---2
-//    //
-//    g.push_back(std::make_pair(graph_type(),18));
-//    add_edge(0, 1,g.back().first);
-//    add_edge(0, 2,g.back().first);
-//    add_edge(1, 3,g.back().first);
-//
-//    //
-//    //     3
-//    //     |
-//    // 1---0---2
-//    //     |
-//    //     4
-//    //
-//    g.push_back(std::make_pair(graph_type(),1));
-//    add_edge(0, 1,g.back().first);
-//    add_edge(0, 2,g.back().first);
-//    add_edge(0, 3,g.back().first);
-//    add_edge(0, 4,g.back().first);
-//
-//    //
-//    //   2       5
-//    //    \     /
-//    //     0---1
-//    //    /     \ 
-//    //   3       4
-//    //
-//    g.push_back(std::make_pair(graph_type(),18));
-//    add_edge(0, 1,g.back().first);
-//    add_edge(0, 2,g.back().first);
-//    add_edge(0, 3,g.back().first);
-//    add_edge(1, 4,g.back().first);
-//    add_edge(1, 5,g.back().first);
-//
-//    //
-//    //           8
-//    //           |
-//    //           4
-//    //           |
-//    //   6---2---0---1---5
-//    //           |
-//    //           3
-//    //           |
-//    //           7
-//    //
-//    g.push_back(std::make_pair(graph_type(),47));
-//    add_edge(0, 1,g.back().first);
-//    add_edge(0, 2,g.back().first);
-//    add_edge(0, 3,g.back().first);
-//    add_edge(0, 4,g.back().first);
-//    add_edge(1, 5,g.back().first);
-//    add_edge(2, 6,g.back().first);
-//    add_edge(3, 7,g.back().first);
-//    add_edge(4, 8,g.back().first);
-//
-//	// graph no. 32340
-//	// 14	15	1	64272
-//    g.push_back(std::make_pair(graph_type(15),64272));
-//    add_edge(13, 14,g.back().first);
-//    add_edge(12, 14,g.back().first);
-//    add_edge( 9, 14,g.back().first);
-//    add_edge( 7, 14,g.back().first);
-//    add_edge( 8, 13,g.back().first);
-//    add_edge( 1, 13,g.back().first);
-//    add_edge( 6, 12,g.back().first);
-//    add_edge( 0, 12,g.back().first);
-//    add_edge(10,  9,g.back().first);
-//    add_edge( 2,  7,g.back().first);
-//    add_edge(11,  8,g.back().first);
-//    add_edge( 3,  6,g.back().first);
-//    add_edge( 4, 10,g.back().first);
-//    add_edge( 5, 11,g.back().first);
 
     int success = 0;
     for(std::vector<std::pair<graph_type, lc_type> >::iterator it= g.begin(); it != g.end(); ++it)
