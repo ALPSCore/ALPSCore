065775fb0521fecc897ccbb6913fee1e1bae2787
wistaria <wistaria@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2012-07-20 15:39:57 +0000

    Fixed std::copy lookup

--- ngs/hdf5/matrix.hpp
+++ ngs/hdf5/matrix.hpp
@@ -212,6 +212,7 @@ namespace detail {
         , std::vector<std::size_t> chunk  = std::vector<std::size_t>()
         , std::vector<std::size_t> offset = std::vector<std::size_t>()
     ){
+        using std::copy;
         if(ar.is_data(path + "/size1") && ar.is_scalar(path + "/size1") && ar.is_datatype<std::size_t>(path + "/size1")) {
             // Old matrix hdf5 format
             std::size_t size1(0), size2(0), reserved_size1(0);
