43c694410df8c25b700e24131236caf4ca63bc84
dolfim <dolfim@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2012-08-30 12:24:49 +0000

    fixing problems with alps::numeric::real

--- numeric/real.hpp
+++ numeric/real.hpp
@@ -40,13 +40,7 @@
 
 namespace alps { namespace numeric {
 
-template <class T>
-inline T real(T x) { return x; }
-
-using std::real;
-
-// if std::complex<T> is used std::real will be called by argument dependent look-up
-
+// *********************************************************** REAL_TYPE TRAITS
 template <class T>
 struct real_type
 {
@@ -61,29 +55,109 @@ struct real_type<std::complex<T> >
 };
 
 template <class T>
-inline std::vector<T> real(std::vector<std::complex<T> > const & x) 
+struct real_type<std::vector<T> >
 {
-  std::vector<T> re;
-  re.reserve(x.size());
-#if defined(__GLIBCPP__) || defined(__GLIBCXX__)
-  std::transform(x.begin(),x.end(),std::back_inserter(re),
-                 static_cast<T const & (*)(std::complex<T> const &)>(&real));
-#else
-  std::transform(x.begin(),x.end(),std::back_inserter(re),
-                 static_cast<T (*)(std::complex<T> const &)>(&real));
-#endif
-  return re;
-}
+    typedef std::vector<typename real_type<T>::type> type;
+};
+
+
+
+// ************************************************************** REAL FUNCTION
+
+template <class T> inline typename real_type<T>::type real(T );
+
+namespace detail {
+    
+    // generic hook
+    template <typename T>
+    struct real_hook;
+
+    // generic impl
+    template <typename T, bool>
+    struct real_impl;
+
+    // overload for fundamental
+    template <typename T>
+    struct real_impl<T, true>{
+        static inline T apply(T x)
+        {
+            return x;
+        }
+    };
+
+    // forward to hooks
+    template <typename T>
+    struct real_impl<T, false>{
+        static inline typename real_type<T>::type apply(T const& x)
+        {
+            return real_hook<T>::apply(x);
+        }
+    };
+
+
+
+    // overload for complex of fundamental
+    template <typename T>
+    struct real_hook<std::complex<T> >{
+        static inline T apply(std::complex<T> const& x)
+        {
+            BOOST_STATIC_ASSERT((boost::is_fundamental<T>::value));
+            return std::real(x);
+        }
+    };
+
+    // overload for std::vector
+    template <typename T>
+    struct real_hook<std::vector<T> >{
+        static inline std::vector<T> const& apply(std::vector<T> const& x)
+        {
+            BOOST_STATIC_ASSERT((boost::is_fundamental<T>::value));
+            return x;
+        }
+    };
+    template <typename T>
+    struct real_hook<std::vector<std::complex<T> > >{
+        static inline std::vector<T> apply(std::vector<std::complex<T> > const& x)
+        {
+              std::vector<T> re;
+              re.reserve(x.size());
+              std::transform(x.begin(),x.end(),std::back_inserter(re),
+                             static_cast<T (*)(std::complex<T> const &)>(&real_hook<std::complex<T> >::apply));
+              return re;
+        }
+    };
+    
+    // overload for std::vector<std::vector>
+    template <typename T>
+    struct real_hook<std::vector<std::vector<T> > >{
+        static inline std::vector<std::vector<T> > const& apply(std::vector<std::vector<T> > const& x)
+        {
+            BOOST_STATIC_ASSERT((boost::is_fundamental<T>::value));
+            return x;
+        }
+    };
+    template <typename T>
+    struct real_hook<std::vector<std::vector<std::complex<T> > > >{
+        static inline std::vector<std::vector<T> > apply(std::vector<std::vector<std::complex<T> > > const& x)
+        {
+            std::vector<std::vector<T> > re;
+            re.reserve(x.size());
+            std::transform(x.begin(),x.end(),std::back_inserter(re),
+                           static_cast<std::vector<T> (*)(std::vector<std::complex<T> > const &)>(&real_hook<std::vector<std::complex<T> > >::apply));
+            return re;
+        }
+    };
+    
+} // end namespace detail
+
 
 template <class T>
-std::vector<std::vector<T> > real(std::vector<std::vector<std::complex<T> > >const & x) 
+inline typename real_type<T>::type real(T x)
 {
-  std::vector<std::vector<T> > re;
-  re.reserve(x.size());
-  std::transform(x.begin(),x.end(),std::back_inserter(re),
-                 static_cast<std::vector<T> (*)(std::vector<std::complex<T> > const &)>(&real));
-  return re;
+    return detail::real_impl<T, boost::is_fundamental<T>::value>::apply(x);
 }
+
+
 
 } } // end namespace alps::numeric
 
