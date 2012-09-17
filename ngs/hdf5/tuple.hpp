0712e1321315604815208d168e1b42891c2c93a3
gamperl <gamperl@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2012-09-17 13:42:23 +0000

    - fixed typo

--- ngs/hdf5/tuple.hpp
+++ ngs/hdf5/tuple.hpp
@@ -94,7 +94,7 @@ namespace alps {
         template <
             typename T0, typename T1, typename T2, typename T3, typename T4, typename T5, typename T6, typename T7, typename T8, typename T9
         > void save(
-              ARCHIVE & ar
+              archive & ar
             , std::string const & path
             , boost::tuple<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9> const & value
             , std::vector<std::size_t> size = std::vector<std::size_t>()
