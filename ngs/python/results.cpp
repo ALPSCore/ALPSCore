66b7163a95e4d2dc0165aaaf69776f0cd54ac9ea
gamperl <gamperl@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2012-11-01 12:29:51 +0000

    made ngs examples simpler

--- ngs/python/results.cpp
+++ ngs/python/results.cpp
@@ -37,23 +37,28 @@
 namespace alps {
     namespace detail {
 
+        std::string mcresults_print(alps::mcresults & self) {
+            std::stringstream sstr;
+            sstr << self;
+            return sstr.str();
+        }
+
         void mcresults_load(alps::mcresults & self, alps::hdf5::archive & ar, std::string const & path) {
             std::string current = ar.get_context();
             ar.set_context(path);
             self.load(ar);
             ar.set_context(current);
         }
-
     }
 }
 
 BOOST_PYTHON_MODULE(pyngsresults_c) {
-
     boost::python::class_<alps::mcresults>(
         "results",
         boost::python::no_init
     )
         .def(boost::python::map_indexing_suite<alps::mcresults>())
+        .def("__str__", &alps::detail::mcresults_print)
         .def("save", &alps::mcresults::save)
         .def("load", &alps::detail::mcresults_load)
     ;
