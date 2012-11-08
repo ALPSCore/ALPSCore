7a90b1816d3bb13f1061913b1767a68bf3ee4f9b
gukel <gukel@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2012-11-08 16:43:08 +0000

    construction and assignment from boost array, subarray, array_view, etc.

--- multi_array/multi_array.hpp
+++ multi_array/multi_array.hpp
@@ -33,76 +33,93 @@
 
 namespace alps{
 
-  template <class T,std::size_t D, class Allocator = std::allocator<T> >
-  class multi_array : public boost::multi_array<T,D,Allocator>
-  {
-    typedef boost::multi_array<T,D,Allocator> base_type;
-    typedef multi_array<T,D,Allocator> array_type;
-
-  public:
-
-    multi_array(std::size_t N, std::size_t M, std::size_t K, std::size_t J, std::size_t I) : base_type(boost::extents[N][M][K][J][I]) {}
-    multi_array(std::size_t N, std::size_t M, std::size_t K, std::size_t J) : base_type(boost::extents[N][M][K][J]) {}
-    multi_array(std::size_t N, std::size_t M, std::size_t K) : base_type(boost::extents[N][M][K]) {}
-    multi_array(std::size_t N, std::size_t M) : base_type(boost::extents[N][M]) {}
-    multi_array(std::size_t N) : base_type(boost::extents[N]) {}
-
-    multi_array(const boost::detail::multi_array::extent_gen<D>& ext) : base_type(ext) {}
-
-    multi_array() : base_type() {}
-
-    array_type& operator=(const array_type& a)
+    template <class T, std::size_t D, class Allocator = std::allocator<T> >
+    class multi_array : public boost::multi_array<T,D,Allocator>
     {
-      if(this != &a){
-      	std::vector<std::size_t> ext(a.shape(),a.shape()+a.num_dimensions());
-      	(*this).resize(ext);
-      	base_type::operator=(a);
-      }
+        typedef boost::multi_array<T,D,Allocator> base_type;
+        typedef multi_array<T,D,Allocator> array_type;
 
-      return *this;
-    }
+    public:
+        typedef typename base_type::size_type size_type;
 
-    array_type& operator+=(const array_type& a)
-    {
-      assert(std::equal(this->shape(),this->shape()+D,a.shape()));
-      std::transform((*this).data(),(*this).data()+(*this).num_elements(),a.data(),(*this).data(),std::plus<T>());
-      return *this;
-    }
+        multi_array(size_type N, size_type M, size_type K, size_type J, size_type I) : base_type(boost::extents[N][M][K][J][I]) {}
+        multi_array(size_type N, size_type M, size_type K, size_type J) : base_type(boost::extents[N][M][K][J]) {}
+        multi_array(size_type N, size_type M, size_type K) : base_type(boost::extents[N][M][K]) {}
+        multi_array(size_type N, size_type M) : base_type(boost::extents[N][M]) {}
+        multi_array(size_type N) : base_type(boost::extents[N]) {}
 
-    array_type& operator-=(const array_type& a)
-    {
-      assert(std::equal(this->shape(),this->shape()+D,a.shape()));
-      std::transform((*this).data(),(*this).data()+(*this).num_elements(),a.data(),(*this).data(),std::minus<T>());
-      return *this;
-    }
+        multi_array(const boost::detail::multi_array::extent_gen<D>& ext) : base_type(ext) {}
 
-    array_type& operator*=(const array_type& a)
-    {
-      assert(std::equal(this->shape(),this->shape()+D,a.shape()));
-      std::transform((*this).data(),(*this).data()+(*this).num_elements(),a.data(),(*this).data(),std::multiplies<T>());
-      return *this;
-    }
+        multi_array() : base_type() {}
 
-    array_type& operator*=(const T s)
-    {
-      std::transform((*this).data(),(*this).data()+(*this).num_elements(),(*this).data(),std::bind2nd(std::multiplies<T>(),s));
-      return *this;
-    }
+        template<typename Array>
+        multi_array(const Array& x) : base_type(x) {};
 
-    array_type& operator/=(const array_type& a)
-    {
-      assert(std::equal(this->shape(),this->shape()+D,a.shape()));
-      std::transform((*this).data(),(*this).data()+(*this).num_elements(),a.data(),(*this).data(),std::divides<T>());
-      return *this;
-    }
 
-    array_type& operator/=(const T s)
-    {
-      std::transform((*this).data(),(*this).data()+(*this).num_elements(),(*this).data(),std::bind2nd(std::divides<T>(),s));
-      return *this;
-    }
-    
-  };//class multi_array
+        array_type& operator=(const array_type& a)
+        {
+            if(this != &a)
+            {
+                const size_type* shp = a.shape();
+                std::vector<size_type> ext(shp,shp+a.num_dimensions());
+                (*this).resize(ext);
+                base_type::operator=(a);
+            }
+
+            return *this;
+        }
+
+        template <typename Array>
+        multi_array& operator=(const Array& a) 
+        {
+            const typename Array::size_type* shp = a.shape();
+            std::vector<size_type> ext(shp,shp+a.num_dimensions());
+            (*this).resize(ext);
+            base_type::operator=(a);
+            return *this;
+        }
+
+        array_type& operator+=(const array_type& a)
+        {
+            assert(std::equal(this->shape(),this->shape()+D,a.shape()));
+            std::transform((*this).data(),(*this).data()+(*this).num_elements(),a.data(),(*this).data(),std::plus<T>());
+            return *this;
+        }
+
+        array_type& operator-=(const array_type& a)
+        {
+            assert(std::equal(this->shape(),this->shape()+D,a.shape()));
+            std::transform((*this).data(),(*this).data()+(*this).num_elements(),a.data(),(*this).data(),std::minus<T>());
+            return *this;
+        }
+
+        array_type& operator*=(const array_type& a)
+        {
+            assert(std::equal(this->shape(),this->shape()+D,a.shape()));
+            std::transform((*this).data(),(*this).data()+(*this).num_elements(),a.data(),(*this).data(),std::multiplies<T>());
+            return *this;
+        }
+
+        array_type& operator*=(const T s)
+        {
+            std::transform((*this).data(),(*this).data()+(*this).num_elements(),(*this).data(),std::bind2nd(std::multiplies<T>(),s));
+            return *this;
+        }
+
+        array_type& operator/=(const array_type& a)
+        {
+            assert(std::equal(this->shape(),this->shape()+D,a.shape()));
+            std::transform((*this).data(),(*this).data()+(*this).num_elements(),a.data(),(*this).data(),std::divides<T>());
+            return *this;
+        }
+
+        array_type& operator/=(const T s)
+        {
+            std::transform((*this).data(),(*this).data()+(*this).num_elements(),(*this).data(),std::bind2nd(std::divides<T>(),s));
+            return *this;
+        }
+
+    };//class multi_array
 
 }//namespace alps
 
