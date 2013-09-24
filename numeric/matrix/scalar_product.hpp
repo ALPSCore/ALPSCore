71350cdf7b9da4f01141c01cbc6a48abe086d680
hehn <hehn@3665984a-f8b0-4bb5-b30c-cdcaecbe7ced>
2013-09-24 12:30:11 +0000

    adding clang bug workaround to the scalar_product

--- numeric/matrix/scalar_product.hpp
+++ numeric/matrix/scalar_product.hpp
@@ -31,6 +31,7 @@
 #include <alps/numeric/matrix/entity.hpp>
 #include <alps/numeric/matrix/is_blas_dispatchable.hpp>
 #include <alps/numeric/matrix/detail/auto_deduce_multiply_return_type.hpp>
+#include <alps/type_traits/element_type.hpp>
 #include <alps/functional.h>
 #include <boost/utility/enable_if.hpp>
 #include <boost/mpl/and.hpp>
@@ -44,11 +45,23 @@ namespace numeric {
 
 
 template <typename T1, typename T2>
-struct scalar_product_return_type : detail::auto_deduce_multiply_return_type<typename T1::value_type,typename T2::value_type>
+struct scalar_product_return_type : detail::auto_deduce_multiply_return_type<typename element_type<T1>::type,typename element_type<T2>::type>
 {
 };
 
 
+#if defined(__clang_major__) && __clang_major__ < 3 || (__clang_major__ == 3 && __clang_minor__ == 0) || defined(BOOST_MSVC)
+// Workaround for a compiler bug in clang 3.0 (and maybe earlier versions)
+template <typename T1, typename T2>
+typename scalar_product_return_type<T1,T2>::type scalar_product_impl(T1 const& t1, T2 const& t2, boost::mpl::false_)
+{
+    using alps::numeric::conj;
+    typename scalar_product_return_type<T1,T2>::type r(0);
+    for(std::size_t i=0; i < t1.size(); ++i)
+        r += conj(t1[i]) * t2[i];
+    return r;
+}
+#else // defined(__clang_major__) && __clang_major__ < 3 || (__clang_major__ == 3 && __clang_minor__ == 0)
 template <typename T1, typename T2>
 typename scalar_product_return_type<T1,T2>::type scalar_product_impl(T1 const& t1, T2 const& t2, boost::mpl::false_)
 {
@@ -58,6 +71,7 @@ typename scalar_product_return_type<T1,T2>::type scalar_product_impl(T1 const& t
         , conj_mult<typename T1::value_type, typename T2::value_type>()
     );
 }
+#endif // defined(__clang_major__) && __clang_major__ < 3 || (__clang_major__ == 3 && __clang_minor__ == 0)
 
 template <typename T1, typename T2>
 typename scalar_product_return_type<T1,T2>::type scalar_product_impl(T1 const& t1, T2 const& t2, boost::mpl::true_)
