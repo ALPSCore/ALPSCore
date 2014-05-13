54b5652b08375f310ad319416138d3c4ccf29592
Andrey Antipov <Andrey.E.Antipov@gmail.com>
2014-05-13 13:14:30 -0400

    added build jenkins script

---
+++ utility/build/build.jenkins.sh
@@ -0,0 +1,24 @@
+#guarantees we are in build directory
+BASEDIR=$(dirname $0)
+cd $BASEDIR
+# now we are in the module directory
+cd ..
+
+MODULEDIR=`pwd`
+TARGET=${MODULEDIR}/target
+mkdir ${TARGET}
+cd ${TARGET} 
+
+echo "Module is in ${MODULEDIR}"
+echo "Building in ${TARGET}"
+
+cmake \
+-DCMAKE_INSTALL_PREFIX="./INSTALL_DIR" \
+-DTesting=ON \
+-DCMAKE_BUILD_TYPE=Release \
+${MODULEDIR}
+
+make 
+make test
+
+echo "all done."
