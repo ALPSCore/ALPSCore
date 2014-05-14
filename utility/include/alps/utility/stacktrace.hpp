5e225c2c8b7e23904c0ddade80fe38503b311c46
Andrey Antipov <Andrey.E.Antipov@gmail.com>
2014-05-14 09:42:42 -0400

    moved stacktrace to utility

--- utility/include/alps/utility/stacktrace.hpp
+++ utility/include/alps/utility/stacktrace.hpp
@@ -33,6 +33,27 @@
 
 #include <string>
 
+// maximal number of stack frames displayed in stacktrace. Default 63
+#ifndef ALPS_NGS_MAX_FRAMES
+    #define ALPS_NGS_MAX_FRAMES 63
+#endif
+
+// prevent the signal object from registering signals
+#ifdef BOOST_MSVC
+    #define ALPS_NGS_NO_SIGNALS
+#endif
+
+// do not print a stacktrace in error messages
+#ifndef __GNUC__
+    #define ALPS_NGS_NO_STACKTRACE
+#endif
+
+// TODO: have_python
+// TODO: have_mpi
+// TODO: have_thread
+
+
+
 // TODO: check for gcc and use __PRETTY_FUNCTION__
 
 #define ALPS_STACKTRACE (                                                          \
