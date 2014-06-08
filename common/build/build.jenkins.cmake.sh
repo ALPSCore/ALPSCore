2881df9bae5d86a49b45845f6d18546f8d0599f6
Andrey Antipov <Andrey.E.Antipov@gmail.com>
2014-06-08 12:31:01 -0400

    jenkins build script with a global cmake

---
+++ common/build/build.jenkins.cmake.sh
@@ -0,0 +1,50 @@
+# Script used by the build server the build
+# all modules of ALPSCore
+
+# This script expects the following environment variables
+# BOOST_ROOT - location for boost distribution
+# HDF5_ROOT - location for the HDF5 distribution
+# MPI_CXX_COMPILER - location for the mpi c++ compile
+
+# Make sure we are in top directory for the repository
+SCRIPTDIR=$(dirname $0)
+cd $SCRIPTDIR/../..
+
+if [[ -z "$BOOST_ROOT" ]]
+then
+  BOOST_SYSTEM=NO
+else
+  BOOST_SYSTEM=YES
+fi
+
+echo "Using BOOST at $BOOST_ROOT - no system path $BOOST_SYSTEM"
+
+if [[ -z "$HDF5_ROOT" ]]
+then
+  echo
+else
+  export DYLD_LIBRARY_PATH=$BOOST_ROOT/lib:$HDF5_ROOT/lib:$DYLD_LIBRARY_PATH
+fi
+
+INSTALLDIR=$PWD/install
+BUILDDIR=$PWD/build.tmp
+ROOTDIR=$PWD
+
+rm -rf $BUILDDIR
+mkdir $BUILDDIR
+cd $BUILDDIR
+
+cmake \
+  -DCMAKE_INSTALL_PREFIX="${INSTALLDIR}" \
+  -DTesting=ON \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBOOST_ROOT="${BOOST_ROOT}" \
+  -DBoost_NO_SYSTEM_PATHS="${BOOST_SYSTEM}" \
+  -DTestXMLOutput=TRUE \
+  ${ROOTDIR}
+
+make || exit 1 
+make test
+make install || exit 1
+
+echo "*** Done ***"
