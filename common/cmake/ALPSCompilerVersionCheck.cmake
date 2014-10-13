9854d4b46aa544283fe24b2e6641d0bfcacda929
Alexander <galexvg@gmail.com>
2014-10-13 15:07:11 -0400

    Provisional workaround for undefined CMAKE_*_COMPILER_VERSION
    The variables `CMAKE_*_COMPILER_VERSION` and `CMAKE_*_COMPILER_ID`
    first appear in 2.8.8 (marked as "internal, subject to change");
    officially supported since 2.8.10. For now, let's just issue a status
    message for older CMake, and a warning for newer ones.

--- common/cmake/ALPSCompilerVersionCheck.cmake
+++ common/cmake/ALPSCompilerVersionCheck.cmake
@@ -1,16 +1,25 @@
 #
-# Check for minimum compiler version and throw error if we either know the compiler doesn't work or can't test for it.
+# Check for minimum compiler version and throw error if we know the compiler doesn't work
+# Warn if we can't test for it.
 #
-if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "4.2") #we can probably support older versions but nobody has any left to test
-    message(FATAL_ERROR "Insufficient gcc version")
+if ((DEFINED CMAKE_CXX_COMPILER_ID) AND (DEFINED CMAKE_CXX_COMPILER_VERSION))
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+    if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "4.2") #we can probably support older versions but nobody has any left to test
+      message(FATAL_ERROR "Insufficient gcc version")
+    endif()
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
+    if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "10.0")
+      message(FATAL_ERROR "Insufficient Intel compiler version")
+    endif()
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+    if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "3.2")
+      message(FATAL_ERROR "Insufficient Clang compiler version")
+    endif()
   endif()
-elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
-  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "10.0")
-    message(FATAL_ERROR "Insufficient Intel compiler version")
-  endif()
-elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-  if(CMAKE_CXX_COMPILER_VERSION VERSION_LESS "3.2")
-    message(FATAL_ERROR "Insufficient Clang compiler version")
+else()
+  if (CMAKE_VERSION VERSION_LESS "2.8.8") # no CMAKE_*_COMPILER_VERSION anyway
+    message(STATUS "Unable to determine C++ compiler version")
+  else()
+    message(WARNING "Unknown C++ compiler version: might not be supported")
   endif()
 endif()
