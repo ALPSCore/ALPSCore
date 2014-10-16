9cfbb33896aebe94d1668884fe5012aa054ecf3a
Alexander <galexvg@gmail.com>
2014-10-15 20:53:50 -0400

    A separate build script to build documentation.
    common/build/build.jenkins.sh does not build docs now.
    common/build/build-doc.jenkins.sh builds only docs (at install/doc/)

--- common/build/build-doc.jenkins.sh
+++ common/build/build-doc.jenkins.sh
@@ -10,18 +10,10 @@
 SCRIPTDIR=$(dirname $0)
 cd $SCRIPTDIR/../..
 
-if [[ -z "$BOOST_ROOT" ]]
-then
-  BOOST_SYSTEM=NO
-else
-  BOOST_SYSTEM=YES
-fi
-
-echo "Using BOOST at $BOOST_ROOT - no system path $BOOST_SYSTEM"
-
 INSTALLDIR=$PWD/install
-BUILDDIR=$PWD/build.tmp
+BUILDDIR=$PWD/build-doc.tmp
 ROOTDIR=$PWD
+DOCDIR=$INSTALLDIR/doc
 
 mkdir $INSTALLDIR 2>/dev/null
 mkdir $DOCDIR 2>/dev/null
@@ -32,18 +24,10 @@ cd $BUILDDIR
 
 cmake \
   -DCMAKE_INSTALL_PREFIX="${INSTALLDIR}" \
-  -DTesting=ON \
-  -DCMAKE_BUILD_TYPE=Release \
-  -DBOOST_ROOT="${BOOST_ROOT}" \
-  -DGTEST_ROOT="${GTEST_ROOT}" \
-  -DBoost_NO_SYSTEM_PATHS="${BOOST_SYSTEM}" \
-  -DENABLE_MPI=TRUE \
-  -DTestXMLOutput=TRUE \
-  -DDocumentation=OFF \
+  -DDocumentationOnly=ON \
+  -DDOXYFILE_OUTPUT_DIR="${DOCDIR}" \
   ${ROOTDIR}
 
-make || exit 1 
-make test
-make install || exit 1
+make doc || exit 1
 
 echo "*** Done ***"
