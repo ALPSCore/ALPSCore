b1ea0cd294dc05e391c80ffee063ad01712f0300
Lukas Gamper <gamperl@gmail.com>
2014-11-24 16:07:38 +0100

    improved tests

--- accumulators/test/mult_by_constant.cpp
+++ accumulators/test/mult_by_constant.cpp
@@ -8,27 +8,33 @@
 #include <alps/accumulators/accumulator.hpp>
 #include "gtest/gtest.h"
 
-TEST(accumulators, TestingMultiplyAccumulatorByConstant) {
-
-	alps::accumulators::accumulator_set measurements;
-	measurements << alps::accumulators::MeanAccumulator<double>("meanDouble")
-		<< alps::accumulators::NoBinningAccumulator<double>("noBinningDouble")
-		<< alps::accumulators::LogBinningAccumulator<double>("logBinningDouble")
-		<< alps::accumulators::FullBinningAccumulator<double>("fullBinningDouble");
-
+void test_mult_by_constant_scalar(alps::accumulators::accumulator_set & measurements, std::string name) {
 	for (int i = 0; i < 1000; ++i)
-		measurements["fullBinningDouble"] << 2. * (i % 2);
+		measurements[name] << 2. * (i % 2);
 
 	std::cout << measurements << std::endl;
 
 	alps::accumulators::result_set results(measurements);
 
-	std::cout << results["fullBinningDouble"] * 2 << std::endl;
-	std::cout << 2 * results["fullBinningDouble"] << std::endl;
+	std::cout << results[name] * 2 << std::endl;
+	std::cout << 2 * results[name] << std::endl;
 
-	std::cout << results["fullBinningDouble"] / 2 << std::endl;
-	std::cout << 2 / results["fullBinningDouble"] << std::endl;
+	std::cout << results[name] / 2 << std::endl;
+	std::cout << 2 / results[name] << std::endl;
 }
+
+#define ALPS_TEST_RUN_MUL_CONST_TEST(type, name)									\
+	TEST(accumulators, mult_by_constant_scalar_ ## name) {							\
+		alps::accumulators::accumulator_set measurements;							\
+		measurements << alps::accumulators:: type <double>( #name );				\
+		test_mult_by_constant_scalar(measurements, #name );							\
+	}
+
+ALPS_TEST_RUN_MUL_CONST_TEST(MeanAccumulator, meanDouble)
+ALPS_TEST_RUN_MUL_CONST_TEST(NoBinningAccumulator, noBinningDouble)
+ALPS_TEST_RUN_MUL_CONST_TEST(LogBinningAccumulator, logBinningDouble)
+ALPS_TEST_RUN_MUL_CONST_TEST(FullBinningAccumulator, fullBinningDouble)
+#undef ALPS_TEST_RUN_MUL_CONST_TEST
 
 int main(int argc, char **argv) 
 {
