0e8e4123baf5b821ba50088a0b7504d2632430e7
Alexander <galexv@umich.edu>
2015-03-09 19:13:40 -0400

    New utility function alps::remove_extensions() to strip extensions from a filename.

---
+++ utilities/include/alps/utilities/remove_extensions.hpp
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 1998-2015 ALPS Collaboration. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ * For use in publications, see ACKNOWLEDGE.TXT
+ */
+
+#ifndef ALPS_UTILITY_REMOVE_EXTENSIONS_HPP
+#define ALPS_UTILITY_REMOVE_EXTENSIONS_HPP
+
+#include <alps/config.hpp>
+#include <string>
+
+namespace alps {
+  /** @brief Returns the file name stem with ALL extensions removed
+      @param filename : file name (e.g., /a/b/c.ver1.txt.gz)
+      @returns File name stem (e.g., /a/b/c)
+  */
+  ALPS_DECL std::string remove_extensions(const std::string& filename);
+}
+#endif // ALPS_UTILITY_REMOVE_EXTENSIONS_HPP
---
+++ utilities/src/remove_extensions.cpp
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 1998-2015 ALPS Collaboration. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ * For use in publications, see ACKNOWLEDGE.TXT
+ */
+
+
+#include <alps/utilities/remove_extensions.hpp>
+#include <boost/filesystem.hpp>
+
+namespace alps {
+  std::string remove_extensions(const std::string& filename)
+  {
+      boost::filesystem::path fp=filename;
+      while (!fp.extension().empty())  fp=fp.stem();
+      return fp.native(); // FIXME: should it be .native() or just .string() ?
+  }
+}
