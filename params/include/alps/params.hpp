ALPSCore/ALPSCore

commit 72854f57285e157503cdf5088de12e9538705149

The parameters are parsed before serialization.

params/include/alps/params.hpp
@@ -314,6 +314,7 @@ namespace alps {
           /// Interface to serialization
           template<class Archive> void serialize(Archive & ar, const unsigned int)
           {
+              possibly_parse();
               ar  & is_valid_
                   & archname_
                   & optmap_

commit f973589efdac1a1b3d8ada7721e3fb6cbf4ea0a4

Fixed a bug introduced by commit 72854f57.
The parameters are parsed before saving and before broadcast on root side.

params/include/alps/params.hpp
@@ -314,7 +314,6 @@ namespace alps {
           /// Interface to serialization
           template<class Archive> void serialize(Archive & ar, const unsigned int)
           {
-              possibly_parse();
               ar  & is_valid_
                   & archname_
                   & optmap_

params/src/params.cpp
@@ -200,6 +200,7 @@ namespace alps {
 
         void params::save(hdf5::archive& ar, const std::string& path) const
         {
+            possibly_parse();
             std::string context = ar.get_context();
             ar.set_context(path);
             save(ar);
@@ -240,6 +241,7 @@ namespace alps {
 #ifdef ALPS_HAVE_MPI
         void params::broadcast(boost::mpi::communicator const & comm, int root)
         {
+            if (comm.rank()==root) possibly_parse();
             boost::mpi::broadcast(comm, *this, root);
         }
 #endif