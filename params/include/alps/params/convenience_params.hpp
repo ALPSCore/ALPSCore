68695d3c9bfcb31e258b085654d791c95257513d
Alexander <galexv@umich.edu>
2015-04-04 17:59:26 -0400

    Simplify the convenience function to define common parameters.

--- params/include/alps/params/convenience_params.hpp
+++ params/include/alps/params/convenience_params.hpp
@@ -2,24 +2,22 @@
 #define ALPS_CONV_PARAMS_INCLUDED
 
 #include "alps/params.hpp"
+#include "alps/utilities/remove_extensions.hpp"
 
 namespace alps {
+    /// @brief Defines a number of frequently-used parameters.
+    /// Defines `size_t timelimt`, `string outputfile`, `string checkpoint`.
     params& define_convenience_parameters(params & parameters) {
+        std::string basename=alps::remove_extensions(parameters.get_origin_name());
         parameters
-            // .define<std::string>("continue", "", "load simulation from the given checkpoint")
             .define<std::size_t>("timelimit", 0, "time limit for the simulation")
-            .define<std::string>("outputfile", "*.out.h5", "name of the output file")
-            .define<std::string>("checkpoint", "*.clone.h5", "name of the checkpoint file to save to")
+            .define<std::string>("outputfile", basename+".out.h5", "name of the output file")
+            .define<std::string>("checkpoint", basename+".clone.h5", "name of the checkpoint file to save to")
         ;
-        // FIXME: this is a hack. I need a method to see if a parameter is actually supplied.
-        if (parameters["outputfile"].as<std::string>()[0]=='*') {
-            parameters["outputfile"]=alps::remove_extensions(parameters.get_origin_name())+".out.h5";
-        }
-        // FIXME: this is a hack. I need a method to see if a parameter is actually supplied.
-        if (parameters["checkpoint"].as<std::string>()[0]=='*') {
-                parameters["checkpoint"]=alps::remove_extensions(parameters.get_origin_name())+".clone.h5";
-            }
-            return parameters;
+        // FIXME: we might want to base checkpoint name on the output file name rather than origin name
+        //        (especially if there is no origin name: e.g. everything is passed via the command line
+        //         and argv[0] is wrong/unavailable)
+        return parameters;
     }
 } // end of namespace alps
 #endif
