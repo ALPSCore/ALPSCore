c4d9bc834ae6835df5ca861023aebe95b8f0454c
Alexander <galexv@umich.edu>
2015-05-01 13:55:49 -0400

    Placeholders for alps::params examples/tutorials.

---
+++ tutorials/params/example_3/params_example3.cpp
@@ -0,0 +1,14 @@
+/** @file params_example.cpp
+    @brief alps::params example 3
+*/
+
+#include <iostream>
+#include "alps/params.hpp"
+
+int main(int argc, const char* argv[])
+{
+  alps::params par(argc,argv);
+
+  std::cout << par;
+  return 0;
+}
