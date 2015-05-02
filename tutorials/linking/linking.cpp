23d862800b1aecd02fe974ea9904dd1bc0343cf6
Emanuel Gull <egull@umich.edu>
2015-05-02 14:04:26 -0400

    tutorial for linking

---
+++ tutorials/linking/linking.cpp
@@ -0,0 +1,19 @@
+/** @file linking.cpp
+*/
+
+#include <iostream>
+
+//this include file will require knowledge of the ALPSCore and boost headers
+#include "alps/params.hpp"
+
+/**
+ * This example shows how to link to ALPSCore
+ * 
+ */
+
+int main()
+{
+    // Creates an instance of the parameter class. This will require linking to the param library
+    alps::params par;
+    
+}
