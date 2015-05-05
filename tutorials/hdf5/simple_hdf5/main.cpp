41bee4ee1f60bf0599d674f8288ef0694dd7ecd7
Gabriele Carcassi <gabriele.carcassi@gmail.com>
2015-05-05 10:28:09 -0400

    hdf5 tutorial: cleanup scalar/vector example

--- tutorials/hdf5/simple_hdf5/main.cpp
+++ tutorials/hdf5/simple_hdf5/main.cpp
@@ -19,7 +19,13 @@ int main(int argc, const char* argv[])
     std::string filename("measurements.h5");
     
     // Sample data to write
+    // We are going to use different data types to show that the library can
+    // handle them. For a full list of type supported consult the reference
+    // documentation (Doxygen).
+    double sDoubleSample = 6.28;
+    int sIntSample = 500;
     std::complex<double> sComplexSample(1.0, -1.0);
+    std::string sStringSample("ALPSCore");
     std::vector<double> vDoubleSample(10, 0);
     for (int i = 0; i < vDoubleSample.size(); i++) {
         vDoubleSample[i] = i / 2.0;
@@ -33,12 +39,15 @@ int main(int argc, const char* argv[])
     std::cout << "Opening parameters.h5..." << std::endl;
     alps::hdf5::archive oar(filename, "w");
     
-    // Write some data
-    std::cout << "Write some data..." << std::endl;
-    oar["/scalars/double"] << 6.28;
-    oar["/scalars/int"] << 500;
+    // We write some data 
+    // Each value is written a path, which corresponds to the location where
+    // the value is stored in the hdf5 hierarchy within the file. Note how
+    // the syntax is the same regardless of the data being saved.
+    std::cout << "Writing data..." << std::endl;
+    oar["/scalars/double"] << sDoubleSample;
+    oar["/scalars/int"] << sIntSample;
     oar["/scalars/complex"] << sComplexSample;
-    oar["/scalars/string"] << filename;
+    oar["/scalars/string"] << sStringSample;
     oar["/vectors/double"] << vDoubleSample;
     oar["/vectors/int"] << vIntSample;
     
@@ -46,22 +55,33 @@ int main(int argc, const char* argv[])
     std::cout << "Closing parameters.h5..." << std::endl;
     oar.close();
 
+    // Declare variables for reading the data
+    // We need a placeholder to load the data. The type does not need to match
+    // the original as long as it can be converted. E.g. int -> double,
+    // real -> complex. Consult the reference documentation (Doxygen) for
+    // a full list of supported conversions.
+    double sDoubleValue;
+    int sIntValue;
+    double sIntConvertedValue;
+    std::complex<double> sComplexValue;
+    std::string sStringValue;
+    std::vector<double> vDoubleValue;
+    std::vector<double> vIntValue;
+
     // Open the hdf5 file with read permission
     std::cout << "Opening parameters.h5..." << std::endl;
     alps::hdf5::archive iar(filename, "r");
     
     // Read the data back
+    // Note that the data does not have to be read in the same order it was.
+    // written. We do it here as it is easier to maintain.
     std::cout << "Reading the data:" << std::endl;
-    double dValue;
-    int iValue;
-    std::complex<double> sComplexValue;
-    std::string sStringValue;
-    std::vector<double> vDoubleValue;
-    std::vector<double> vIntValue;
-    iar["/scalars/double"] >> dValue;
-    std::cout << "/scalars/double: " << dValue << std::endl;
-    iar["/scalars/int"] >> iValue;
-    std::cout << "/scalars/int: " << iValue << std::endl;
+    iar["/scalars/double"] >> sDoubleValue;
+    std::cout << "/scalars/double: " << sDoubleValue << std::endl;
+    iar["/scalars/int"] >> sIntValue;
+    std::cout << "/scalars/int: " << sIntValue << std::endl;
+    iar["/scalars/int"] >> sIntConvertedValue;
+    std::cout << "/scalars/int (as double): " << sIntConvertedValue << std::endl;
     iar["/scalars/complex"] >> sComplexValue;
     std::cout << "/scalars/complex: " << sComplexValue << std::endl;
     iar["/scalars/string"] >> sStringValue;
