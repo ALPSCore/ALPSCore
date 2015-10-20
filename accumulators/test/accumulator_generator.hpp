a327058cb76666c59ad358fab371232496e09864
Alexander <galexv@umich.edu>
2015-10-20 13:16:17 -0400

    Additions to accumulators test harness.

--- accumulators/test/accumulator_generator.hpp
+++ accumulators/test/accumulator_generator.hpp
@@ -35,32 +35,87 @@ namespace alps {
                 EXPECT_NEAR(expected, actual, tol) << "Values of "+descr+" differ";
             }
 
-            /// Get scalar data point 
-            template <typename T>
-            inline T get_data(T val, unsigned int =0) { return val; }
+            /// Functor class generating random data [0,1)
+            struct RandomData {
+                explicit RandomData(double ini=43) { srand48(ini); }
+                double operator()() const { return drand48(); }
+            };
 
-            /// Get vector data point
+            /// Functor class generating constant data (as initialized, 0.5 by default)
+            struct ConstantData {
+                double ini_;
+                explicit ConstantData(double ini=0.5) :ini_(ini) { }
+                double operator()() const { return ini_; }
+            };
+
+            /// Functor class generating alternating data (0.5 +/- initialized; initialized=0.5 by default)
+            struct AlternatingData {
+                double ini_;
+                mutable unsigned long step_;
+                
+                explicit AlternatingData(double ini=0.5) :ini_(ini), step_(0) { }
+                double operator()() const {
+                    if (step_==0) {
+                        step_=1;
+                        return 0.5-ini_;
+                    } else {
+                        step_=0;
+                        return 0.5+ini_;
+                    }
+                }
+            };
+
+
+            /// Class generating data points of a general (presumably scalar) type T
+            /** Usage: `T x=gen_data<T>(val,sz);` */ 
+            // (a class is used to allow partial template specialization)
             template <typename T>
-            inline T get_data(typename T::value_type val, unsigned int vsz=10)
-            {
-                return T(vsz, val);
-            }
+            struct gen_data {
+                typedef T value_type;
+                T val_;
+                gen_data(T val, unsigned int =0) : val_(val) {}
+                operator value_type() { return val_; }
+            };
+
+            /// Class generating data points of vector<T>
+            /** Usage: `std::vector<T> v=gen_data<T>(T(val),sz);` */ 
+            template <typename T>
+            struct gen_data< std::vector<T> > {
+                typedef std::vector<T> value_type;
+                T val_;
+                unsigned int vsz_;
+                gen_data(T val, unsigned int vsz /*=10*/) : val_(val), vsz_(vsz) {} // FIXME: uncomment if/when needed
+                operator value_type() { return value_type(val_,vsz_); }
+            };
+
+
+            // /// Get scalar data point 
+            // template <typename T>
+            // inline T get_data(T val, unsigned int =0) { return val; }
+
+            // /// Get vector data point
+            // template <typename T>
+            // inline T get_data(typename T::value_type val, unsigned int vsz=10)
+            // {
+            //     return T(vsz, val);
+            // }
 
 
             /// Class to generate accumulators and results of a given type
-            template <typename A, unsigned long int NPOINTS_PARAM=10000>
+            template <typename A, unsigned long int NPOINTS_PARAM=10000, typename NG=RandomData>
             class AccResultGenerator  {
                 private:
                 alps::accumulators::result_set* results_ptr_;
                 alps::accumulators::accumulator_set* measurements_ptr_;
                 const std::string name_;
+                NG number_generator;
     
                 public:
                 typedef A named_acc_type;
                 typedef typename alps::accumulators::value_type<typename named_acc_type::accumulator_type>::type value_type;
 
                 static const unsigned long int NPOINTS=NPOINTS_PARAM; /// < Number of data points
-                static double tol() { return 5.E-3; }         /// < Recommended tolerance to compare expected and actual results (FIXME: should depend on NPOINTS)
+                static double tol() { return 5.E-3; }         /// < Recommended tolerance to compare expected and actual results (FIXME: should depend on NPOINTS and NG)
                 /// Free the memory allocated in the constructor
                 virtual ~AccResultGenerator()
                 {
@@ -71,13 +126,13 @@ namespace alps {
                 /// Generate the data points for the accumulator
                 AccResultGenerator() : name_("acc")
                 {
-                    srand48(43);
+                    // srand48(43);
                     measurements_ptr_=new alps::accumulators::accumulator_set();
                     alps::accumulators::accumulator_set& m=*measurements_ptr_;
                     m << named_acc_type(name_);
                     for (int i=0; i<NPOINTS; ++i) {
-                        double d=drand48();
-                        m[name_] << get_data<value_type>(d);
+                        double d=number_generator();
+                        m[name_] << gen_data<value_type>(d);
                     }
                     results_ptr_=new alps::accumulators::result_set(m);
                 }
@@ -95,17 +150,18 @@ namespace alps {
                 }
 
                 /// Returns the expected mean
-                double expected_mean() const { return 0.5; }
+                double expected_mean() const { return 0.5; } // FIXME: depends on NG!
                 /// Returns the expected error
-                double expected_err() const { return 1/(12*std::sqrt(NPOINTS-1.)); }
+                double expected_err() const { return 1/(12*std::sqrt(NPOINTS-1.)); } // FIXME: depends on NG!
             };
 
             /// Class to generate a pair of accumulators with identical data: A<T> and A<vector<T>>
-            template <template <typename> class A, typename T, unsigned long NPOINTS_P=1000, unsigned int VSZ_P=10>
+            template <template <typename> class A, typename T, unsigned long NPOINTS_P=1000, unsigned int VSZ_P=10, typename NG=RandomData>
             class acc_vs_pair_gen {
                 private:
                 alps::accumulators::result_set* results_ptr_;
                 alps::accumulators::accumulator_set* measurements_ptr_;
+                NG number_generator;
 
                 public:
                 typedef T scalar_data_type;
@@ -129,16 +185,16 @@ namespace alps {
                 /// Generate the data points for the accumulator
                 acc_vs_pair_gen(const std::string& sname, const std::string& vname) : scal_name(sname), vec_name(vname)
                 {
-                    srand48(43);
+                    // srand48(43);
                     measurements_ptr_=new alps::accumulators::accumulator_set();
                     alps::accumulators::accumulator_set& m=*measurements_ptr_;
                     m << vector_acc_type(vec_name)
                       << scalar_acc_type(scal_name);
 
                     for (int i=0; i<NPOINTS; ++i) {
-                        double d=drand48();
-                        m[vec_name]  << get_data<vector_data_type>(d, VSIZE);
-                        m[scal_name] << get_data<scalar_data_type>(d);
+                        double d=number_generator();
+                        m[vec_name]  << gen_data<vector_data_type>(d, VSIZE);
+                        m[scal_name] << gen_data<scalar_data_type>(d);
                     }
                     results_ptr_=new alps::accumulators::result_set(m);
                 }
@@ -157,11 +213,12 @@ namespace alps {
             };
 
             /// Class to generate accumulators with identical, correlated data: Mean,NoBinning,LogBinning,FullBinning
-            template <typename T, unsigned long NPOINTS_P=1000, unsigned long CORRL_P=10, unsigned int VSZ_P=10>
+            template <typename T, unsigned long NPOINTS_P=1000, unsigned long CORRL_P=10, unsigned int VSZ_P=10, typename NG=RandomData>
             class acc_correlated_gen {
                 private:
                 alps::accumulators::result_set* results_ptr_;
                 alps::accumulators::accumulator_set* measurements_ptr_;
+                NG number_generator;
 
                 public:
                 typedef T data_type;
@@ -191,7 +248,7 @@ namespace alps {
                                 const std::string& logbin="logbin", const std::string& fullbin="fullbin")
                     : mean_name(mean), nobin_name(nobin), logbin_name(logbin), fullbin_name(fullbin)
                 {
-                    srand48(43);
+                    // srand48(43);
                     measurements_ptr_=new alps::accumulators::accumulator_set();
                     alps::accumulators::accumulator_set& m=*measurements_ptr_;
                     m <<    mean_acc_type(   mean_name)
@@ -202,10 +259,10 @@ namespace alps {
                     double d[CORRL_P];
                     for (int j=0; j<CORRL_P; ++j) d[j]=0;
                     for (int i=0; i<NPOINTS; ++i) {
-                        d[0]=drand48()/CORRL_P;
+                        d[0]=number_generator()/CORRL_P;
                         for (int j=1; j<CORRL_P; ++j) d[0]+=d[j]/CORRL_P; // auto-correlated sample
                         // d[0]=0.6*d[1]+0.2*d[2]+0.15*d[3]+0.05*drand48(); // auto-correlated sample; 100% of [0,1]-random var
-                        data_type sample=get_data<data_type>(d[0], VSIZE);
+                        data_type sample=gen_data<data_type>(d[0], VSIZE);
                         m[   mean_name]  << sample;
                         m[  nobin_name]  << sample;
                         m[ logbin_name]  << sample;
@@ -226,6 +283,89 @@ namespace alps {
                 {
                     return *measurements_ptr_;
                 }
+                /// Returns the expected mean
+                double expected_mean() const { return 0.5; }
+                /// Returns the expected error if uncorrelated
+                double expected_uncorr_err() const { return 1/(12*std::sqrt(NPOINTS-1.)); }
+            };
+
+            /// Class to generate a single accumulator A with correlated data of type T
+            template <template<typename> class A, typename T, unsigned long NPOINTS_P=1000, unsigned long CORRL_P=10, unsigned int VSZ_P=10, typename NG=RandomData>
+            class acc_one_correlated_gen {
+                private:
+                alps::accumulators::result_set* results_ptr_;
+                alps::accumulators::accumulator_set* measurements_ptr_;
+                NG number_generator;
+
+                public:
+                typedef T data_type;
+                typedef A<T> acc_type;
+
+                static const unsigned long int NPOINTS=NPOINTS_P; /// < Number of data points
+                static const unsigned int VSIZE=VSZ_P; /// size of the vector
+                static const unsigned long int CORRL=CORRL_P; /// correlation length (samples)
+
+                const std::string acc_name;
+
+                /// Free the memory allocated in the constructor
+                virtual ~acc_one_correlated_gen()
+                {
+                    delete results_ptr_;
+                    delete measurements_ptr_;
+                }
+
+                /// Generate the data points for the accumulator
+                acc_one_correlated_gen(const std::string& name="data")
+                    : acc_name(name)
+                {
+                    // srand48(43);
+                    measurements_ptr_=new alps::accumulators::accumulator_set();
+                    alps::accumulators::accumulator_set& m=*measurements_ptr_;
+                    m << acc_type(acc_name);
+
+                    double d[CORRL_P];
+                    for (int j=0; j<CORRL_P; ++j) d[j]=0;
+                    for (int i=0; i<NPOINTS; ++i) {
+                        d[0]=number_generator()/CORRL_P;
+                        for (int j=1; j<CORRL_P; ++j) d[0]+=d[j]/CORRL_P; // auto-correlated sample
+                        // d[0]=0.6*d[1]+0.2*d[2]+0.15*d[3]+0.05*drand48(); // auto-correlated sample; 100% of [0,1]-random var
+                        data_type sample=gen_data<data_type>(d[0], VSIZE);
+                        m[acc_name]  << sample;
+                        for (int j=CORRL_P-1; j>=1; --j) d[j]=d[j-1];
+                    }
+                    results_ptr_=new alps::accumulators::result_set(m);
+                }
+
+                /// Returns extracted result set
+                const alps::accumulators::result_set& results() const
+                {
+                    return *results_ptr_;
+                }
+
+                /// Returns extracted result set as non-const
+                alps::accumulators::result_set& results()
+                {
+                    return *results_ptr_;
+                }
+
+                /// Returns the accumulator set
+                const alps::accumulators::accumulator_set& accumulators() const
+                {
+                    return *measurements_ptr_;
+                }
+
+                /// Returns extracted result
+                const alps::accumulators::result_wrapper& result() const
+                {
+                    return results()[acc_name];
+                }
+
+                /// Returns the accumulator set
+                const alps::accumulators::accumulator_wrapper& accumulator() const
+                {
+                    return accumulators()[acc_name];
+                }
+                
                 /// Returns the expected mean
                 double expected_mean() const { return 0.5; }
                 /// Returns the expected error if uncorrelated
