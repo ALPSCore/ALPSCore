334477c323488fe0d5038d284fe8d4a9e62c380c
Alexander <galexv@umich.edu>
2016-03-29 13:26:04 -0400

    Adds a function to "rectangularize" a vector of vectors.
    The commit adds the function and the test.
    Necessary to fix issue #198.

---
+++ utilities/include/alps/numeric/rectangularize.hpp
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 1998-2016 ALPS Collaboration. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ * For use in publications, see ACKNOWLEDGE.TXT
+ */
+
+/** @file rectangularize.hpp
+    @brief Defines a function to rectangularize a vector-of-vectors
+*/
+
+#ifndef ALPS_NUMERIC_RECTANGULARIZE_HPP_08679765945f4171af5c396beff05503
+#define ALPS_NUMERIC_RECTANGULARIZE_HPP_08679765945f4171af5c396beff05503
+
+#include <vector>
+#include <boost/foreach.hpp>
+
+namespace alps {
+    namespace numeric {
+
+        /// Make sure that vector-of-vectors is a rectangular matrix (generic dummy template)
+        template <typename T>
+        void rectangularize(const T&) {}
+
+        /// Make sure that vector-of-vectors is a rectangular matrix
+        /**  Converts an uneven vector-of-vectors to a rectangular matrix,
+             resizing rows as needed (that is, right-padding the rows with 0)
+
+             @note Does not work with 3-tensors.
+         */
+        template <typename T>
+        void rectangularize(std::vector< std::vector<T> >& vec)
+        {
+            std::size_t mx_size=0;
+            BOOST_FOREACH(std::vector<T>& val, vec) {
+                // FIXME: // would be needed for 3-tensors, but not quite working:
+                // FIXME: rectangularize(val);
+                if (mx_size<val.size()) mx_size=val.size();
+            }
+            BOOST_FOREACH(std::vector<T>& val, vec) {
+                val.resize(mx_size);
+            }
+        }
+
+    }
+}
+
+#endif /* ALPS_NUMERIC_RECTANGULARIZE_HPP_08679765945f4171af5c396beff05503 */
---
+++ utilities/test/rectangularize.cpp
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 1998-2016 ALPS Collaboration. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ * For use in publications, see ACKNOWLEDGE.TXT
+ */
+
+#include "alps/numeric/rectangularize.hpp"
+#include <gtest/gtest.h>
+
+TEST(Rectangularize,Rectangularize)
+{
+    typedef std::vector<double> vec_type;
+    typedef std::vector<vec_type> mtx_type;
+
+    vec_type row1(3, 1.5);
+    vec_type row2(2, 2.25);
+    mtx_type mtx(3);
+
+    mtx[0]=row1;
+    mtx[1]=row2;
+
+    alps::numeric::rectangularize(mtx);
+
+    mtx_type expected_mtx;
+    expected_mtx.push_back(row1);
+    expected_mtx.push_back(row2); expected_mtx[1].push_back(0);
+    expected_mtx.push_back(vec_type(3, 0.0));
+
+    EXPECT_EQ(expected_mtx, mtx);
+}
+
+    
