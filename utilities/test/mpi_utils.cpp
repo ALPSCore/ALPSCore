ALPSCore/ALPSCore

commit 2e2b700cfa11a03e45bcf75c43b908cea0abc46b

Implemented owning and duplicating a communicator.

utilities/include/alps/utilities/mpi.hpp
@@ -83,14 +83,45 @@ namespace alps {
 
         class communicator {
             MPI_Comm comm_;
+            bool destroy_;
+
+            /// Assignment: deleted.
+            void operator=(const communicator&) {
+                throw std::logic_error("CAN'T HAPPEN: should never be called!");
+            }
+
+            /// Copy constructor: deleted.
+            communicator(const communicator&)  {
+                throw std::logic_error("CAN'T HAPPEN: should never be called!");
+            }
             
             public:
 
-            communicator() : comm_(MPI_COMM_WORLD) {} // FIXME? Shall we deprecate it?
+            communicator() : comm_(MPI_COMM_WORLD), destroy_(false) {} // FIXME? Shall we deprecate it?
+
+            // FIXME: introduce error checking!!
             
-            communicator(const MPI_Comm& comm, comm_create_kind kind) : comm_(comm) {
-                if (comm_attach!=kind) {
-                    throw std::logic_error("alps::mpi::communicator(): kind!=comm_attach is not supported.");
+            communicator(const MPI_Comm& comm, comm_create_kind kind) : comm_(comm), destroy_(false) {
+                switch (kind) {
+                  default:
+                      throw std::logic_error("alps::mpi::communicator(): unsupported `kind` argument.");
+                      break;
+                  case comm_attach:
+                      break;
+                  case take_ownership:
+                      destroy_=true;
+                      break;
+                  case comm_duplicate:
+                      MPI_Comm_dup(comm, &comm_);
+                      destroy_=true;
+                      break;
+                }
+            }
+
+            /// Destroys communicator if it is "managed"
+            ~communicator() {
+                if (destroy_) {
+                    MPI_Comm_free(&comm_);
                 }
             }
 

utilities/test/mpi_utils.cpp
@@ -60,6 +60,31 @@ TEST_F(MpiTest, CommConstructAttach) {
     EXPECT_TRUE(is_valid(newcomm_));
 }
 
+TEST_F(MpiTest, CommConstructDuplicate) {
+    MPI_Comm tmpcomm;
+    {
+        am::communicator comm(newcomm_, am::comm_duplicate);
+        EXPECT_NE(newcomm_, comm);
+        EXPECT_EQ(myrank_, comm.rank());
+        EXPECT_EQ(nproc_, comm.size());
+        tmpcomm=comm;
+    }
+    EXPECT_TRUE(is_valid(newcomm_));
+    // This is undefined and will crash:
+    // EXPECT_FALSE(is_valid(tmpcomm));
+}
+
+TEST_F(MpiTest, CommConstructOwn) {
+    {
+        am::communicator comm(newcomm_, am::take_ownership);
+        EXPECT_EQ(newcomm_, comm);
+        EXPECT_EQ(myrank_, comm.rank());
+        EXPECT_EQ(nproc_, comm.size());
+    }
+    // This is undefined and will crash:
+    // EXPECT_FALSE(is_valid(newcomm_));
+}
+
 int main(int argc, char** argv)
 {
     alps::mpi::environment env(argc, argv, false);

commit d8703af7587bd0d4f4afa3dd34ac6cc3a96db1ef

Reimplemented MPI communicator wrapper with copy semantics.
This fixes errors caused by commit 2e2b700cfa.

MPI communicator wrappers (alps::mpi::communicator) now can be safely
copied (assigned) and copy-constructed; they are managed using
boost::shared_ptr<> and are now allocated on heap.

utilities/include/alps/utilities/mpi.hpp
@@ -23,6 +23,7 @@
 #include <complex>
 
 #include <boost/scoped_array.hpp> /* for std::string broadcast */
+#include <boost/shared_ptr.hpp> /* for proper copy/assign of managed communicators */
 
 // DEBUG:
 #include <stdexcept>
@@ -77,76 +78,74 @@ namespace alps {
         } // detail::
 
 
+        /// Possible ways to make a C++ object from MPI communicator
         enum comm_create_kind {
-            comm_attach, comm_duplicate, take_ownership
+            comm_attach, ///< do not destroy when going out of scope
+            comm_duplicate, ///< duplicate and destroy when going out of scope
+            take_ownership ///< do not duplicate, but *destroy* when going out of scope
         };
 
+        /// Encapsulation of an MPI communicator and some communicator-related operations
         class communicator {
-            MPI_Comm comm_;
-            bool destroy_;
+            boost::shared_ptr<MPI_Comm> comm_ptr_;
 
-            /// Assignment: deleted.
-            void operator=(const communicator&) {
-                throw std::logic_error("CAN'T HAPPEN: should never be called!");
-            }
+            // Internal functor class to destroy communicator when needed
+            struct comm_deleter {
+                void operator()(MPI_Comm* comm_ptr) {
+                    int finalized;
+                    MPI_Finalized(&finalized);
+                    if (!finalized) MPI_Comm_free(comm_ptr);
+                    delete comm_ptr;
+                }
+            };
 
-            /// Copy constructor: deleted.
-            communicator(const communicator&)  {
-                throw std::logic_error("CAN'T HAPPEN: should never be called!");
-            }
-            
             public:
 
-            communicator() : comm_(MPI_COMM_WORLD), destroy_(false) {} // FIXME? Shall we deprecate it?
+            communicator() : comm_ptr_(new MPI_Comm(MPI_COMM_WORLD)) {} // FIXME? Shall we deprecate it?
 
             // FIXME: introduce error checking!!
             
-            communicator(const MPI_Comm& comm, comm_create_kind kind) : comm_(comm), destroy_(false) {
+            communicator(const MPI_Comm& comm, comm_create_kind kind) {
                 switch (kind) {
                   default:
                       throw std::logic_error("alps::mpi::communicator(): unsupported `kind` argument.");
                       break;
                   case comm_attach:
+                      comm_ptr_.reset(new MPI_Comm(comm));
                       break;
                   case take_ownership:
-                      destroy_=true;
+                      comm_ptr_.reset(new MPI_Comm(comm), comm_deleter());
                       break;
                   case comm_duplicate:
-                      MPI_Comm_dup(comm, &comm_);
-                      destroy_=true;
+                      MPI_Comm* newcomm_ptr=new MPI_Comm();
+                      MPI_Comm_dup(comm, newcomm_ptr);
+                      comm_ptr_.reset(newcomm_ptr, comm_deleter());
                       break;
-                }
-            }
-
-            /// Destroys communicator if it is "managed"
-            ~communicator() {
-                if (destroy_) {
-                    MPI_Comm_free(&comm_);
                 }
             }
 
             /// Returns process rank in this communicator
             int rank() const {
                 int myrank;
-                MPI_Comm_rank(comm_,&myrank);
+                MPI_Comm_rank(*comm_ptr_,&myrank);
                 return myrank;
             }
 
             /// Returns the number of processes in this communicator
             int size() const {
                 int sz;
-                MPI_Comm_size(comm_,&sz);
+                MPI_Comm_size(*comm_ptr_,&sz);
                 return sz;
             }
 
             /// Barrier on this communicator
             void barrier() const {
-                MPI_Barrier(comm_);
+                MPI_Barrier(*comm_ptr_);
             }
 
             /// Converts this communicator object to MPI communicator
             operator MPI_Comm() const {
-                return comm_;
+                return *comm_ptr_;
             }
         };
 

utilities/test/mpi_utils.cpp
@@ -85,6 +85,63 @@ TEST_F(MpiTest, CommConstructOwn) {
     // EXPECT_FALSE(is_valid(newcomm_));
 }
 
+TEST_F(MpiTest, CommConctructCopy) {
+    am::communicator comm_d1(newcomm_, am::comm_duplicate);
+    am::communicator comm_a1(newcomm_, am::comm_attach);
+    {
+        am::communicator comm_d2(comm_d1);
+        EXPECT_EQ(static_cast<MPI_Comm>(comm_d1), static_cast<MPI_Comm>(comm_d2));
+        am::communicator comm_a2(comm_a1);
+        EXPECT_EQ(static_cast<MPI_Comm>(comm_a1), static_cast<MPI_Comm>(comm_a2));
+    }
+    // after comm_{a,d}2 are destroyed, the corresponding comm1's are valid
+    EXPECT_TRUE(is_valid(comm_d1));
+    EXPECT_TRUE(is_valid(comm_a1));
+}
+
+TEST_F(MpiTest, CommAssignAttached) {
+    am::communicator comm_d1(newcomm_, am::comm_duplicate);
+    am::communicator comm_a1(newcomm_, am::comm_attach);
+    MPI_Comm mpicomm_d1=comm_d1;
+    {
+        am::communicator comm_a2(newcomm_, am::comm_attach);
+        comm_d1=comm_a2;
+        EXPECT_EQ(static_cast<MPI_Comm>(comm_a2), static_cast<MPI_Comm>(comm_d1));
+        comm_a1=comm_a2;
+        EXPECT_EQ(static_cast<MPI_Comm>(comm_a2), static_cast<MPI_Comm>(comm_a1));
+    }
+    // after comm_a2 is destroyed, the comm1's are valid
+    EXPECT_EQ(newcomm_,comm_d1);
+    EXPECT_EQ(newcomm_,comm_a1);
+    EXPECT_TRUE(is_valid(comm_d1));
+    EXPECT_TRUE(is_valid(comm_a1));
+    // and mpicomm_d1 (underlying MPI communicator for comm_d1) is not valid and may crash
+    // EXPECT_FALSE(is_valid(mpicomm_d1));
+}
+
+TEST_F(MpiTest, CommAssignDuplicated) {
+    am::communicator comm_d1(newcomm_, am::comm_duplicate);
+    am::communicator comm_a1(newcomm_, am::comm_attach);
+    MPI_Comm mpicomm_d1=comm_d1;
+    MPI_Comm mpicomm_a2;
+    {
+        am::communicator comm_a2(newcomm_, am::comm_duplicate);
+        mpicomm_a2=comm_a2;
+        comm_d1=comm_a2;
+        EXPECT_EQ(static_cast<MPI_Comm>(comm_a2), static_cast<MPI_Comm>(comm_d1));
+        comm_a1=comm_a2;
+        EXPECT_EQ(static_cast<MPI_Comm>(comm_a2), static_cast<MPI_Comm>(comm_a1));
+    }
+    // after comm_a2 is destroyed, the comm1's (and the underlying MPI comm) are valid
+    EXPECT_EQ(mpicomm_a2,comm_d1);
+    EXPECT_EQ(mpicomm_a2,comm_a1);
+    EXPECT_TRUE(is_valid(comm_d1));
+    EXPECT_TRUE(is_valid(comm_a1));
+    // and mpicomm_d1 (underlying MPI communicator for comm_d1) is not valid and may crash
+    // EXPECT_FALSE(is_valid(mpicomm_d1));
+}
+
+
 int main(int argc, char** argv)
 {
     alps::mpi::environment env(argc, argv, false);