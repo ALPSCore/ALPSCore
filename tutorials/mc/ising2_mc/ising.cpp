ALPSCore/ALPSCore

commit d0b5e94328a535ed32fcded01d92c5e074be32b1

Added assertions and cross-checks to 2D Ising.
(This commit is made for archiving purposes and will likely be reverted).

tutorials/mc/ising2_mc/ising.cpp
@@ -9,6 +9,34 @@
 #include <alps/params/convenience_params.hpp>
 #include <boost/lambda/lambda.hpp>
 
+double ising_sim::get_energy()
+{
+    double en=0;
+    for (int i=0; i<length; ++i) {
+        for (int j=0; j<length; ++j) {
+            int i_next=(i+1)%length;
+            int j_next=(j+1)%length;
+            en += -(spins(i,j)*spins(i,j_next)+
+                    spins(i,j)*spins(i_next,j));
+            
+        }
+    }
+    return en;
+}
+
+double ising_sim::get_magnetization()
+{
+    double mag=0;
+    for (int i=0; i<length; ++i) {
+        for (int j=0; j<length; ++j) {
+            mag += spins(i,j);
+        }
+    }
+    return mag;
+}
+
+
+
 // Defines the parameters for the ising simulation
 void ising_sim::define_parameters(parameters_type & parameters) {
     // If the parameters are restored, they are already defined
@@ -40,13 +68,36 @@ ising_sim::ising_sim(parameters_type const & parms, std::size_t seed_offset)
     , total_sweeps(parameters["sweeps"])
     , beta(1. / parameters["temperature"].as<double>())
     , spins(length,length)
+    , current_energy(0)
+    , current_magnetization(0)
 {
     // Initializes the spins
     for(int i=0; i<length; ++i) {
         for (int j=0; j<length; ++j) {
-            spins(i,j) = (random() < 0.5 ? 1 : -1);
+            // spins(i,j) = (random() < 0.5 ? 1 : -1);
+            spins(i,j) = 1;
         }
     }
+
+    // Calculates initial magnetization and energy
+    // current_magnetization=get_magnetization();
+    // current_energy=get_energy();
+    current_magnetization=length*length;
+    current_energy=-2*length*length;
+    assert(get_energy()==current_energy);
+    assert(get_magnetization()==current_magnetization);
+    
+    // for (int i=0; i<length; ++i) {
+    //     for (int j=0; j<length; ++j) {
+    //         current_magnetization += spins(i,j);
+    //         int i_next=(i+1)%length;
+    //         int j_next=(j+1)%length;
+    //         current_energy += -(spins(i,j)*spins(i,j_next)+
+    //                             spins(i,j)*spins(i_next,j));
+            
+    //     }
+    // }
+    // std::cerr << "Initial energy=" << current_energy << " magnetization=" << current_magnetization << std::endl << spins; // DEBUG
     
     // Adds the measurements
     measurements
@@ -79,39 +130,35 @@ void ising_sim::update() {
                      spins(i,j2)); // down
     // Step acceptance:
     if (delta<=0. || random() < exp(-beta*delta)) {
+        // update energy:
+        current_energy += delta;
+        // update magnetization:
+        current_magnetization -= 2*spins(i,j);
         // flip the spin
         spins(i,j) = -spins(i,j);
+
+        // std::cerr << "Accepted, Delta=" << delta << std::endl << spins; // DEBUG
+    } else {
+        // std::cerr << "Rejected, Delta=" << delta << std::endl; // DEBUG
     }
+        
 }
 
 // Collects the measurements at each MC step.
 void ising_sim::measure() {
-    sweeps++;
+    ++sweeps;
     if (sweeps<thermalization_sweeps) return;
     
-    double tmag = 0; // magnetization
-    double ten = 0; // energy
+    const double l2=length*length; // number of sites
+    double tmag = current_magnetization / l2; // magnetization
 
-    // Calculate magnetization and energy.
-    // FIXME: Actually we do not need to do it,
-    // FIXME: it's possible to know it from update!
-    for (int i=0; i<length; ++i) {
-        for (int j=0; j<length; ++j) {
-            tmag += spins(i,j);
-            int i_next=(i+1)%length;
-            int j_next=(j+1)%length;
-            ten += -(spins(i,j)*spins(i,j_next)+
-                     spins(i,j)*spins(i_next,j));
-            
-        }
-    }
-
-    const double l2=length*length;
-    ten /= l2;
-    tmag /= l2;
-
-    // Accumulate the data
-    measurements["Energy"] << ten;
+    assert(get_energy()==current_energy); // DEBUG
+    assert(get_magnetization()==current_magnetization); // DEBUG
+    // std::cerr << "Energy=" << current_energy << " Magnetization=" << current_magnetization << std::endl; // DEBUG
+    // std::cerr << "Energ1=" << get_energy() << " Magnetizatio1=" << get_magnetization() << std::endl; // DEBUG
+    
+    // Accumulate the data (per site)
+    measurements["Energy"] << (current_energy / l2);
     measurements["Magnetization"] << tmag;
     measurements["AbsMagnetization"] << fabs(tmag);
     measurements["Magnetization^2"] << tmag*tmag;

tutorials/mc/ising2_mc/ising.hpp
@@ -30,6 +30,11 @@ class ising_sim : public alps::mcbase {
     int total_sweeps;
     double beta;
     storage_type spins;
+    double current_energy;
+    double current_magnetization;
+
+    double get_energy();
+    double get_magnetization();
         
   public:
     ising_sim(parameters_type const & parms, std::size_t seed_offset = 0);

tutorials/mc/ising2_mc/storage_type.hpp
@@ -4,6 +4,8 @@
  * For use in publications, see ACKNOWLEDGE.TXT
  */
 
+#include <iostream>
+
 // Storage class for 2D spin array.
 // Implemented as vector of vectors for simplicity.
 class storage_type {
@@ -32,4 +34,18 @@ class storage_type {
     void load(alps::hdf5::archive& ar) {
         ar["2Darray"] >> data_;
     }
+
+    friend std::ostream& operator<<(std::ostream& s, const storage_type& obj) {
+        int n=obj.data_.size();
+        for (int i=0; i<n; ++i) {
+            for (int j=0; j<n; ++j) {
+                int c=obj.data_.at(i).at(j);
+                assert(c==1 || c==-1);
+                s << (c==1?".":"*");
+            }
+            s << "\n";
+        }
+        return s;
+    }
 };
+

commit b5ba89bde4660f30820ad11b7222d0bf426ed9e8

Removed most assertions/debug code introduced by commit d0b5e94328a
Essentially reverts commit d0b5e94328a535ed32fcded01d92c5e074be32b1.

tutorials/mc/ising2_mc/ising.cpp
@@ -9,34 +9,6 @@
 #include <alps/params/convenience_params.hpp>
 #include <boost/lambda/lambda.hpp>
 
-double ising_sim::get_energy()
-{
-    double en=0;
-    for (int i=0; i<length; ++i) {
-        for (int j=0; j<length; ++j) {
-            int i_next=(i+1)%length;
-            int j_next=(j+1)%length;
-            en += -(spins(i,j)*spins(i,j_next)+
-                    spins(i,j)*spins(i_next,j));
-            
-        }
-    }
-    return en;
-}
-
-double ising_sim::get_magnetization()
-{
-    double mag=0;
-    for (int i=0; i<length; ++i) {
-        for (int j=0; j<length; ++j) {
-            mag += spins(i,j);
-        }
-    }
-    return mag;
-}
-
-
-
 // Defines the parameters for the ising simulation
 void ising_sim::define_parameters(parameters_type & parameters) {
     // If the parameters are restored, they are already defined
@@ -75,30 +47,21 @@ ising_sim::ising_sim(parameters_type const & parms, std::size_t seed_offset)
     // Initializes the spins
     for(int i=0; i<length; ++i) {
         for (int j=0; j<length; ++j) {
-            // spins(i,j) = (random() < 0.5 ? 1 : -1);
-            spins(i,j) = 1;
+            spins(i,j) = (random() < 0.5 ? 1 : -1);
         }
     }
 
     // Calculates initial magnetization and energy
-    // current_magnetization=get_magnetization();
-    // current_energy=get_energy();
-    current_magnetization=length*length;
-    current_energy=-2*length*length;
-    assert(get_energy()==current_energy);
-    assert(get_magnetization()==current_magnetization);
-    
-    // for (int i=0; i<length; ++i) {
-    //     for (int j=0; j<length; ++j) {
-    //         current_magnetization += spins(i,j);
-    //         int i_next=(i+1)%length;
-    //         int j_next=(j+1)%length;
-    //         current_energy += -(spins(i,j)*spins(i,j_next)+
-    //                             spins(i,j)*spins(i_next,j));
+    for (int i=0; i<length; ++i) {
+        for (int j=0; j<length; ++j) {
+            current_magnetization += spins(i,j);
+            int i_next=(i+1)%length;
+            int j_next=(j+1)%length;
+            current_energy += -(spins(i,j)*spins(i,j_next)+
+                                spins(i,j)*spins(i_next,j));
             
-    //     }
-    // }
-    // std::cerr << "Initial energy=" << current_energy << " magnetization=" << current_magnetization << std::endl << spins; // DEBUG
+        }
+    }
     
     // Adds the measurements
     measurements
@@ -130,9 +93,6 @@ void ising_sim::update() {
                      spins(i,j1)+  // up
                      spins(i,j2)); // down
     
-    double expval=iexp_(delta); // DEBUG
-    double expval0=exp(-beta*delta); // DEBUG
-    assert(expval==expval0); // DEBUG
     // Step acceptance:
     if (delta<=0. || random() < iexp_(delta)) {
         // update energy:
@@ -141,12 +101,7 @@ void ising_sim::update() {
         current_magnetization -= 2*spins(i,j);
         // flip the spin
         spins(i,j) = -spins(i,j);
-
-        // std::cerr << "Accepted, Delta=" << delta << std::endl << spins; // DEBUG
-    } else {
-        // std::cerr << "Rejected, Delta=" << delta << std::endl; // DEBUG
-    }
-        
+    }        
 }
 
 // Collects the measurements at each MC step.
@@ -157,11 +112,6 @@ void ising_sim::measure() {
     const double l2=length*length; // number of sites
     double tmag = current_magnetization / l2; // magnetization
 
-    assert(get_energy()==current_energy); // DEBUG
-    assert(get_magnetization()==current_magnetization); // DEBUG
-    // std::cerr << "Energy=" << current_energy << " Magnetization=" << current_magnetization << std::endl; // DEBUG
-    // std::cerr << "Energ1=" << get_energy() << " Magnetizatio1=" << get_magnetization() << std::endl; // DEBUG
-    
     // Accumulate the data (per site)
     measurements["Energy"] << (current_energy / l2);
     measurements["Magnetization"] << tmag;

tutorials/mc/ising2_mc/ising.hpp
@@ -37,9 +37,6 @@ class ising_sim : public alps::mcbase {
 
     exp_beta iexp_; // function object to compute exponent
 
-    double get_energy();
-    double get_magnetization();
-        
   public:
     ising_sim(parameters_type const & parms, std::size_t seed_offset = 0);
 

tutorials/mc/ising2_mc/storage_type.hpp
@@ -7,7 +7,6 @@
 #ifndef ALPS_TUTORIALS_MC_ISING2_STORAGE_TYPE_HPP_228662e62e9247d5903f52f9644b6455
 #define ALPS_TUTORIALS_MC_ISING2_STORAGE_TYPE_HPP_228662e62e9247d5903f52f9644b6455
 
-#include <iostream>
 #include <vector>
 
 // Storage class for 2D spin array.
@@ -37,19 +36,6 @@ class storage_type {
     // Custom load
     void load(alps::hdf5::archive& ar) {
         ar["2Darray"] >> data_;
-    }
-
-    friend std::ostream& operator<<(std::ostream& s, const storage_type& obj) {
-        int n=obj.data_.size();
-        for (int i=0; i<n; ++i) {
-            for (int j=0; j<n; ++j) {
-                int c=obj.data_.at(i).at(j);
-                assert(c==1 || c==-1);
-                s << (c==1?".":"*");
-            }
-            s << "\n";
-        }
-        return s;
     }
 };
 