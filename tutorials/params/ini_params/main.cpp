8cdcc06fe4e47c219b36e5201f61607466a7b2dc
Alexander <galexv@umich.edu>
2016-09-27 14:33:07 -0400

    Expanded parameters tutorial example, including sections.
    This addresses issue #217.

--- tutorials/params/ini_params/main.cpp
+++ tutorials/params/ini_params/main.cpp
@@ -11,6 +11,7 @@
  * This example shows how to read parameters from a .ini file.
  * The file is passed through the first command-line argument.
  * A couple of example .ini files are provided in this directory.
+ * A .ini file can have sections.
  * The parameters can also be overridden using command-line options.
  * Run the example with different arguments combinations. For example:
  * <ul>
@@ -35,13 +36,30 @@ int main(int argc, const char* argv[])
     // names, default values, and descriptions. By default, only the "help" 
     // parameter is defined, and is initialized to false.
     //
+    // If a parameter is defined without a default value, the
+    // parameter must be supplied (either as an ini file entry or as a
+    // command line argument).
+    //
     // If an ini file entry matches "parameterName=Xxx" then the parameter 
     // is set to the value; if no ini file entry matches, the default
     // value is used. Command-line options will override the ini file entry.
+    //
+    // An ini file can contain [sections]. A parameter name `name` in
+    // a section `[section]` is referred to as `section.name`.
+    // 
     std::cout << "Defining parameters..." << std::endl;
-    par.define<int>("count", 0, "Number of interconnected elements");
-    par.define<double>("val", 6.28, "Value of implosion constant");
-    par.define<std::string>("name", "Judas", "Name of de-construction algorithm");
+    // no sections:
+    par .define<int>("count", 0, "Number of interconnected elements")
+        .define<double>("val", 6.28, "Value of implosion constant")
+        .define<std::string>("name", "Judas", "Name of de-construction algorithm");
+    // inside a section [other]:
+    par .define<int>("other.count", -1, "Number of the other interconnected elements")
+        .define<double>("other.val", 1.25, "Value of the other implosion constant")
+        .define<std::string>("other.name", "Jack", "Name of the other de-construction algorithm");
+    // a parameter with a required value:
+    par.define<std::string>("user", "User name");
+    // a "switch" parameter (either it is present or not):
+    par.define("verbose","Be verbose");
     
     // If requested, we print the help message, which is constructed from the
     // information we gave when defining the parameters.
@@ -49,8 +67,39 @@ int main(int argc, const char* argv[])
         return 0;
     }
 
-    // Printing parameter to standard output.
-    std::cout << "Parameter values" << std::endl;    
+    // We can also check if there are any parameters that do not have
+    // a value assigned; if so, a short message will be printed to the
+    // given stream.
+    if (par.has_missing(std::cout)) {
+        return 1;
+    }
+
+    // Using the parameters.
+    // Type checking and conversion happens automatically, when possible.
+    std::string user=par["user"];
+    std::cout << "Hello, " << user << "!\n";
+    if (par["verbose"]) {
+        std::cout << "You asked me to be verbose.\n";
+    }
+    std::cout << "Your parameters are:\n";
+    int count=par["count"];
+    double val=par["val"];
+    std::cout << "count=" << count << " val=" << val;
+    // Here C++ cannot guess the type to convert to, we have to help:
+    std::cout << " name=" << par["name"].as<std::string>()
+              << std::endl;
+
+    // Parameters from the other section have `other.` prefix.
+    // Also, we are using explicit type conversion.
+    std::cout << "Your other parameters are:\n"
+              << "count=" << int(par["other.count"])
+              << " val="  << double(par["other.val"])
+             // but calling std::string() would be ambiguous, we have to use .as<...>(...)
+              << " name=" << par["other.name"].as<std::string>()
+              << std::endl;
+
+    // We can also print parameters to standard output.
+    std::cout << "\nAll parameter values" << std::endl;    
     std::cout << "----------------" << std::endl;    
     std::cout << par;
     return 0;
