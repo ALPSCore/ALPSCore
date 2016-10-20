f8a5e227fe24912e2c13fcab0fb587c72e2028cd
Alexander <galexv@umich.edu>
2016-10-20 14:22:06 -0400

    Better testing of alps::mpi broadcasts.
    Catches type mismatches
    (e.g., defining MPI_CXX_BOOL as MPI_INT would now fail).

--- utilities/test/mpi_utils_bcast.cpp
+++ utilities/test/mpi_utils_bcast.cpp
@@ -4,13 +4,15 @@
  * For use in publications, see ACKNOWLEDGE.TXT
  */
 
-#include <algorithm> // for std::min, std::copy
+#include <algorithm>
 #include <boost/scoped_array.hpp>
 
 #include <alps/utilities/mpi.hpp>
+
 #include <gtest/gtest.h>
 
 #include "./test_utils.hpp"
+#include <alps/utilities/gtest_par_xml_output.hpp>
 
 /* Test MPI broadcasts for basic types */
 
@@ -18,7 +20,7 @@ namespace am=alps::mpi;
 
 template <typename T>
 class MpiBcastTest_base : public ::testing::Test {
-  protected:
+  private:
     typedef std::vector<T> vector_type;
     typedef T value_type;
     static const int ROOT_=0;
@@ -33,40 +35,64 @@ class MpiBcastTest_base : public ::testing::Test {
 
     void bcast_scalar() {
         value_type root_data=alps::testing::datapoint<value_type>::get(true);
+        value_type root_data0=root_data;
         value_type slave_data=alps::testing::datapoint<value_type>::get(false);
         value_type& my_data=*(this->is_root_? &root_data : &slave_data);
 
         // Sanity check:
         if (this->is_root_) {
-            ASSERT_EQ(root_data,my_data) << "Data mess-up on root";
+            // must be trivially true by construction
+            ASSERT_EQ(root_data,my_data) << "Can't happen: Data mess-up on root";
         } else {
-            ASSERT_NE(root_data,my_data) << "Data mess-up on slave";
+            // must be true if get(true|false) works 
+            ASSERT_NE(root_data,my_data) << "Data mess-up on slave: root data does not differ";
         }
-    
-        am::broadcast(this->comm_, my_data, this->ROOT_);
+
+        am::broadcast(comm_, my_data, ROOT_);
 
         EXPECT_EQ(root_data, my_data);
     }
 
-    void bcast_array() {
-        vector_type root_data=alps::testing::datapoint<vector_type>::get(true);
-        vector_type slave_data=alps::testing::datapoint<vector_type>::get(false);
-        std::size_t sz=std::min(root_data.size(), slave_data.size());
-        root_data.resize(sz);
-        slave_data.resize(sz);
-        vector_type& my_data=*(this->is_root_? &root_data : &slave_data);
+    void bcast_array() { // works for vector<bool> too
+        const std::size_t bufsz=41; // some "odd" number
+        // margin large enough to catch data type size mismatches up to 16x without a crash:
+        const std::size_t margin=bufsz*16+3; 
+        const std::size_t vsize=2*margin+bufsz; // vector is buffer surrounded by margins
+        
+        vector_type root_data=alps::testing::datapoint<vector_type>::get(true,vsize);
+        vector_type slave_data=alps::testing::datapoint<vector_type>::get(false,vsize);
 
-        // Sanity check:
-        if (this->is_root_) {
-            ASSERT_EQ(root_data,my_data) << "Data mess-up on root";
+        boost::scoped_array<value_type> my_data(new value_type[vsize]);
+        if (is_root_) {
+            std::copy(root_data.begin(), root_data.end(), my_data.get());
         } else {
-            ASSERT_NE(root_data,my_data) << "Data mess-up on slave";
+            std::copy(slave_data.begin(), slave_data.end(), my_data.get());
         }
-    
-        am::broadcast(this->comm_, &my_data[0], sz, this->ROOT_);
 
-        EXPECT_EQ(root_data, my_data);
+        // broadcast the middle of the vector only
+        am::broadcast(comm_, my_data.get()+margin, bufsz, ROOT_);
+
+        typedef typename vector_type::const_iterator iter_type;
+        if (is_root_) {
+            // data on root should not change
+            EXPECT_TRUE(std::equal(root_data.begin(), root_data.end(), my_data.get())) << "Data changed on root";
+        } else {
+            // data on slave should change in the buffer only
+            const T* left_margin_beg=my_data.get();
+            iter_type left_margin_beg0=slave_data.begin();
+
+            const T* buffer_beg=left_margin_beg+margin;
+            iter_type buffer_beg0=root_data.begin()+margin;
+            
+            const T* right_margin_beg=buffer_beg+bufsz;
+            iter_type right_margin_beg0=slave_data.end()-margin;
+            
+            EXPECT_TRUE(std::equal(left_margin_beg, buffer_beg, left_margin_beg0)) << "Left margin is damaged on slave";
+            EXPECT_TRUE(std::equal(buffer_beg, right_margin_beg, buffer_beg0)) << "Broadcast data incorrect";
+            EXPECT_TRUE(std::equal(right_margin_beg, right_margin_beg+margin, right_margin_beg0)) << "Right margin is damaged on slave";
+        }
     }        
+
 };
 
 template <typename T>
@@ -83,37 +109,6 @@ class MpiBcastTest<std::string>: public MpiBcastTest_base<std::string> {
                   << std::endl;
     }
 };
-
-// specialization for T=bool (because vector<bool> is special)
-template <>
-class MpiBcastTest<bool>: public MpiBcastTest_base<bool> {
-  public:
-    // redefinition of bcast_array
-    void bcast_array() {
-        vector_type root_data=alps::testing::datapoint<vector_type>::get(true);
-        vector_type slave_data=alps::testing::datapoint<vector_type>::get(false);
-        std::size_t sz=std::min(root_data.size(), slave_data.size());
-        root_data.resize(sz);
-        slave_data.resize(sz);
-        boost::scoped_array<value_type> my_data(new value_type[sz]);
-        if (is_root_) {
-            std::copy(root_data.begin(), root_data.end(), my_data.get());
-        }
-        else {
-            std::copy(slave_data.begin(), slave_data.end(), my_data.get());
-        }
-        // Sanity check:
-        if (is_root_) {
-            ASSERT_TRUE(std::equal(root_data.begin(), root_data.end(), my_data.get())) << "Data mess-up on root";
-        } else {
-            ASSERT_FALSE(std::equal(root_data.begin(), root_data.end(), my_data.get())) << "Data mess-up on slave";
-        }
-    
-        am::broadcast(this->comm_, my_data.get(), sz, this->ROOT_);
-
-        EXPECT_TRUE(std::equal(root_data.begin(), root_data.end(), my_data.get()));
-    }        
-};   
 
 typedef ::testing::Types<bool,
                          char,
@@ -145,8 +140,8 @@ TYPED_TEST(MpiBcastTest, BcastArray) { this->bcast_array(); }
 int main(int argc, char** argv)
 {
     alps::mpi::environment env(argc, argv); // initializes MPI environment
-    // alps::gtest_par_xml_output tweak;
-    // tweak(alps::mpi::communicator().rank(), argc, argv);
+    alps::gtest_par_xml_output tweak;
+    tweak(alps::mpi::communicator().rank(), argc, argv);
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
