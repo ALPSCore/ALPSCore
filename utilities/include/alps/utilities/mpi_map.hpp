a6567a4e87efd0edf3a41b1516eb5bfa37a2f4d3
Alexander <galexv@umich.edu>
2017-02-14 01:00:34 -0500

    Implemented MPI bcast of std::vector<T>
    ...for T=bool, string, and primitive types.
    FIXME: No tests yet.

---
+++ utilities/include/alps/utilities/mpi_map.hpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright (C) 1998-2016 ALPS Collaboration. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ * For use in publications, see ACKNOWLEDGE.TXT
+ */
+
+/** @file mpi_map.hpp
+    
+    @brief Header for object-oriented interface to MPI for std::map
+*/
+
+#ifndef ALPS_UTILITIES_MPI_MAP_HPP_INCLUDED_b6555a13ab3b46c2a2b4a434a3484099
+#define ALPS_UTILITIES_MPI_MAP_HPP_INCLUDED_b6555a13ab3b46c2a2b4a434a3484099
+
+#include <alps/utilities/mpi.hpp>
+#include <map>
+#include <boost/foreach.hpp>
+
+namespace alps {
+    namespace mpi {
+
+        /// MPI_BCast of an std::map
+        /** @todo FIXME: does a series of broadcasts */
+        // FIXME: what is exception safety status?
+        // FIXME!: make a test
+        template <typename K, typename V>
+        inline void broadcast(const communicator& comm, std::map<K,V>& a_map, int root)
+        {
+            typedef std::map<K,V> map_type;
+            typedef typename map_type::value_type value_type;
+
+            std::size_t root_sz=a_map.size();
+            broadcast(comm, root_sz, root);
+              
+            if (comm.rank()==root) {
+                BOOST_FOREACH(value_type& pair, a_map) {
+                    broadcast(comm, const_cast<K&>(pair.first), root);
+                    broadcast(comm, pair.second, root);
+                }
+            } else {
+                map_type new_map;
+                while (root_sz--) {
+                    std::pair<K,V> pair;
+                    broadcast(comm, pair.first, root);
+                    broadcast(comm, pair.second, root);
+                    new_map.insert(pair);
+                }
+                using std::swap;
+                swap(a_map, new_map);
+            }
+        }
+
+    } // mpi::
+} // alps::
+
+#endif /* ALPS_UTILITIES_MPI_MAP_HPP_INCLUDED_b6555a13ab3b46c2a2b4a434a3484099 */
---
+++ utilities/include/alps/utilities/mpi_vector.hpp
@@ -0,0 +1,84 @@
+/*
+ * Copyright (C) 1998-2016 ALPS Collaboration. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ * For use in publications, see ACKNOWLEDGE.TXT
+ */
+
+/** @file mpi_vector.hpp
+    
+    @brief Header for object-oriented interface to MPI for vector data structures
+*/
+
+#ifndef ALPS_UTILITIES_MPI_VECTOR_HPP_INCLUDED_ba202cf9fb0040e493596702a707ca9b
+#define ALPS_UTILITIES_MPI_VECTOR_HPP_INCLUDED_ba202cf9fb0040e493596702a707ca9b
+
+#include <vector>
+#include <alps/utilities/mpi.hpp>
+#include <string>
+
+#include <boost/foreach.hpp>
+#include <boost/container/vector.hpp>
+
+namespace alps {
+    namespace mpi {
+
+        /// MPI_BCast of a vector of strings
+        // FIXME!: make a test
+        // FIXME: what is exception safety status?
+        // FIXME: implement generically for non-contiguous types
+        // FIXME: inline to have it header-only. A tad too complex to be inlined?
+        inline void broadcast(const communicator& comm, std::vector<std::string>& vec, int root)
+        {
+            // FIXME? it might be better to trade traffic to memory and first combine elements in a vector
+            using alps::mpi::broadcast;
+            std::size_t root_sz=vec.size();
+            broadcast(comm, root_sz, root);
+            if (comm.rank() != root) {
+                vec.resize(root_sz);
+            }
+            BOOST_FOREACH(std::string& elem, vec) {
+                broadcast(comm, elem, root);
+            }
+        }
+
+
+        /// MPI_BCast of a vector of (primitive) type T
+        // FIXME!: make a test
+        // FIXME: what is exception safety status?
+        // FIXME: verify that it is a "primitive" (or, at least, contiguous) type!
+        template <typename T>
+        inline void broadcast(const communicator& comm, std::vector<T>& vec, int root)
+        {
+            broadcast(comm, &vec[0], vec.size(), root);
+        }
+      
+        /// MPI_BCast of a vector of bool
+        // FIXME!: make a test
+        // FIXME: what is exception safety status?
+        // FIXME: implement generically for non-contiguous types
+        inline void broadcast(const communicator& comm, std::vector<bool>& vec, int root)
+        {
+            // FIXME? it might be better to trade traffic to memory and first combine elements in a vector
+            using alps::mpi::broadcast;
+            typedef std::vector<bool> vector_type;
+            typedef vector_type::iterator iter_type;
+            
+            std::size_t root_sz=vec.size();
+            broadcast(comm, root_sz, root);
+            if (comm.rank() != root) {
+                vec.resize(root_sz);
+            }
+
+            for (iter_type it=vec.begin(); it!=vec.end(); ++it) {
+                bool elem=*it;
+                broadcast(comm, elem, root);
+                *it=elem;
+            }
+        }
+      
+
+    } // mpi::
+} // alps::
+
+
+#endif /* ALPS_UTILITIES_MPI_VECTOR_HPP_INCLUDED_ba202cf9fb0040e493596702a707ca9b */
