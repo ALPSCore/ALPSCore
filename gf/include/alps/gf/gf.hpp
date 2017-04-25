ef2e6b9a59e268a3ed122f63e4d381a3971d4f4a
Emanuel Gull <egull@umich.edu>
2017-04-25 14:00:54 -0400

    seven index gf

--- gf/include/alps/gf/gf.hpp
+++ gf/include/alps/gf/gf.hpp
@@ -975,6 +975,9 @@ namespace alps {
           return os;
         }
 
+
+
+
         template<class VTYPE, class MESH1, class MESH2, class MESH3, class MESH4, class MESH5> class five_index_gf
             :boost::additive<five_index_gf<VTYPE,MESH1,MESH2,MESH3,MESH4,MESH5>,
              boost::multiplicative2<five_index_gf<VTYPE,MESH1,MESH2,MESH3,MESH4,MESH5>,VTYPE> > {
@@ -1147,7 +1150,7 @@ namespace alps {
             {
                 return do_op< detail::choose_rhs<value_type> >(rhs);
             }
-            
+
             /// Save the GF to HDF5
             void save(alps::hdf5::archive& ar, const std::string& path) const
             {
@@ -1197,27 +1200,299 @@ namespace alps {
                 detail::broadcast(comm, data_, root);
             }
 #endif
-            
-        };
-        template<class value_type, class MESH1, class MESH2, class MESH3, class MESH4, class MESH5> std::ostream &operator<<(std::ostream &os, five_index_gf<value_type,MESH1,MESH2,MESH3,MESH4,MESH5> G){
-          using detail::operator<<;
-          os<<G.mesh1()<<G.mesh2()<<G.mesh3()<<G.mesh4();
-          for(int i=0;i<G.mesh1().extent();++i){
-            os<<G.mesh1().points()[i]<<" ";
-            for(int j=0;j<G.mesh2().extent();++j){
-              for(int k=0;k<G.mesh3().extent();++k){
-                for(int l=0;l<G.mesh4().extent();++l){
-                  for(int m=0;m<G.mesh5().extent();++m){
-                  detail::print_no_complex<value_type>(os, G(typename MESH1::index_type(i),typename MESH2::index_type(j),typename MESH3::index_type(k),typename MESH4::index_type(l),typename MESH5::index_type(m))); os<<" ";
-                }
-                }
-              }
-            }
-            os<<std::endl;
-          }
-          return os;
-        }
 
+        };
+            template<class value_type, class MESH1, class MESH2, class MESH3, class MESH4, class MESH5> std::ostream &operator<<(std::ostream &os, five_index_gf<value_type,MESH1,MESH2,MESH3,MESH4,MESH5> G){
+              using detail::operator<<;
+              os<<G.mesh1()<<G.mesh2()<<G.mesh3()<<G.mesh4();
+              for(int i=0;i<G.mesh1().extent();++i){
+                os<<G.mesh1().points()[i]<<" ";
+                for(int j=0;j<G.mesh2().extent();++j){
+                  for(int k=0;k<G.mesh3().extent();++k){
+                    for(int l=0;l<G.mesh4().extent();++l){
+                      for(int m=0;m<G.mesh5().extent();++m){
+                      detail::print_no_complex<value_type>(os, G(typename MESH1::index_type(i),typename MESH2::index_type(j),typename MESH3::index_type(k),typename MESH4::index_type(l),typename MESH5::index_type(m))); os<<" ";
+                    }
+                    }
+                  }
+                }
+                os<<std::endl;
+              }
+              return os;
+            }
+
+            template<class VTYPE, class MESH1, class MESH2, class MESH3, class MESH4, class MESH5, class MESH6, class MESH7> class seven_index_gf
+                 :boost::additive<seven_index_gf<VTYPE,MESH1,MESH2,MESH3,MESH4,MESH5,MESH6,MESH7>,
+                  boost::multiplicative2<seven_index_gf<VTYPE,MESH1,MESH2,MESH3,MESH4,MESH5,MESH6,MESH7>,VTYPE> > {
+                 public:
+                 typedef VTYPE value_type;
+                 typedef boost::multi_array<value_type,7> container_type;
+                 typedef MESH1 mesh1_type;
+                 typedef MESH2 mesh2_type;
+                 typedef MESH3 mesh3_type;
+                 typedef MESH4 mesh4_type;
+                 typedef MESH5 mesh5_type;
+                 typedef MESH6 mesh6_type;
+                 typedef MESH7 mesh7_type;
+
+
+                 private:
+
+                 bool is_empty_;
+
+                 MESH1 mesh1_;
+                 MESH2 mesh2_;
+                 MESH3 mesh3_;
+                 MESH4 mesh4_;
+                 MESH5 mesh5_;
+                 MESH6 mesh6_;
+                 MESH7 mesh7_;
+
+                 container_type data_;
+
+                 /// Throws if gf is empty
+                 inline void throw_if_empty() const {
+                     if (is_empty_) {
+                         throw std::runtime_error("gf is empty");
+                     }
+                 }
+
+                 /// Check if meshes are compatible, throw if not
+                 void check_meshes(const seven_index_gf& rhs)
+                 {
+                     if (mesh1_!=rhs.mesh1_ ||
+                         mesh2_!=rhs.mesh2_ ||
+                         mesh3_!=rhs.mesh3_ ||
+                         mesh4_!=rhs.mesh4_ ||
+                         mesh5_!=rhs.mesh5_ ||
+                         mesh6_!=rhs.mesh6_ ||
+                         mesh7_!=rhs.mesh7_) {
+                         throw std::invalid_argument("Green Functions have incompatible meshes");
+                     }
+                 }
+
+                 public:
+                 seven_index_gf()
+                         : is_empty_(true), mesh1_(), mesh2_(), mesh3_(), mesh4_(),mesh5_(), mesh6_(),mesh7_(),data_()
+                 {
+                 }
+
+                 seven_index_gf(const MESH1& mesh1,
+                               const MESH2& mesh2,
+                               const MESH3& mesh3,
+                               const MESH4& mesh4,
+                               const MESH5& mesh5,
+                               const MESH6& mesh6,
+                               const MESH7& mesh7)
+                     : is_empty_(false), mesh1_(mesh1), mesh2_(mesh2), mesh3_(mesh3), mesh4_(mesh4),mesh5_(mesh5),mesh6_(mesh6),mesh7_(mesh7),
+                       data_(boost::extents[mesh1_.extent()][mesh2_.extent()][mesh3_.extent()][mesh4_.extent()][mesh5_.extent()][mesh6_.extent()][mesh7_.extent()])
+                 {
+                 }
+
+                 seven_index_gf(const MESH1& mesh1,
+                               const MESH2& mesh2,
+                               const MESH3& mesh3,
+                               const MESH4& mesh4,
+                               const MESH5& mesh5,
+                               const MESH5& mesh6,
+                               const MESH5& mesh7,
+                               const container_type& data)
+                     : is_empty_(false), mesh1_(mesh1), mesh2_(mesh2), mesh3_(mesh3), mesh4_(mesh4), mesh5_(mesh5), mesh6_(mesh6), mesh7_(mesh7),
+                       data_(data)
+                 {
+                     if (mesh1_.extent()!=data_.shape()[0] || mesh2_.extent()!=data_.shape()[1] ||
+                         mesh3_.extent()!=data_.shape()[2] || mesh4_.extent()!=data_.shape()[3] ||
+                         mesh5_.extent()!=data_.shape()[4] || mesh6_.extent()!=data_.shape()[5] || mesh7_.extent()!=data_.shape()[6])
+                         throw std::invalid_argument("Initialization of GF with the data of incorrect size");
+                 }
+
+                 const MESH1& mesh1() const { return mesh1_; }
+                 const MESH2& mesh2() const { return mesh2_; }
+                 const MESH3& mesh3() const { return mesh3_; }
+                 const MESH4& mesh4() const { return mesh4_; }
+                 const MESH5& mesh5() const { return mesh5_; }
+                 const MESH6& mesh6() const { return mesh6_; }
+                 const MESH7& mesh7() const { return mesh7_; }
+                 const container_type& data() const { return data_; }
+
+                 const value_type& operator()(
+                     typename MESH1::index_type i1, typename MESH2::index_type i2, typename MESH3::index_type i3,
+                     typename MESH4::index_type i4, typename MESH5::index_type i5, typename MESH6::index_type i6, typename MESH7::index_type i7) const
+                 {
+                     return data_[mesh1_(i1)][mesh2_(i2)][mesh3_(i3)][mesh4_(i4)][mesh5_(i5)][mesh6_(i6)][mesh7_(i7)];
+                 }
+
+                 value_type& operator()(typename MESH1::index_type i1, typename MESH2::index_type i2, typename MESH3::index_type i3,
+                     typename MESH4::index_type i4, typename MESH5::index_type i5, typename MESH6::index_type i6, typename MESH7::index_type i7)
+                 {
+                     return data_[mesh1_(i1)][mesh2_(i2)][mesh3_(i3)][mesh4_(i4)][mesh5_(i5)][mesh6_(i6)][mesh7_(i7)];
+                 }
+
+
+                 /// Initialize the GF data to value_type(0.)
+                 void initialize()
+                 {
+                     for (int i=0; i<mesh1_.extent(); ++i) {
+                         for (int j=0; j<mesh2_.extent(); ++j) {
+                             for (int k=0; k<mesh3_.extent(); ++k) {
+                               for (int l=0; l<mesh4_.extent(); ++l) {
+                                 for (int m=0; m<mesh5_.extent(); ++m) {
+                                   for (int n=0; n<mesh6_.extent(); ++n) {
+                                     for (int o=0; o<mesh7_.extent(); ++o) {
+                                     data_[i][j][k][l][m][n][o]=value_type(0.0);
+                                     }
+                                   }
+                                 }
+                               }
+                             }
+                         }
+                     }
+                 }
+                 double norm() const
+                 {
+                     throw_if_empty();
+                     using std::abs;
+                     double v=0;
+                     for (const value_type* ptr=data_.origin(); ptr!=data_.origin()+data_.num_elements(); ++ptr) {
+                         v=std::max(abs(*ptr), v);
+                     }
+                     return v;
+                 }
+                 /// Assignment-op
+                 template <typename op>
+                 seven_index_gf& do_op(const seven_index_gf& rhs)
+                 {
+                     throw_if_empty();
+                     check_meshes(rhs);
+                     std::transform(data_.origin(), data_.origin()+data_.num_elements(), rhs.data_.origin(), // inputs
+                                    data_.origin(), // output
+                                    op());
+
+                     return *this;
+                 }
+
+                 /// Element-wise addition
+                 seven_index_gf& operator+=(const seven_index_gf& rhs)
+                 {
+                     return do_op< std::plus<value_type> >(rhs);
+                 }
+
+                 /// Element-wise subtraction
+                 seven_index_gf& operator-=(const seven_index_gf& rhs)
+                 {
+                     return do_op< std::minus<value_type> >(rhs);
+                 }
+                 /// Assignment-op with scalar
+                 template <typename op>
+                 seven_index_gf& do_op(const value_type& scalar)
+                 {
+
+                     throw_if_empty();
+                     std::transform(data_.origin(), data_.origin()+data_.num_elements(), // inputs
+                                    data_.origin(), // output
+                                    std::bind2nd(op(), scalar)); // bound binary(?,scalar)
+
+                     return *this;
+                 }
+                 /// Element-wise scaling
+                 seven_index_gf& operator*=(const value_type& scalar)
+                 {
+                     return do_op< std::multiplies<value_type> >(scalar);
+                 }
+
+                 /// Element-wise scaling
+                 seven_index_gf& operator/=(const value_type& scalar)
+                 {
+                     return do_op< std::divides<value_type> >(scalar);
+                 }
+
+                 /// Element-wise assignment
+                 /** @Note Copies only the data, requires identical grids. */
+                 // FIXME: this is a hack, to be replaced by a proper assignment later
+                 seven_index_gf& operator=(const seven_index_gf& rhs)
+                 {
+                     return do_op< detail::choose_rhs<value_type> >(rhs);
+                 }
+
+                 /// Save the GF to HDF5
+                 void save(alps::hdf5::archive& ar, const std::string& path) const
+                 {
+                     throw_if_empty();
+                     save_version(ar,path);
+                     ar[path+"/data"] << data_;
+                     ar[path+"/mesh/N"] << int(container_type::dimensionality);
+                     mesh1_.save(ar,path+"/mesh/1");
+                     mesh2_.save(ar,path+"/mesh/2");
+                     mesh3_.save(ar,path+"/mesh/3");
+                     mesh4_.save(ar,path+"/mesh/4");
+                     mesh5_.save(ar,path+"/mesh/5");
+                     mesh6_.save(ar,path+"/mesh/6");
+                     mesh7_.save(ar,path+"/mesh/7");
+                 }
+
+                 /// Load the GF from HDF5
+                 void load(alps::hdf5::archive& ar, const std::string& path)
+                 {
+                     if (!check_version(ar,path)) throw std::runtime_error("Incompatible archive version");
+
+                     is_empty_ = false;
+
+                     int ndim;
+                     ar[path+"/mesh/N"] >> ndim;
+                     if (ndim != container_type::dimensionality) throw std::runtime_error("Wrong number of dimension reading Matsubara GF, ndim="+boost::lexical_cast<std::string>(ndim));
+
+                     mesh1_.load(ar,path+"/mesh/1");
+                     mesh2_.load(ar,path+"/mesh/2");
+                     mesh3_.load(ar,path+"/mesh/3");
+                     mesh4_.load(ar,path+"/mesh/4");
+                     mesh5_.load(ar,path+"/mesh/5");
+                     mesh6_.load(ar,path+"/mesh/6");
+                     mesh7_.load(ar,path+"/mesh/7");
+
+                     data_.resize(boost::extents[mesh1_.extent()][mesh2_.extent()][mesh3_.extent()][mesh4_.extent()][mesh5_.extent()][mesh6_.extent()][mesh7_.extent()]);
+
+                     ar[path+"/data"] >> data_;
+                 }
+
+     #ifdef ALPS_HAVE_MPI
+                 /// Broadcast the GF (with meshes)
+               void broadcast(const alps::mpi::communicator& comm, int root)
+                 {
+                     throw_if_empty();
+                     mesh1_.broadcast(comm,root);
+                     mesh2_.broadcast(comm,root);
+                     mesh3_.broadcast(comm,root);
+                     mesh4_.broadcast(comm,root);
+                     mesh5_.broadcast(comm,root);
+                     mesh6_.broadcast(comm,root);
+                     mesh7_.broadcast(comm,root);
+                     detail::broadcast(comm, data_, root);
+                 }
+     #endif
+
+             };
+             template<class value_type, class MESH1, class MESH2, class MESH3, class MESH4, class MESH5, class MESH6, class MESH7> std::ostream &operator<<(std::ostream &os, seven_index_gf<value_type,MESH1,MESH2,MESH3,MESH4,MESH5,MESH6,MESH7> G){
+               using detail::operator<<;
+               os<<G.mesh1()<<G.mesh2()<<G.mesh3()<<G.mesh4();
+               for(int i=0;i<G.mesh1().extent();++i){
+                 os<<G.mesh1().points()[i]<<" ";
+                 for(int j=0;j<G.mesh2().extent();++j){
+                   for(int k=0;k<G.mesh3().extent();++k){
+                     for(int l=0;l<G.mesh4().extent();++l){
+                       for(int m=0;m<G.mesh5().extent();++m){
+                         for(int n=0;n<G.mesh6().extent();++n){
+                           for(int o=0;o<G.mesh5().extent();++o){
+                       detail::print_no_complex<value_type>(os, G(typename MESH1::index_type(i),typename MESH2::index_type(j),typename MESH3::index_type(k),typename MESH4::index_type(l),typename MESH5::index_type(m),typename MESH5::index_type(n),typename MESH5::index_type(o))); os<<" ";
+                     }
+                     }
+                       }
+                     }
+                   }
+                 }
+                 os<<std::endl;
+               }
+               return os;
+             }
         typedef five_index_gf<std::complex<double>, matsubara_mesh<mesh::POSITIVE_ONLY>, real_space_index_mesh,real_space_index_mesh, index_mesh, index_mesh> omega_r1_r2_sigma1_sigma2_gf;
         typedef five_index_gf<             double , itime_mesh    , real_space_index_mesh,real_space_index_mesh, index_mesh, index_mesh> itime_r1_r2_sigma1_sigma2_gf;
 
