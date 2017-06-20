e5fbbf433179356d9abe9da1493a73a3863f124d
Alexander <galexv@umich.edu>
2017-06-20 10:02:19 -0400

    Re-introduced generating temporary filenames
    Some codes seem to use this function, so it is brought back.
    However, it may go away again in a future version.

---
+++ utilities/include/alps/utilities/temporary_filename.hpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 1998-2017 ALPS Collaboration. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ * For use in publications, see ACKNOWLEDGE.TXT
+ */
+
+#ifndef ALPS_UTILITIES_TEMPORARY_FILENAME_HPP_ae2c8128ef7647a8827212faaab05d00
+#define ALPS_UTILITIES_TEMPORARY_FILENAME_HPP_ae2c8128ef7647a8827212faaab05d00
+
+#include <alps/testing/unique_file.hpp>
+
+namespace alps {
+    /// Generate a reasonably unique file name with a given prefix
+    /**
+       @note There is no guarantee that the name rename unique by the
+       time it is used.
+
+       @note The file is actually created (then removed), avoid
+       generating many temporary file names in a loop.
+    */
+    inline std::string temporary_filename(const std::string& prefix)
+    {
+        return alps::testing::unique_file(prefix, alps::testing::unique_file::REMOVE_AND_DISOWN).name();
+    }
+}
+#endif /* ALPS_UTILITIES_TEMPORARY_FILENAME_HPP_ae2c8128ef7647a8827212faaab05d00 */
+
