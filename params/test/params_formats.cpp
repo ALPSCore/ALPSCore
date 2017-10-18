c9f65fc80a9c63502f967c1ac156b346feea9c88
Alexander <galexv@umich.edu>
2017-10-17 21:51:46 -0400

    Started testing various input formats for params

---
+++ params/test/params_formats.cpp
@@ -0,0 +1,98 @@
+/*
+ * Copyright (C) 1998-2017 ALPS Collaboration. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ * For use in publications, see ACKNOWLEDGE.TXT
+ */
+
+/** @file params_formats.cpp
+    
+    @brief Tests various input formats fro parameters
+*/
+
+#include <boost/foreach.hpp>
+
+#include "./params_test_support.hpp"
+
+namespace ap=alps::params_new_ns;
+namespace de=ap::exception;
+using ap::params;
+
+namespace test_data {
+    static const char inifile_content[]=
+        "my_true=true\n"
+        "my_false=false\n"
+        "my_long=8589934593\n" // 2^33+1
+        "my_float=1.25\n"
+        "my_double=2.75\n"
+        "my_bool_vec=true,false\n"
+        "my_int_vec=1,2,3,4,5\n"
+        "my_long_vec=1,2,8589934593\n"
+        "my_double_vec=1.25,2.75\n"
+        "my_true_pair=key:true\n"
+        "my_false_pair=key:false\n"
+        "my_int_pair=key:10\n"
+        "my_long_pair=key:8589934593\n" // 2^33+1
+        "my_double_pair=key:2.75\n"
+        ;
+}
+
+template <typename T, int K>
+struct my_data;
+
+#define MAKE_SOURCE2(__name__,__type__,__value__,__kind__)      \
+template <>                                              \
+struct my_data<__type__,__kind__> {                      \
+    typedef __type__ value_type;                         \
+    static __type__ get() { return __value__; }          \
+    static std::string name() { return #__name__; }     \
+};                                                       \
+typedef my_data<__type__,__kind__> __name__;
+
+#define MAKE_SOURCE(__name__, __type__,__value__) MAKE_SOURCE2(__name__,__type__,__value__,0)
+
+MAKE_SOURCE2(my_true, bool,true, 1)
+MAKE_SOURCE2(my_false, bool,false, 2)
+
+MAKE_SOURCE(my_long, long, 8589934593)
+MAKE_SOURCE(my_float, float, 1.25)
+MAKE_SOURCE(my_double, double, 2.75)
+
+template <typename T>
+class ParamsTest1 : public testing::Test {
+  protected:
+    ParamsAndFile params_and_file_;
+    params& par_;
+    const params& cpar_;
+
+    typedef T generator_type;
+    typedef typename generator_type::value_type value_type;
+  public:
+    ParamsTest1() : params_and_file_(::test_data::inifile_content),
+                    par_(*params_and_file_.get_params_ptr()),
+                    cpar_(par_)
+    {    }
+
+    void read() {
+        std::string name=generator_type::name();
+        value_type expected=generator_type::get();
+        EXPECT_TRUE(par_.define<value_type>(name, "parameter"));
+        value_type actual=cpar_[name];
+        EXPECT_EQ(expected,actual);
+    }
+};
+
+typedef ::testing::Types<
+    my_true
+    ,
+    my_false
+    ,
+    my_long
+    ,
+    my_float
+    ,
+    my_double
+    > MyTypes;
+
+TYPED_TEST_CASE(ParamsTest1, MyTypes);
+
+TYPED_TEST(ParamsTest1, read) { this->read(); }
