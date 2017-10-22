5ed81813f4bb71e393ea2f4f6ab375f88aa12818
Alexander <galexv@umich.edu>
2017-10-21 20:58:30 -0400

    alps::dict test generator helper now tests object types

--- params/test/params_gen_equality_test_helper.py
+++ params/test/params_gen_equality_test_helper.py
@@ -5,11 +5,15 @@ from itertools import \
     combinations_with_replacement as tri_prod, \
     product
 
-domains=(('neg_long',),
-         ('neg_int', 'neg_long_is'),
-         ('pos_int', 'uint_is', 'pos_long_is', 'ulong_is'),
-         ('pos_long', 'ulong_ls'),
-         ('ulong',))
+num_domains=(('neg_long',),
+             ('neg_int', 'neg_long_is'),
+             ('pos_int', 'uint_is', 'pos_long_is', 'ulong_is'),
+             ('pos_long', 'ulong_ls'),
+             ('ulong',))
+
+fp_types=('my_float', 'my_double')
+
+incompat_types=('my_bool', 'my_int', 'my_string', 'my_vec', 'my_pair')
 
 # Mapping between domain names and function name parts
 dom_name={
@@ -17,7 +21,14 @@ dom_name={
     'neg_int' : 'NegInt',
     'pos_int' : 'PosInt',
     'pos_long' : 'PosLong',
-    'ulong' : 'ULong' }
+    'ulong' : 'ULong',
+    'my_int' : 'Int',
+    'my_string' : 'String',
+    'my_bool'  : 'Bool',
+    'my_vec' : 'Vec',
+    'my_pair' : 'Pair',
+    'my_float' : 'Float',
+    'my_double' : 'Double'}
 
 def get_refs(v): return (v, 'p["%s"]'%v)
 
@@ -26,28 +37,60 @@ def get_ref_pairs(lhs,rhs):
     p.__next__()
     return p
 
-for lhs_domain, rhs_domain in tri_prod(domains, 2):
-    if lhs_domain is rhs_domain:
-        print("// Equalities within domain %s" % lhs_domain[0])
-        print("TEST_F(MyTest, eq%s) {" % dom_name[lhs_domain[0]])
-        for lhs_val, rhs_val in tri_prod(lhs_domain, 2):
+def generate_num_equalities():
+    for lhs_domain, rhs_domain in tri_prod(num_domains, 2):
+        if lhs_domain is rhs_domain:
+            print("// Equalities within domain %s" % lhs_domain[0])
+            print("TEST_F(MyTest, eq%s) {" % dom_name[lhs_domain[0]])
+            for lhs_val, rhs_val in tri_prod(lhs_domain, 2):
+                print("    // Same values:")
+                for lhs,rhs in get_ref_pairs(lhs_val,rhs_val):
+                    print('    EXPECT_TRUE(  %s==%s );' % (lhs,rhs))
+                    print('    EXPECT_FALSE( %s!=%s );' % (lhs,rhs))
+    
+                print("    // Different values:")
+                for lhs,rhs in get_ref_pairs(lhs_val,rhs_val+"1"):
+                    print('    EXPECT_TRUE(  %s!=%s );' % (lhs,rhs))
+                    print('    EXPECT_FALSE( %s==%s );' % (lhs,rhs))
+        else:
+            print("// Equalities between domains %s:%s" % (lhs_domain[0],rhs_domain[0]))
+            print("TEST_F(MyTest, eq%s%s) {" % (dom_name[lhs_domain[0]], dom_name[rhs_domain[0]]))
+            for lhs_val, rhs_val in product(lhs_domain, rhs_domain):
+                for lhs, rhs in get_ref_pairs(lhs_val,rhs_val):
+                    print('    EXPECT_TRUE(  %s!=%s );' % (lhs,rhs))
+                    print('    EXPECT_FALSE( %s==%s );' % (lhs,rhs))
+    
+        # endif
+        print("}\n")
+    # endfor
+    return
+
+def generate_obj_equalities():
+    for lhs_type, rhs_type in tri_prod(incompat_types, 2):
+        if lhs_type is rhs_type:
+            print("// Equalities within same type %s" % lhs_type)
+            print("TEST_F(MyTest, eq%s) {" % dom_name[lhs_type])
             print("    // Same values:")
-            for lhs,rhs in get_ref_pairs(lhs_val,rhs_val):
+            for lhs,rhs in get_ref_pairs(lhs_type,rhs_type):
                 print('    EXPECT_TRUE(  %s==%s );' % (lhs,rhs))
                 print('    EXPECT_FALSE( %s!=%s );' % (lhs,rhs))
-
+    
             print("    // Different values:")
-            for lhs,rhs in get_ref_pairs(lhs_val,rhs_val+"1"):
+            for lhs,rhs in get_ref_pairs(lhs_type,rhs_type+"1"):
                 print('    EXPECT_TRUE(  %s!=%s );' % (lhs,rhs))
                 print('    EXPECT_FALSE( %s==%s );' % (lhs,rhs))
-    else:
-        print("// Equalities between domains %s:%s" % (lhs_domain[0],rhs_domain[0]))
-        print("TEST_F(MyTest, eq%s%s) {" % (dom_name[lhs_domain[0]], dom_name[rhs_domain[0]]))
-        for lhs_val, rhs_val in product(lhs_domain, rhs_domain):
-            for lhs, rhs in get_ref_pairs(lhs_val,rhs_val):
+        else:
+            print("// Equalities between different types %s:%s" % (lhs_type,rhs_type))
+            print("TEST_F(MyTest, eq%s%s) {" % (dom_name[lhs_type],dom_name[rhs_type]))
+            for lhs, rhs in get_ref_pairs(lhs_type,rhs_type):
                 print('    EXPECT_TRUE(  %s!=%s );' % (lhs,rhs))
                 print('    EXPECT_FALSE( %s==%s );' % (lhs,rhs))
+            
 
-    # endif
-    print("}\n")
-# endfor
+        print("}\n")
+            
+
+
+# generate_num_equalities()
+generate_obj_equalities()
+
