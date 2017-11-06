0ca11fde54f1ff2d27a5d32d1b7b98ba3d1911da
Alexander <galexv@umich.edu>
2017-11-06 14:07:39 -0500

    Increased timeouts in Eigen3 downloading

--- common/cmake/ALPSEnableEigen.cmake
+++ common/cmake/ALPSEnableEigen.cmake
@@ -62,10 +62,10 @@ function(add_eigen)
     if (NOT EIGEN3_INCLUDE_DIR)
       message(STATUS "Trying to download and unpack Eigen3")
       if (NOT EXISTS "${ALPS_EIGEN_TGZ_FILE}")
-        message(STATUS "Downloading Eigen3, timeout 30 sec")
+        message(STATUS "Downloading Eigen3, timeout 600 sec")
         file(DOWNLOAD ${ALPS_EIGEN_DOWNLOAD_LOCATION} ${ALPS_EIGEN_TGZ_FILE}
-          INACTIVITY_TIMEOUT 30
-          TIMEOUT 30
+          INACTIVITY_TIMEOUT 60
+          TIMEOUT 600
           STATUS status_
           SHOW_PROGRESS)
         if (status_ EQUAL 0)
