06fe2005d0e6a7684ee6f7b943dd33f38730e94e
Alexander <galexv@umich.edu>
2018-01-22 13:53:39 -0500

    Converted "death test" to a regular test for acc concurrent access
    This should resolve issue #372

--- accumulators/test/concurrent_access.cpp
+++ accumulators/test/concurrent_access.cpp
@@ -23,20 +23,10 @@ static bool counting_thread() {
     return 3u==count(acc);
 }
 
-TEST(accumulatorsDeathTest, ConcurrentAccCtor) {
-    ASSERT_EXIT({
-            auto r1_future=std::async(std::launch::async,counting_thread);
-            auto r2_future=std::async(std::launch::async,counting_thread);
+TEST(accumulators, ConcurrentAccCtor) {
+    auto r1_future=std::async(std::launch::async,counting_thread);
+    auto r2_future=std::async(std::launch::async,counting_thread);
 
-            if (!r1_future.get()) {
-                std::cerr << "Thread 1 failed";
-                exit(1);
-            }
-            if (!r2_future.get()) {
-                std::cerr << "Thread 2 failed";
-                exit(2);
-            }
-            std::cerr << "Success";
-            exit(0);
-        }, ::testing::ExitedWithCode(0), "Success");
+    EXPECT_TRUE(r1_future.get()) << "Thread 1 failed";
+    EXPECT_TRUE(r2_future.get()) << "Thread 2 failed";
 }
