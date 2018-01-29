4ce9c65d0baa81d27142e6db1a840c4bab8eed9a
Markus Wallerberger <mwallerb@umich.edu>
2018-01-29 18:35:13 -0500

    Enables joining of different types (maybe)

--- alea/include/alps/alea/internal/joined.hpp
+++ alea/include/alps/alea/internal/joined.hpp
@@ -29,42 +29,44 @@ namespace alps { namespace alea { namespace internal {
 template <typename T1, typename T2>
 constexpr bool joins_batch()
 {
-    return std::is_same<typename T1::value_type, typename T2::value_type>::value
-        && T1::HAVE_BATCH && T2::HAVE_BATCH;
+    return T1::HAVE_BATCH && T2::HAVE_BATCH;
 }
 
 template <typename T1, typename T2>
 constexpr bool joins_autocorr()
 {
-    return std::is_same<typename T1::value_type, typename T2::value_type>::value
-        && T1::HAVE_TAU && T2::HAVE_TAU;
+    return T1::HAVE_TAU && T2::HAVE_TAU;
 }
 
 template <typename T1, typename T2>
 constexpr bool joins_cov()
 {
-    return std::is_same<typename T1::value_type, typename T2::value_type>::value
-           && T1::HAVE_COV && T2::HAVE_COV
-           && !(T1::HAVE_BATCH && T2::HAVE_BATCH);
+    return T1::HAVE_COV && T2::HAVE_COV
+        && !(T1::HAVE_BATCH && T2::HAVE_BATCH);
 }
 
 template <typename T1, typename T2>
 constexpr bool joins_var()
 {
-    return std::is_same<typename T1::value_type, typename T2::value_type>::value
-           && (T1::HAVE_VAR && T2::HAVE_VAR)
-           && !(T1::HAVE_COV && T2::HAVE_COV)
-           && !(T1::HAVE_TAU && T2::HAVE_TAU)
-           && !(T1::HAVE_BATCH && T2::HAVE_BATCH);
+    return (T1::HAVE_VAR && T2::HAVE_VAR)
+        && !(T1::HAVE_COV && T2::HAVE_COV)
+        && !(T1::HAVE_TAU && T2::HAVE_TAU)
+        && !(T1::HAVE_BATCH && T2::HAVE_BATCH);
 }
 
 template <typename T1, typename T2>
 constexpr bool joins_mean()
 {
-    return std::is_same<typename T1::value_type, typename T2::value_type>::value
-           && T1::HAVE_MEAN && T2::HAVE_MEAN
-           && !(T1::HAVE_VAR && T2::HAVE_VAR);
+    return T1::HAVE_MEAN && T2::HAVE_MEAN
+        && !(T1::HAVE_VAR && T2::HAVE_VAR);
 }
+
+template <typename R1, typename R2>
+struct joined_value
+{
+    typedef decltype(typename traits<R1>::value_type(0)
+                   + typename traits<R2>::value_type(0)) type;
+};
 
 /**
  * Determines the "greatest common denominator" type when combining results.
@@ -76,21 +78,21 @@ template <typename R1, typename R2>
 struct joined<R1, R2,
         typename std::enable_if<joins_batch<traits<R1>, traits<R2> >()>::type>
 {
-    typedef batch_result<typename traits<R1>::value_type> result_type;
+    typedef batch_result<typename joined_value<R1, R2>::type> result_type;
 };
 
 template <typename R1, typename R2>
 struct joined<R1, R2,
         typename std::enable_if<joins_autocorr<traits<R1>, traits<R2> >()>::type>
 {
-    typedef autocorr_result<typename traits<R1>::value_type> result_type;
+    typedef autocorr_result<typename joined_value<R1, R2>::type> result_type;
 };
 
 template <typename R1, typename R2>
 struct joined<R1, R2,
         typename std::enable_if<joins_cov<traits<R1>, traits<R2> >()>::type>
 {
-    typedef cov_result<typename traits<R1>::value_type,
+    typedef cov_result<typename joined_value<R1, R2>::type,
                        typename traits<R1>::strategy_type> result_type;
 };
 
@@ -98,7 +100,7 @@ template <typename R1, typename R2>
 struct joined<R1, R2,
         typename std::enable_if<joins_var<traits<R1>, traits<R2> >()>::type>
 {
-    typedef var_result<typename traits<R1>::value_type,
+    typedef var_result<typename joined_value<R1, R2>::type,
                        typename traits<R1>::strategy_type> result_type;
 };
 
@@ -106,7 +108,7 @@ template <typename R1, typename R2>
 struct joined<R1, R2,
         typename std::enable_if<joins_mean<traits<R1>, traits<R2> >()>::type>
 {
-    typedef mean_result<typename traits<R1>::value_type> result_type;
+    typedef mean_result<typename joined_value<R1, R2>::type> result_type;
 };
 
 }}}   /* namespace alps::alea::internal */
