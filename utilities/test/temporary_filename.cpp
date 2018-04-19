2985d02b115545fe649a094d942ba6b8a0d1f00f
Alexander <galexv@umich.edu>
2018-04-19 11:16:26 -0400

    Hotfix for an error creating a temp file in params

--- utilities/test/temporary_filename.cpp
+++ utilities/test/temporary_filename.cpp
@@ -12,19 +12,19 @@
 
 TEST(temporary_fname, main)
 {
-    std::string prefix="alps_temp_filename_test";
+    std::string prefix="./alps_temp_filename_test";
     // NOTE: those files are actually *created*, so be careful with how many of them you generate
     const unsigned int n_uniq_names=(1<<7); // how many uniq names to create
 
     typedef std::set<std::string> set_type;
     typedef set_type::iterator iter_type;
     typedef std::pair<iter_type,bool> res_type;
-    
+
     set_type nameset;
     for (unsigned int i=0; i<n_uniq_names; ++i) {
         std::string fname=alps::temporary_filename(prefix);
         ASSERT_EQ(prefix, fname.substr(0,prefix.size()));
-        
+
         res_type result=nameset.insert(fname);
         ASSERT_TRUE(result.second) << "Clash for i=" << i << " name=" << fname;
     }
