86acd9171f7947c253a8e97493667d0f50c1d4fe
Sergei Iskakov <sir.iskakoff@gmail.com>
2018-07-08 09:38:03 -0400

    Update mesh.hpp
    Points in real space mesh will be zero when do copy-assignment. Also the same for Legendre

--- gf/include/alps/gf/mesh.hpp
+++ gf/include/alps/gf/mesh.hpp
@@ -799,7 +799,7 @@ namespace alps {
             public:
 
             typedef generic_index<momentum_index_mesh> index_type;
-            real_space_index_mesh(const real_space_index_mesh& rhs) : base_type(rhs.kind(), rhs.extent(), rhs.dimension()) {}
+            real_space_index_mesh(const real_space_index_mesh& rhs) : base_type(rhs) {}
 
             real_space_index_mesh(): base_type("REAL_SPACE_INDEX",0,0)
             {
@@ -909,7 +909,7 @@ namespace alps {
 
         public:
             typedef generic_index<legendre_mesh> index_type;
-            legendre_mesh(const legendre_mesh& rhs) : beta_(rhs.beta_), n_max_(rhs.n_max_), statistics_(rhs.statistics_) {}
+            legendre_mesh(const legendre_mesh& rhs) : beta_(rhs.beta_), n_max_(rhs.n_max_), statistics_(rhs.statistics_) {compute_points();}
             legendre_mesh(gf::statistics::statistics_type statistics=statistics::FERMIONIC):
                     beta_(0.0), n_max_(0), statistics_(statistics) {}
 
