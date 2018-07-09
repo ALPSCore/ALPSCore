787e03a929e383394f85b373298c50288195c064
Alexander <galexv@umich.edu>
2018-07-09 12:09:07 -0400

    TravisCI: Download Boost outside source dir
    ...and fix an error in .travis.yml.

--- common/build/build.travis.sh
+++ common/build/build.travis.sh
@@ -5,19 +5,20 @@
 set -ev
 
 # Explicitly download Boost, if a specific version is requested
-boost_no_system_paths=false
+boost_cmake_params=""
 if [ -n "$ALPS_BOOST_VERSION" ]; then
-  boost_tgz=boost_${ALPS_BOOST_VERSION}.tar.gz
+  download_dir=$HOME/boost
+  mkdir -pv $download_dir
+  boost_tgz=$download_dir/boost_${ALPS_BOOST_VERSION}.tar.gz
   boost_url=https://sourceforge.net/projects/boost/files/boost/${ALPS_BOOST_VERSION//_/.}/boost_${ALPS_BOOST_VERSION}.tar.gz/download
   wget -S -O $boost_tgz $boost_url
-  tar -xzf $boost_tgz
-  export BOOST_ROOT=$PWD/boost_${ALPS_BOOST_VERSION}
-  boost_no_system_paths=true
+  tar -C $download_dir -xzf $boost_tgz
+  boost_cmake_params="-DBoost_NO_SYSTEM_PATHS=true -DBoost_NO_BOOST_CMAKE=true -DBOOST_ROOT=$download_dir/boost_${ALPS_BOOST_VERSION}"
 fi
 
 # Build ALPSCore
-mkdir build
-mkdir install
+mkdir -pv build
+mkdir -pv install
 cd build
 cmake ..                                              \
 -DCMAKE_BUILD_TYPE=Debug                              \
@@ -29,7 +30,8 @@ cmake ..                                              \
 -DALPS_BUNDLE_DOWNLOAD_TRIES=3                        \
 -DMPIEXEC=mpiexec -DMPIEXEC_NUMPROC_FLAG='-n'         \
 -DENABLE_MPI=$ENABLE_MPI                              \
--DBoost_NO_SYSTEM_PATHS=${boost_no_system_paths}
+${boost_cmake_params}
+
 make -j3
 env ALPS_TEST_MPI_NPROC=3 make test
 make install
