ffd1d55298afe26ba526980d514490e6d1f9d56b
Alexander <galexv@umich.edu>
2018-07-23 11:07:16 -0400

    Added/fixed 2018 copyright notice

--- alea/test/mpi_twogauss.cpp
+++ alea/test/mpi_twogauss.cpp
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 1998-2018 ALPS Collaboration. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ * For use in publications, see ACKNOWLEDGE.TXT
+ */
 #include <alps/alea/mpi.hpp>
 #include <alps/alea/mean.hpp>
 #include <alps/alea/variance.hpp>
--- alea/test/stream_serializer.cpp
+++ alea/test/stream_serializer.cpp
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 1998-2018 ALPS Collaboration. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ * For use in publications, see ACKNOWLEDGE.TXT
+ */
 #include <alps/alea/plugin/stream_serializer.hpp>
 
 #include "gtest/gtest.h"
