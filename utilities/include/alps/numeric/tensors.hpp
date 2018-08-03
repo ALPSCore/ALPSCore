4bca3ac5efc5f622b9920b0c36e58cc3d5d539c7
iskakoff <sir.iskakoff@gmail.com>
2018-08-03 02:32:55 -0400

    cleanup

--- utilities/include/alps/numeric/tensors.hpp
+++ utilities/include/alps/numeric/tensors.hpp
@@ -1,6 +1,8 @@
-//
-// Created by Sergei on 8/2/18.
-//
+/*
+ * Copyright (C) 1998-2018 ALPS Collaboration. See COPYRIGHT.TXT
+ * All rights reserved. Use is subject to license terms. See LICENSE.TXT
+ * For use in publications, see ACKNOWLEDGE.TXT
+ */
 
 #ifndef ALPSCORE_TENSORS_HPP
 #define ALPSCORE_TENSORS_HPP
