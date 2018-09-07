541e1f83a988bbccb20330fb2dad431121870b18
Alexander <galexv@umich.edu>
2018-09-07 19:22:52 -0400

    Added test for params after h5 file in cmdline

--- params/test/params_hdf5.cpp
+++ params/test/params_hdf5.cpp
@@ -5,7 +5,7 @@
  */
 
 /** @file params_hdf5.cpp
-    
+
     @brief Tests saving/loading of parameters
 */
 
@@ -35,7 +35,7 @@ class ParamsTest : public ::testing::Test {
     ParamsTest() : params_and_file_(::test_data::inifile_content),
                    par_(*params_and_file_.get_params_ptr()),
                    file_("params_hdf5_test.h5.", alps::testing::unique_file::REMOVE_AFTER/*KEEP_AFTER*/)
-                   
+
     {   }
 };
 
@@ -81,7 +81,7 @@ TEST_F(ParamsTest, h5Ctor) {
     }
     arg_holder args;
     args.add(file_.name());
-    
+
     params p_new(args.argc(), args.argv());
 
     EXPECT_EQ(par_, p_new);
@@ -93,15 +93,49 @@ TEST_F(ParamsTest, h5Ctor) {
     EXPECT_EQ(file_.name(), p_new.get_archive_name());
 }
 
-TEST_F(ParamsTest, h5CtorExtraArgs) {
+// struct define_visitor {
+//     params& p_;
+//     const std::string& name_;
+//     const std::string& descr_;
+
+//     define_visitor(params& p, const std::string& name, const std::string& descr): p_(p), name_(name), descr_(descr) {}
+//     typedef void result_type;
+//     template <typename T>
+//     void operator()(const T& val) {
+//         p_.define<T>(name_, descr_, val);
+//     }
+// };
+
+TEST_F(ParamsTest, h5CtorOverride) {
     {
         ah5::archive ar(file_.name(), "w");
+        par_
+            .define<bool>("my_bool","Boolean")
+            .define<int>("my_int", "Integer")
+            .define<std::string>("my_string", "String")
+            .define<double>("my_double", "Double");
         ar["/parameters"] << par_;
     }
     arg_holder args;
-    args.add(file_.name()).add("some=something");
-    
-    EXPECT_ANY_THROW(params p_new(args.argc(), args.argv()));
+    args.add(file_.name()).add("my_int=7777");
+
+    params p_new(args.argc(), args.argv());
+
+    // params p_loaded(1, args.argv());
+    // params p_new(args.argc()-1, args.argv()+1);
+    // for (auto it=p_loaded.begin(), fin=p_loaded.end(); it!=fin; ++it) {
+    //     apply_visitor(define_visitor(p_new, fin->name(), const std::string &descr)
+    // }
+
+
+    p_new.define<bool>("my_bool","Boolean")
+        .define<int>("my_int", "Integer")
+        .define<std::string>("my_string", "String")
+        .define<double>("my_double", "Double");
+    EXPECT_EQ(true, p_new["my_bool"]);
+    EXPECT_EQ("ABC", p_new["my_string"]);
+    EXPECT_EQ(12.75, p_new["my_double"]);
+    EXPECT_EQ(7777, p_new["my_int"]);
 }
 
 TEST_F(ParamsTest, h5CtorNotFirstArgument) {
@@ -111,7 +145,6 @@ TEST_F(ParamsTest, h5CtorNotFirstArgument) {
     }
     arg_holder args;
     args.add("some=something").add(file_.name());
-    
+
     EXPECT_ANY_THROW(params p_new(args.argc(), args.argv()));
 }
-
