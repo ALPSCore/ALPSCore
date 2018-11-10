45fbe21705e28fa1a461dcfa417069cd76da0581
iskakoff <sir.iskakoff@gmail.com>
2018-11-09 23:00:14 -0500

    Fourier Transform. cleaning up the steaming mess

--- gf/include/alps/gf/fourier.hpp
+++ gf/include/alps/gf/fourier.hpp
@@ -9,130 +9,118 @@
 namespace alps {
 namespace gf {
 
-inline std::complex<double> f_omega(double wn, double c1, double c2, double c3) {
-  std::complex<double> iwn(0., wn);
-  std::complex<double> iwnsq=iwn*iwn;
-  return c1/iwn + c2/(iwnsq) + c3/(iwn*iwnsq);
-}
+  inline std::complex<double> f_omega(double wn, double c1, double c2, double c3) {
+    std::complex<double> iwn(0., wn);
+    std::complex<double> iwnsq=iwn*iwn;
+    return c1/iwn + c2/(iwnsq) + c3/(iwn*iwnsq);
+  }
 
-inline double f_tau(double tau, double beta, double c1, double c2, double c3) {
-  return -0.5*c1 + (c2*0.25)*(-beta+2.*tau) + (c3*0.25)*(beta*tau-tau*tau);
-}
+  inline double f_tau(double tau, double beta, double c1, double c2, double c3) {
+    return -0.5*c1 + (c2*0.25)*(-beta+2.*tau) + (c3*0.25)*(beta*tau-tau*tau);
+  }
 
-///Fourier transform kernel of the omega -> tau transform
-inline void transform_vector_no_tail(const std::vector<std::complex<double> >&input_data, const std::vector<double> &omega, std::vector<double> &output_data, const std::vector<double> &tau, double beta){
-  for (unsigned int i=0; i<output_data.size(); ++i) {
-    output_data[i] = 0.;
-    for (unsigned int k=0; k<input_data.size(); ++k) {
-      double wt=omega[k]*tau[i];
-      output_data[i]+= 2/beta*(cos(wt)*input_data[k].real()+sin(wt)*input_data[k].imag());
+
+  template<size_t D>
+  inline alps::numerics::tensor<std::complex<double>, D> f_omega(double wn, const alps::numerics::tensor<double, D>& c1, const alps::numerics::tensor<double, D>& c2, const alps::numerics::tensor<double, D>&c3) {
+    std::complex<double> iwn(0., wn);
+    std::complex<double> iwnsq=iwn*iwn;
+    return c1/iwn + c2/(iwnsq) + c3/(iwn*iwnsq);
+  }
+
+  template<size_t D>
+  inline alps::numerics::tensor<double, D> f_tau(double tau, double beta,
+                                                 const alps::numerics::tensor<double, D>& c1,
+                                                 const alps::numerics::tensor<double, D>& c2,
+                                                 const alps::numerics::tensor<double, D>& c3) {
+    return c1 *(-0.5) + (c2*0.25)*(-beta+2.*tau) + (c3*0.25)*(beta*tau-tau*tau);
+  }
+
+  ///Fourier transform kernel of the omega -> tau transform
+  template<size_t D>
+  inline void transform_vector_no_tail_matrix(const alps::numerics::tensor<std::complex<double>, D>&input_data, const std::vector<double> &omega,
+                                              alps::numerics::tensor<double, D> &output_data, const std::vector<double> &tau, double beta){
+    using Matrix  = Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+    using MatrixX = Eigen::Matrix<std::complex<double>, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>;
+    Matrix Cos(tau.size(), omega.size());
+    Matrix Sin(tau.size(), omega.size());
+    for (unsigned int i=0; i<tau.size(); ++i) {
+      for (unsigned int k=0; k<omega.size(); ++k) {
+        double wt=omega[k]*tau[i];
+        Cos(i, k) = cos(wt);
+        Sin(i, k) = sin(wt);
+      }
+    }
+    int ld_in = omega.size();
+    int rest_in = input_data.size() / ld_in;
+    int ld_out = tau.size();
+    int rest_out = output_data.size() / ld_out;
+
+    assert(rest_in == rest_out);
+
+    Eigen::Map<const MatrixX> In(input_data.data(), ld_in, rest_in);
+    Eigen::Map<Matrix>        Out(output_data.data(), ld_out, rest_out);
+
+    // + sign comes from the -i in the phase
+    Out = 2.0*(Cos * In.real() + Sin * In.imag())/beta;
+  }
+
+  ///Fourier transform kernel of the omega -> tau transform
+  template<size_t D>
+  inline void transform_vector_no_tail_loop(const alps::numerics::tensor<std::complex<double>, D> &input_data,
+                                            const std::vector<double> &omega,
+                                            alps::numerics::tensor<double, D> &output_data, const std::vector<double> &tau, double beta){
+    using Matrix  = Eigen::Matrix<double, 1, Eigen::Dynamic, Eigen::RowMajor>;
+    using MatrixX = Eigen::Matrix<std::complex<double>, 1, Eigen::Dynamic, Eigen::RowMajor>;
+    int ld_in = omega.size();
+    int rest_in = input_data.size() / ld_in;
+    int ld_out = tau.size();
+    int rest_out = output_data.size() / ld_out;
+    assert(rest_in == rest_out);
+
+    output_data.set_zero();
+    for (int r = 0; r < rest_in; ++r) {
+      for (unsigned int i=0; i<tau.size(); ++i) {
+        for (unsigned int k = 0; k < omega.size(); ++k) {
+          double wt=omega[k]*tau[i];
+          output_data.data()[i * rest_in + r] += 2.0 * (cos(wt) * input_data.data()[k * rest_in + r].real() + sin(wt) * input_data.data()[k * rest_in + r].imag()) / beta;
+        }
+      }
     }
   }
-}
-///Fourier transform a two-index matsubara gf to an imag time gf
-template<class MESH1> void fourier_frequency_to_time(const two_index_gf_with_tail<
-    two_index_gf<std::complex<double>, matsubara_positive_mesh, MESH1>, one_index_gf<double, MESH1> > &g_omega,
-    two_index_gf_with_tail<two_index_gf<double, itime_mesh, MESH1>, one_index_gf<double, MESH1> > &g_tau){
 
-  std::vector<std::complex<double> > input_data(g_omega.mesh1().extent(), 0.);
-  std::vector<double >               output_data(g_tau.mesh1().extent(), 0.);
+  ///Fourier transform a matsubara gf to an imag time gf
+  template<class...MESHES> void fourier_frequency_to_time(
+      const gf_tail<greenf<std::complex<double>, matsubara_positive_mesh, MESHES...>,
+      greenf<double,MESHES...> > &g_omega,
+      gf_tail<greenf<double, itime_mesh, MESHES...>, greenf<double, MESHES...> > &g_tau){
+    alps::numerics::tensor<std::complex<double>, (sizeof...(MESHES)) + 1> in_data(g_omega.data().shape());
 
-  for(int i=0;i<g_omega.mesh2().extent();++i){
-    double c0=(g_omega.min_tail_order()==0 && g_omega.max_tail_order()>=0 )? g_omega.tail(0)(typename MESH1::index_type(i)):0;
-    double c1=(g_omega.min_tail_order()<=1 && g_omega.max_tail_order()>=1 )? g_omega.tail(1)(typename MESH1::index_type(i)):0;
-    double c2=(g_omega.min_tail_order()<=2 && g_omega.max_tail_order()>=2 )? g_omega.tail(2)(typename MESH1::index_type(i)):0;
-    double c3=(g_omega.min_tail_order()<=3 && g_omega.max_tail_order()>=3 )? g_omega.tail(3)(typename MESH1::index_type(i)):0;
-    if(c0 != 0) throw std::runtime_error("attempt to Fourier transform an object which goes to a constant. FT is ill defined");
-    for(int n=0;n<g_omega.mesh1().extent();++n){
-      input_data[n]=g_omega(matsubara_index(n),typename MESH1::index_type(i))-f_omega(g_omega.mesh1().points()[n],c1,c2,c3);
+    using tail_data = alps::numerics::tensor<double, sizeof...(MESHES)>;
+
+    std::array<size_t, sizeof...(MESHES)> tail_shape;
+    for (int i = 0; i < sizeof...(MESHES); ++i) {
+      tail_shape[i] = g_omega.data().shape()[i+1];
     }
-    transform_vector_no_tail(input_data,g_omega.mesh1().points(), output_data, g_tau.mesh1().points(), g_tau.mesh1().beta());
+
+    tail_data zero_tail(tail_shape);
+
+    const alps::numerics::tensor<double, sizeof...(MESHES)>& c0=(g_omega.min_tail_order()==0 && g_omega.max_tail_order()>=0 )? g_omega.tail(0).data():zero_tail;
+    const alps::numerics::tensor<double, sizeof...(MESHES)>& c1=(g_omega.min_tail_order()<=1 && g_omega.max_tail_order()>=1 )? g_omega.tail(1).data():zero_tail;
+    const alps::numerics::tensor<double, sizeof...(MESHES)>& c2=(g_omega.min_tail_order()<=2 && g_omega.max_tail_order()>=2 )? g_omega.tail(2).data():zero_tail;
+    const alps::numerics::tensor<double, sizeof...(MESHES)>& c3=(g_omega.min_tail_order()<=3 && g_omega.max_tail_order()>=3 )? g_omega.tail(3).data():zero_tail;
+
+    for(int n=0;n<g_omega.mesh1().extent();++n) {
+      in_data(n) = g_omega(matsubara_index(n)).data() - f_omega(g_omega.mesh1().points()[n],c1,c2,c3);
+    }
+
+    if(g_omega.mesh1().extent() * g_tau.mesh1().extent() > 5000000 || g_omega.data().size()/g_omega.mesh1().extent() < 100)
+      transform_vector_no_tail_loop(in_data,g_omega.mesh1().points(), g_tau.data(), g_tau.mesh1().points(), g_tau.mesh1().beta());
+    else
+      transform_vector_no_tail_matrix(in_data,g_omega.mesh1().points(), g_tau.data(), g_tau.mesh1().points(), g_tau.mesh1().beta());
+
     for(int t=0;t<g_tau.mesh1().extent();++t){
-      g_tau(itime_index(t),typename MESH1::index_type(i))=output_data[t]+f_tau(g_tau.mesh1().points()[t],g_tau.mesh1().beta(),c1,c2,c3);
+      g_tau(itime_index(t)).data() += f_tau(g_tau.mesh1().points()[t],g_tau.mesh1().beta(),c1,c2,c3);
     }
   }
-}
-///Fourier transform a three-index matsubara gf to an imag time gf
-template<class MESH1, class MESH2> void fourier_frequency_to_time(const three_index_gf_with_tail<
-    three_index_gf<std::complex<double>, matsubara_positive_mesh, MESH1,MESH2>, two_index_gf<double, MESH1,MESH2> > &g_omega,
-    three_index_gf_with_tail<three_index_gf<double, itime_mesh, MESH1,MESH2>, two_index_gf<double, MESH1,MESH2> > &g_tau){
-
-  std::vector<std::complex<double> > input_data(g_omega.mesh1().extent(), 0.);
-  std::vector<double >               output_data(g_tau.mesh1().extent(), 0.);
-
-  for(int i=0;i<g_omega.mesh2().extent();++i){
-    for(int j=0;j<g_omega.mesh3().extent();++j){
-      double c0=(g_omega.min_tail_order()==0 && g_omega.max_tail_order()>=0 )? g_omega.tail(0)(typename MESH1::index_type(i),typename MESH2::index_type(j)):0;
-      double c1=(g_omega.min_tail_order()<=1 && g_omega.max_tail_order()>=1 )? g_omega.tail(1)(typename MESH1::index_type(i),typename MESH2::index_type(j)):0;
-      double c2=(g_omega.min_tail_order()<=2 && g_omega.max_tail_order()>=2 )? g_omega.tail(2)(typename MESH1::index_type(i),typename MESH2::index_type(j)):0;
-      double c3=(g_omega.min_tail_order()<=3 && g_omega.max_tail_order()>=3 )? g_omega.tail(3)(typename MESH1::index_type(i),typename MESH2::index_type(j)):0;
-      if(c0 != 0) throw std::runtime_error("attempt to Fourier transform an object which goes to a constant. FT is ill defined");
-      for(int n=0;n<g_omega.mesh1().extent();++n){
-        input_data[n]=g_omega(matsubara_index(n),typename MESH1::index_type(i),typename MESH2::index_type(j))-f_omega(g_omega.mesh1().points()[n],c1,c2,c3);
-      }
-      transform_vector_no_tail(input_data,g_omega.mesh1().points(), output_data, g_tau.mesh1().points(), g_tau.mesh1().beta());
-      for(int t=0;t<g_tau.mesh1().extent();++t){
-        g_tau(itime_index(t),typename MESH1::index_type(i),typename MESH2::index_type(j))=output_data[t]+f_tau(g_tau.mesh1().points()[t],g_tau.mesh1().beta(),c1,c2,c3);
-      }
-    }
-  }
-}
-///Fourier transform a four-index matsubara gf to an imag time gf
-template<class MESH1, class MESH2, class MESH3> void fourier_frequency_to_time(const four_index_gf_with_tail<
-    four_index_gf<std::complex<double>, matsubara_positive_mesh, MESH1,MESH2,MESH3>, three_index_gf<double, MESH1,MESH2,MESH3> > &g_omega,
-    four_index_gf_with_tail<four_index_gf<double, itime_mesh, MESH1,MESH2,MESH3>, three_index_gf<double, MESH1,MESH2,MESH3> > &g_tau){
-
-  std::vector<std::complex<double> > input_data(g_omega.mesh1().extent(), 0.);
-  std::vector<double >               output_data(g_tau.mesh1().extent(), 0.);
-
-  for(int i=0;i<g_omega.mesh2().extent();++i){
-    for(int j=0;j<g_omega.mesh3().extent();++j){
-      for(int k=0;k<g_omega.mesh4().extent();++k){
-        double c0=(g_omega.min_tail_order()==0 && g_omega.max_tail_order()>=0 )? g_omega.tail(0)(typename MESH1::index_type(i),typename MESH2::index_type(j),typename MESH3::index_type(k)):0;
-        double c1=(g_omega.min_tail_order()<=1 && g_omega.max_tail_order()>=1 )? g_omega.tail(1)(typename MESH1::index_type(i),typename MESH2::index_type(j),typename MESH3::index_type(k)):0;
-        double c2=(g_omega.min_tail_order()<=2 && g_omega.max_tail_order()>=2 )? g_omega.tail(2)(typename MESH1::index_type(i),typename MESH2::index_type(j),typename MESH3::index_type(k)):0;
-        double c3=(g_omega.min_tail_order()<=3 && g_omega.max_tail_order()>=3 )? g_omega.tail(3)(typename MESH1::index_type(i),typename MESH2::index_type(j),typename MESH3::index_type(k)):0;
-        if(c0 != 0) throw std::runtime_error("attempt to Fourier transform an object which goes to a constant. FT is ill defined");
-        for(int n=0;n<g_omega.mesh1().extent();++n){
-          input_data[n]=g_omega(matsubara_index(n),typename MESH1::index_type(i),typename MESH2::index_type(j),typename MESH3::index_type(k))-f_omega(g_omega.mesh1().points()[n],c1,c2,c3);
-        }
-        transform_vector_no_tail(input_data,g_omega.mesh1().points(), output_data, g_tau.mesh1().points(), g_tau.mesh1().beta());
-        for(int t=0;t<g_tau.mesh1().extent();++t){
-          g_tau(itime_index(t),typename MESH1::index_type(i),typename MESH2::index_type(j),typename MESH3::index_type(k))=output_data[t]+f_tau(g_tau.mesh1().points()[t],g_tau.mesh1().beta(),c1,c2,c3);
-        }
-      }
-    }
-  }
-}
-///Fourier transform a five-index matsubara gf to an imag time gf
-template<class MESH1, class MESH2, class MESH3,class MESH4> void fourier_frequency_to_time(const five_index_gf_with_tail<
-    five_index_gf<std::complex<double>, matsubara_positive_mesh, MESH1,MESH2,MESH3,MESH4>, four_index_gf<double, MESH1,MESH2,MESH3,MESH4> > &g_omega,
-    five_index_gf_with_tail<five_index_gf<double, itime_mesh, MESH1,MESH2,MESH3,MESH4>, four_index_gf<double, MESH1,MESH2,MESH3,MESH4> > &g_tau){
-
-  std::vector<std::complex<double> > input_data(g_omega.mesh1().extent(), 0.);
-  std::vector<double >               output_data(g_tau.mesh1().extent(), 0.);
-
-  for(int i=0;i<g_omega.mesh2().extent();++i){
-    for(int j=0;j<g_omega.mesh3().extent();++j){
-      for(int k=0;k<g_omega.mesh4().extent();++k){
-        for(int l=0;l<g_omega.mesh5().extent();++l){
-        double c0=(g_omega.min_tail_order()==0 && g_omega.max_tail_order()>=0 )? g_omega.tail(0)(typename MESH1::index_type(i),typename MESH2::index_type(j),typename MESH3::index_type(k),typename MESH4::index_type(l)):0;
-        double c1=(g_omega.min_tail_order()<=1 && g_omega.max_tail_order()>=1 )? g_omega.tail(1)(typename MESH1::index_type(i),typename MESH2::index_type(j),typename MESH3::index_type(k),typename MESH4::index_type(l)):0;
-        double c2=(g_omega.min_tail_order()<=2 && g_omega.max_tail_order()>=2 )? g_omega.tail(2)(typename MESH1::index_type(i),typename MESH2::index_type(j),typename MESH3::index_type(k),typename MESH4::index_type(l)):0;
-        double c3=(g_omega.min_tail_order()<=3 && g_omega.max_tail_order()>=3 )? g_omega.tail(3)(typename MESH1::index_type(i),typename MESH2::index_type(j),typename MESH3::index_type(k),typename MESH4::index_type(l)):0;
-        if(c0 != 0) throw std::runtime_error("attempt to Fourier transform an object which goes to a constant. FT is ill defined");
-        for(int n=0;n<g_omega.mesh1().extent();++n){
-          input_data[n]=g_omega(matsubara_index(n),typename MESH1::index_type(i),typename MESH2::index_type(j),typename MESH3::index_type(k),typename MESH4::index_type(l))-f_omega(g_omega.mesh1().points()[n],c1,c2,c3);
-        }
-        transform_vector_no_tail(input_data,g_omega.mesh1().points(), output_data, g_tau.mesh1().points(), g_tau.mesh1().beta());
-        for(int t=0;t<g_tau.mesh1().extent();++t){
-          g_tau(itime_index(t),typename MESH1::index_type(i),typename MESH2::index_type(j),typename MESH3::index_type(k),typename MESH4::index_type(l))=output_data[t]+f_tau(g_tau.mesh1().points()[t],g_tau.mesh1().beta(),c1,c2,c3);
-        }
-        }
-      }
-    }
-  }
-}
-
 }
 } // end alps::
