800a75b415665e6353164cb42ad63d9e3267fab3
Emanuel Gull <egull@umich.edu>
2018-11-19 11:46:04 -0500

    updated copyright notice

--- alea/include/alps/alea/util/model.hpp
+++ alea/include/alps/alea/util/model.hpp
@@ -1,8 +1,9 @@
 /*
- * Copyright (C) 1998-2017 ALPS Collaboration. See COPYRIGHT.TXT
+ * Copyright (C) 1998-2018 ALPS Collaboration. See COPYRIGHT.TXT
  * All rights reserved. Use is subject to license terms. See LICENSE.TXT
  * For use in publications, see ACKNOWLEDGE.TXT
  */
+
 #pragma once
 
 #include <alps/alea/core.hpp>
