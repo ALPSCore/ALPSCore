2acfde03114ec723f7fb5e42161648dee2b4868f
Emanuel Gull <egull@users.noreply.github.com>
2018-11-19 11:48:39 -0500

    Update pooling.hpp

--- alea/include/alps/alea/internal/pooling.hpp
+++ alea/include/alps/alea/internal/pooling.hpp
@@ -1,4 +1,4 @@
-/**
+/*
  * Copyright (C) 1998-2018 ALPS Collaboration. See COPYRIGHT.TXT
  * All rights reserved. Use is subject to license terms. See LICENSE.TXT
  * For use in publications, see ACKNOWLEDGE.TXT
