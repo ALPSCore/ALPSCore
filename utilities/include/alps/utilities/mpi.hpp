4225a276ec4da278df0119b1ce873c616dbeb112
Emanuel Gull <egull@umich.edu>
2022-03-03 09:15:33 -0500

    fixing uncaught exception warning

--- utilities/include/alps/utilities/mpi.hpp
+++ utilities/include/alps/utilities/mpi.hpp
@@ -21,7 +21,7 @@
 
 #include <vector>
 #include <complex>
-#include <exception> /* for std::uncaught_exception() */
+#include <exception> /* for std::uncaught_exceptions() */
 #include <functional> /* for std::plus */
 #include <algorithm> /* for std::max */
 
@@ -245,7 +245,7 @@ namespace alps {
             {
                 if (!initialized_) return; // we are not in control, don't mess up other's logic.
                 if (finalized()) return; // MPI is finalized --- don't touch it.
-                if (abort_on_exception_ && std::uncaught_exception()) {
+                if (abort_on_exception_ && (std::uncaught_exceptions()>0)) {
                     this->abort(255); // FIXME: make the return code configurable?
                 }
                 MPI_Finalize();
